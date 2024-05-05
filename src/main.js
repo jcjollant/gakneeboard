90ccbf728652b6850dd68d5808bd9f62373edf95
JC Jollant <jc@jollant.net>
2024-05-05 01:40:30 -0700

    primevue theme

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import './style.css'
 import App from './App.vue'
 import PrimeView from 'primevue/config';
 import 'primeicons/primeicons.css'
+import 'primevue/resources/themes/saga-blue/theme.css'
 
 createApp(App)
     .use(PrimeView)
