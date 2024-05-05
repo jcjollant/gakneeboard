4385e2e95d8dc14f3c813bc1d74fcc7ef6aca42a
JC Jollant <jc@jollant.net>
2024-05-05 01:35:44 -0700

    Grouped request in airport list

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,4 +1,4 @@
-export const version = '503'
+export const version = '505'
 const apiRootUrl = 'https://ga-api-seven.vercel.app/'
 // const apiRootUrl = 'http://localhost:3000/'
 
@@ -6,7 +6,7 @@ const apiRootUrl = 'https://ga-api-seven.vercel.app/'
 
 import axios from 'axios'
 
-export const demoRadioData = [
+const demoRadioData = [
   {'target':'NAV1','freq':'116.8','name':'SEA VOR'},
   {'target':'NAV2','freq':'124.7','name':'OLM VOR'},
   {'target':'COM1','freq':'124.7','name':'RNT TWR'},
@@ -16,8 +16,7 @@ export const demoRadioData = [
   {'target':'COM1','freq':'120.2','name':'PAE TWR 34R'},
   {'target':'COM1','freq':'132.95','name':'PAE TWR 34L'}
 ]
-
-export const demoPage = [
+ const demoPage = [
   {'id':0,'name':'airport','data':{'code':'krnt','rwy':'16-34'}},
   {'id':1,'name':'airport','data':{'code':'s36','rwy':'15-33'}},
   {'id':2,'name':'airport','data':{'code':'w39','rwy':'NE-SW'}},
@@ -32,7 +31,7 @@ export const demoPage = [
   {'id':11,'name':'notes','data':{}},
 ]
 
-export const blankPage = [
+ const blankPage = [
   {'id':0,'name':'','data':{}},
   {'id':1,'name':'','data':{}},
   {'id':2,'name':'','data':{}},
@@ -45,146 +44,142 @@ export const blankPage = [
   {'id':9,'name':'','data':{}},
   {'id':10,'name':'','data':{}},
   {'id':11,'name':'','data':{}},
-]
+]  
 
 let airports = {}
 let pendingCodes = []
+/**
+ * Query airport data backend
+ * @param {*} codeParam airport code, case doesn't matter
+ * @param {*} group whether this request should be grouped with others
+ * @returns airport data
+ */
+export async function getAirport( codeParam, group = false) {
+    const code = codeParam.toUpperCase()
 
-// get a list of airports in one pass
-export async function preloadAirports(codes) {
-    console.log( 'preloadAirports ' + JSON.stringify(codes))
-
-    // filter out those we already know
-    const newCodes = codes.filter( code => {
-      return !((code in airports) || pendingCodes.includes(code)) 
-    })
-
-    // TODO: Remove
-    // Artifically adds all airports in the cache to prevent further requests
-    newCodes.forEach( code => {
-      airports[code] = {'data':''}
-    })
-
-    // if we don't have anything left, we stop here
-    if( newCodes.length == 0) return;
-
-    console.log( 'fetching airports ' + JSON.stringify(newCodes))
-
-    const url = apiRootUrl + 'airports/';
-    axios.post(url, JSON.stringify(newCodes))
-      .then( response => {
-          // console.log( JSON.stringify(response.data))
-          airportList = response.data
-          airportList.forEach( airport => {
-            // reply should be a list of codes and data
-            // [{'code':'xyz','data':{...}}]
-            airports[airport.code] = airport.data
-          })
-      })
-      .catch( error => {
-        console.log( 'error ' + error)
-      })
-}
-
-export async function getOneAirport( code) {
     // console.log( 'fetching ' + code);
     let airport = null
 
+    // weed out incomplete or invalid codes
     if( !code || code.length < 3 || code.length > 4) {
         // console.log( 'invalid code ' + code)
-        return null
+        return airport
     }
 
-    // do we already know this code?
+    // do we already know this code in the cache?
     if( code in airports) {
         airport = airports[code]
         // console.log( 'found in cache ' + code)
         return airport
     }
 
-    // is this code already in the queue?
+    // if this code is already in the queue we'll just wait for the data
     if( pendingCodes.includes(code)) {
-      // console.log( 'already in queue ' + code)
-      while( !(code in airports)) {
-        // console.log( 'waiting for ' + pendingQueries[0])
-        await new Promise(r => setTimeout(r, 500));
-      }
-      return airports[code]
+      console.log( 'already in queue ' + code)
+      return await waitForAirportData( code)
     }
 
     // add ourselves to the list of pending queries
     pendingCodes.push(code)
 
-    while( pendingCodes[0] != code) {
-      // console.log( 'waiting for ' + pendingQueries[0])
-      await new Promise(r => setTimeout(r, 250));
+    if( group) {
+      // wait until there are no more queries
+      const beforeCount = pendingCodes.length
+      await new Promise(r => setTimeout(r, 500));
+      const afterCount = pendingCodes.length
+      if( beforeCount == afterCount) {
+        await requestAllAirports( pendingCodes)
+      }
+      airport = await waitForAirportData( code)
+      // remove ourselves from the list of pending queries
+      pendingCodes.splice( pendingCodes.indexOf(code), 1)
+
+    } else {
+      // wait until we are in first position
+      while( pendingCodes[0] != code) {
+        // console.log( 'waiting for ' + pendingQueries[0])
+        await new Promise(r => setTimeout(r, 100));
+      }
+
+      airport = await requestOneAirport( code)
+
+      // remove ourselves from the first position in the queue
+      pendingCodes.shift()
     }
 
-    // console.log( pendingCodes.length + " queries in the queue")
+    return airport
+  }
 
-    const url = apiRootUrl + 'airport/';
-    await axios.get(url + code)
-      .then( response => {
-          // console.log( JSON.stringify(response.data))
-          airport = response.data
-          // memorize for next pass
-          airports[code] = airport
-          // console.log( 'added to cache ' + code)
-      })
-      .catch( error => {
-        // save this code as invalid
-        airports[code] = null
-      })
-      pendingCodes.shift()
-      return airport
-}
-  
+/**
+ * @returns a copy of blank page data
+ */
+export function getBlankPage() {
+  return JSON.parse( JSON.stringify(blankPage))
+}  
 
-  var testCount = 0;
-function testKeyInSource(key, source, code) {
-  testCount++;
-  if( !(key in source)) console.log( key +' missing from ' + JSON.stringify[source] + ' at ' + code);
+/**
+ * @returns a copy of demo page data 
+ */
+export function getDemoPage() {
+  return JSON.parse( JSON.stringify(demoPage))
+}  
 
-}
-function testRunway(rwy, rwys, code) {
-  testKeyInSource(rwy, rwys, code);
-  // test this runway has an orientation and pattern
-  testKeyInSource('orientation', rwys[rwy], code);
-  testKeyInSource('pattern', rwys[rwy], code);
-  const pattern = rwys[rwy]['pattern'];
-  if( pattern != 'left' && pattern != 'right') console.log('pattern value ' + pattern + ' is invalid for runway ' + rwy + ' at ' + code)
-}
-export function validate() {
-  testCount = 0;
-  Object.keys(airports).forEach((code)=>{
-    // console.log('validating ' + code);
-    // we want all the essential fields
-    testKeyInSource('code', airports[code], code);
-    testKeyInSource('name', airports[code], code);
-    testKeyInSource('elev', airports[code], code);
-    testKeyInSource('tpa', airports[code], code);
-    testKeyInSource('weather', airports[code], code);
-    // testKeyInSource('ctaf', airports[code], code);
-    testKeyInSource('rwy', airports[code], code);
-    // traffic pattern consistency
-    if( airports[code].tpa - airports[code].elev > 1100) console.log('traffic pattern is too high at ' + code);
-    if( airports[code].tpa - airports[code].elev < 900) console.log('traffic pattern is too low at ' + code);
-    // test weather has a frequency and a type
-    testKeyInSource('freq', airports[code].weather, code);
-    testKeyInSource('type', airports[code].weather, code);
-    // test runways are all showing up
-    airports[code].rwy.forEach((rwys)=>{
-      testKeyInSource('name', rwys, code);
-      testKeyInSource('length', rwys, code);
-      testKeyInSource('surface', rwys, code);
-      testKeyInSource('type', rwys['surface'], code);
-      testKeyInSource('condition', rwys['surface'], code);
-      var [rwyA,rwyB] = rwys.name.split('-');
-      // test each runway has an entry
-      testRunway(rwyA, rwys, code);
-      testRunway(rwyB, rwys, code);
+async function requestAllAirports( codes) {
+  // console.log( 'perform group request for ' + codes.length)
+  const url = apiRootUrl + 'airports/' + codes.join('-');
+  await axios.get(url, codes)
+    .then( response => {
+        // console.log( JSON.stringify(response.data))
+        const airportList = response.data
+        airportList.forEach( (airport,index) => {
+          // reply should be a list of codes and data
+          // [{'code':'xyz','data':{...}}]
+          // console.log('data received for ' + codes[index])
+          // console.log( JSON.stringify(airport))
+          airports[codes[index]] = airport
+        })
     })
-  })
-  console.log('Airport data validation complete ' + testCount + ' tests ' + Object.keys(airports).length + ' airports')
+    .catch( error => {
+      console.log( 'error ' + error)
+    })
 }
 
+/**
+ * Perform the API request for a single airport code
+ * @param {*} code 
+ * @returns 
+ */
+async function requestOneAirport( code) {
+  // console.log( pendingCodes.length + " queries in the queue")
+  let airport = null
+
+  const url = apiRootUrl + 'airport/';
+  await axios.get(url + code)
+    .then( response => {
+        // console.log( JSON.stringify(response.data))
+        airport = response.data
+        // add this data to cache
+        airports[code] = airport
+        // console.log( 'added to cache ' + code)
+    })
+    .catch( error => {
+      // cache this airport as invalid
+      airports[code] = null
+    })
+
+  return airport
+}
+
+/**
+ * Our data has already been requested, we are waiting for it
+ * @param {*} code 
+ * @returns 
+ */
+async function waitForAirportData( code) {
+  // console.log( 'waiting for ' + code) 
+  while( !(code in airports)) {
+    await new Promise(r => setTimeout(r, 250));
+  }
+  // console.log( 'done waiting for ' + code)
+  return airports[code]
+}
