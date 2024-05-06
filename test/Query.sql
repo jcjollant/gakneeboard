dfcb079728da05f13e40ef20d0e95435a308811b
JC Jollant <jc@jollant.net>
2024-05-05 22:23:01 -0700

    Useful queries

---
+++ test/Query.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(*) FROM airports
+
+SELECT COUNT(*) FROM airports WHERE version=4
+
+# How many airports have not been upgraded
+SELECT COUNT(*) FROM airports WHERE version<4
+SELECT * FROM airports WHERE version<4
+
+
+SELECT COUNT(*) as count, Code from Airports GROUP BY Code ORDER BY count DESC
+
+
+
+SELECT * FROM Airports WHERE Code='KRNT'
