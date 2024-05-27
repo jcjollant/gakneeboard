2f6b71cad47187b558966c6aa06756237c97e869
JC Jollant <jc@jollant.net>
2024-05-27 13:09:50 -0700

    Users table

--- test/Query.sql
+++ test/Query.sql
@@ -10,6 +10,8 @@ SELECT * FROM airports WHERE version<5
 
 SELECT COUNT(*) as count, Code from Airports GROUP BY Code ORDER BY count DESC
 
+#########################################
+# Feedback table
 SELECT * FROM feedback
 
 DELETE FROM feedback WHERE Id in (33,38,40,41)
@@ -20,3 +22,13 @@ SELECT * FROM Unknowns
 
 SELECT count(*) FROM Unknowns
 
+#########################################
+# Users table
+SELECT COUNT(*) FROM Users
+
+SELECT * FROM Users
+
+# Particular user : JC
+SELECT * FROM Users WHERE sha256 = 'bfaa2eb49bf63f41c05a016e03653fe2d7f8bf196ba6fb3f3340d3dcd7016770'
+# Anne-Sophie
+SELECT * FROM Users WHERE sha256 = 'f731afac9fc850681158ed35a3e289d577770c104fe063c26b4cef4b1af22444'
