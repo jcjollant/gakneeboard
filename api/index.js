f6b71ffc233a0d1b6b1560a0c50f38033cdc8330
JC Jollant <jc@jollant.net>
2024-05-27 16:47:47 -0700

    Added /api to verci.json, removed use express.json

--- api/index.js
+++ api/index.js
@@ -40,7 +40,7 @@ if( process.env.NODE_ENV == 'development') {
     });
 
 }    
-app.use(express.json())
+// app.use(express.json())
 
 
 app.get("/", (req, res) => res.send("GA API version " + version));
