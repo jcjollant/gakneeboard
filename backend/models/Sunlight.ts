74d07bef7d5e596c2009359614af32644ae7e6dd
JC Jollant <jc@jollant.net>
2024-07-17 15:33:59 -0700

    New class to store sunlight data

---
+++ backend/models/Sunlight.ts
@@ -0,0 +1,27 @@
+export class Sunlight {
+    date:string;
+    sunrise:string;
+    sunset:string;
+    civilTwilight:{am:string,pm:string};
+    solarNoon:string;
+    goldenHour:string;
+
+    constructor( from:any, to:any|undefined=undefined) {
+        if(from && from.results) {
+            this.date = from.results.date;
+            this.sunrise = from.results.sunrise;
+            this.civilTwilight = {am:'',pm:''};
+            this.civilTwilight.am = from.results.dawn;
+            this.solarNoon = from.results.solar_noon;
+            if(to) {
+                this.sunset = to.results.sunset;
+                this.civilTwilight.pm = to.results.dusk;
+                this.goldenHour = to.results.golden_hour;
+            } else {
+                this.sunset = from.results.sunset;
+                this.civilTwilight.pm = from.results.dusk;
+                this.goldenHour = from.results.golden_hour;
+            }
+        }
+    }
+}
