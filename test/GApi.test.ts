873db1ce128cddb3e7b08201694ef3084fb2e13c
JC Jollant <jc@jollant.net>
2024-07-17 15:34:42 -0700

    Switched to typescript

--- test/GApi.test.ts
+++ test/GApi.test.ts
@@ -2,6 +2,7 @@ import { describe, expect, test} from '@jest/globals';
 import { GApi, GApiError } from '../backend/GApi'
 import { jcHash, postgresUrl, jcUserId, jcDemoSheet, jcToken, jcName } from './constants'
 import { AirportView } from '../backend/models/AirportView'
+import { Sunlight } from '../backend/models/Sunlight';
 
 process.env.POSTGRES_URL=postgresUrl
 
@@ -9,39 +10,48 @@ describe( 'GApi Tests', () => {
 
     test('Renton is found with both codes', async () => {
         let airport = await GApi.getAirport('RNT')
-        expect(airport.code).toBe('KRNT')
+        expect(airport).toBeDefined()
+        expect(airport?.code).toBe('KRNT')
         let airport2 = await GApi.getAirport('KRNT')
-        expect(airport2.code).toBe('KRNT')
+        expect(airport2).toBeDefined()
+        expect(airport2?.code).toBe('KRNT')
     })
 
     test('Getting multiple Airports', async () => {
         let list = ['rnt','jfk']
-        let airports = await GApi.getAirportsList(list)
+        let airports = await GApi.getAirportViewList(list)
         // console.log(airports)
         expect(airports.length).toBe(list.length)
-        expect(airports[0].code).toBe('KRNT')
-        expect(airports[1].code).toBe('KJFK')
+        expect(airports[0]).toBeDefined()
+        expect(airports[0]?.code).toBe('KRNT')
+        expect(airports[1]).toBeDefined()
+        expect(airports[1]?.code).toBe('KJFK')
 
         let list2 = ['jc','pae','jcj']
-        airports = await GApi.getAirportsList(list2)
+        airports = await GApi.getAirportViewList(list2)
         expect(airports).toHaveLength(list2.length)
-        expect(airports[0].code).toBe('JC')
-        expect(airports[0].version).toBe(-1)
-        expect(airports[1].code).toBe('KPAE')
-        expect(airports[1].version).toBe(AirportView.currentVersion)
-        expect(airports[2].code).toBe('JCJ')
-        expect(airports[2].version).toBe(-1)
+        expect(airports[0]).toBeDefined()
+        expect(airports[0]?.code).toBe('JC')
+        expect(airports[0]?.version).toBe(-1)
+        expect(airports[1]).toBeDefined()
+        expect(airports[1]?.code).toBe('KPAE')
+        expect(airports[1]?.version).toBe(AirportView.currentVersion)
+        expect(airports[2]).toBeDefined()
+        expect(airports[2]?.code).toBe('JCJ')
+        expect(airports[2]?.version).toBe(-1)
     })
 
     test('Invalid airports list', async() =>{
         let list = ['nt','fk']
-        let airports = await GApi.getAirportsList(list)
+        let airports = await GApi.getAirportViewList(list)
         // console.log(airports)
         expect(airports).toHaveLength(2)
-        expect(airports[0].code).toBe('NT')
-        expect(airports[0].version).toBe(-1)
-        expect(airports[1].code).toBe('FK')
-        expect(airports[1].version).toBe(-1)
+        expect(airports[0]).toBeDefined()
+        expect(airports[0]?.code).toBe('NT')
+        expect(airports[0]?.version).toBe(-1)
+        expect(airports[1]).toBeDefined()
+        expect(airports[1]?.code).toBe('FK')
+        expect(airports[1]?.version).toBe(-1)
     })
 
     test('Invalid Airport Code', async () => {
@@ -81,7 +91,6 @@ describe( 'GApi Tests', () => {
     })
 
     test('Militrary frequencies', () => {
-        expect(GApi.isMilitary(null)).toBe(false)
         expect(GApi.isMilitary('-.-')).toBe(false)
         expect(GApi.isMilitary('')).toBe(false)
         expect(GApi.isMilitary('121.6')).toBe(false)
@@ -132,6 +141,22 @@ describe( 'GApi Tests', () => {
         // Bogus Hash should return undefined
         await GApi.userShaToId('bogusHash').then( id => {
             expect(id).toBeUndefined()
+        }).catch( (e) => {
+            console.log(e)
+            expect(true).toBe(false) // should not get here
+        })
+    })
+
+    test('Sunlight', async () => {
+        await GApi.getSunlight('KRNT', 'KSFF', 20240717).then( data => {
+            expect(data).toBeDefined()
+            expect(data?.date).toBe('2024-07-17')
+            expect(data?.sunrise).toBe('5:29:46 AM')
+            expect(data?.civilTwilight.am).toBe('4:51:22 AM')
+            expect(data?.civilTwilight.pm).toBe('9:22:09 PM')
+            expect(data?.solarNoon).toBe('1:16:03 PM')
+            expect(data?.sunset).toBe('8:43:31 PM')
+            expect(data?.goldenHour).toBe('7:56:53 PM')
         }).catch( (e) => {
             console.log(e)
             expect(true).toBe(false) // should not get here
