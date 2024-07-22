285608512f9b0917280874dc59a70247c51731af
JC Jollant <jc@jollant.net>
2024-07-21 22:00:59 -0700

    Removed unknown

--- backend/db.js
+++ backend/db.js
@@ -4,21 +4,21 @@ const postgres = require('@vercel/postgres')
  * adds a known unknown to the DB
  * @param {*} code whatever was recevied from the caller
  */
-async function addKnownUnknown(code) {
-    console.log( '[db] addKnownUnknown ' + code)
-    await postgres.sql`INSERT INTO Unknowns (Code) VALUES (${code})`;
-}
+// async function addKnownUnknown(code) {
+//     console.log( '[db] addKnownUnknown ' + code)
+//     await postgres.sql`INSERT INTO Unknowns (Code) VALUES (${code})`;
+// }
 
 /**
  * Figure out if a code is already unknown
  * @param {*} code 
  * @returns 
 */
-async function isKnownUnknown(code) {
-    const result = await postgres.sql`SELECT * FROM Unknowns WHERE Code = ${code}`;
+// async function isKnownUnknown(code) {
+//     const result = await postgres.sql`SELECT * FROM Unknowns WHERE Code = ${code}`;
     
-    return result.rowCount > 0
-}
+//     return result.rowCount > 0
+// }
 
 async function saveFeedback(data) {
     console.log( '[db.saveFeedback] ')
@@ -30,4 +30,4 @@ async function saveFeedback(data) {
 
 }
 
-module.exports = { addKnownUnknown, isKnownUnknown, saveFeedback};
+module.exports = { saveFeedback};
