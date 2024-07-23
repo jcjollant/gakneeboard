cf40e0ee8082f99cbc010523be03d27c7b388488
JC Jollant <jc@jollant.net>
2024-07-22 18:01:35 -0700

    Merged airport tool into AirportView

--- backend/AirportTools.ts
+++ backend/AirportTools.ts
@@ -1,56 +1,11 @@
-import { Airport, Runway, RunwayEnd, PatternDirection, Frequency } from "./models/Airport"
-import { AirportView } from "./models/AirportView"
+import { Airport, Frequency } from "./models/Airport"
 
 export class AirportTools {
     static getFrequency(freq:Frequency[], name: string) {
         return freq.find((freq) => freq.name == name)?.mhz
     }
-    /**
-     * Transform the input to an AirportView object for UI use
-     * @param input 
-     * @returns 
-     */
-    public static format(input:any):AirportView|undefined {
-        if(input == undefined || input.version == undefined) return undefined
-        let output:any = undefined
 
-        if(input.version == 5) {
-            // Version 5 needs to be reworked
-            output = new Airport(input.code, input.name, input.elev);
-            if('ctaf' in input) output.addFrequency( 'CTAF', parseFloat(input.ctaf));
-            if('gnd' in input) output.addFrequency( 'GND', parseFloat(input.gnd));
-            if( 'weather' in input) output.addFrequency( input.weather.type, parseFloat(input.weather.freq));
-            if('effectiveDate' in input) output.setEffectiveDate( input.effectiveDate);
-            for( const rwy of input.rwy) {
-                const runway = new Runway( rwy.name, rwy.length, rwy.width)
-                const endsName = runway.getEndsName()
-                for(const endName of endsName) {
-                    const rwyEnd:RunwayEnd|undefined = runway.getEnd(endName)
-                    if( rwyEnd) {
-                        rwyEnd.setMagneticOrientation( rwy[endName].orientation)
-                        rwyEnd.setTrafficPattern(rwy[endName].pattern == 'right' ? PatternDirection.Right : PatternDirection.Left)
-                    }
-                }
-                if( 'surface' in rwy) {
-                    runway.setSurface(rwy.surface.type, rwy.surface.condition)
-                }
-                output.addRunway(runway)
-                if('freq' in rwy) {
-                    output.setRunwayFrequency(rwy.name, parseFloat(rwy.freq))
-                }
-
-            }
-            output = new AirportView(output)
-        } else if ( input.version >= 6) {
-            output = new AirportView(input)
-        } else if ( input.version == -1) {
-            output = input
-        }
-
-        return output
-    }
-
-    public static isValidAirportCode(code:string):boolean {
+   public static isValidAirportCode(code:string):boolean {
         return Airport.isValidCode(code)
     }
 }
--- test/AirportView.test.ts
+++ test/AirportView.test.ts
@@ -1,6 +1,8 @@
 import {describe, expect, test} from '@jest/globals';
-import { Airport } from '../backend/models/Airport';
+import { Adip } from '../backend/Adip';
+import { Airport, PatternDirection } from '../backend/models/Airport';
 import { AirportView } from '../backend/models/AirportView';
+import { kbfiData, krntData } from './adipData'
 
 describe( 'Airport View', () => {
     test( 'Undefined constructor', () => {
@@ -15,6 +17,9 @@ describe( 'Airport View', () => {
         expect(undefinedView).toBeDefined()
         expect(undefinedView.code).toBe(airportCode)
         expect(undefinedView.version).toBe(-1)
+        expect(undefinedView.freq).toHaveLength(0)
+        expect(undefinedView.rwys).toHaveLength(0)
+        expect(undefinedView.navaids).toHaveLength(0)
     })
 
     test('Defined view', () => {
@@ -30,5 +35,64 @@ describe( 'Airport View', () => {
         expect(view.freq).toHaveLength(0)
         expect(view.rwys).toHaveLength(0)
         expect(view.custom).toBeFalsy()
+    })
+
+    test('Boeing View', () => {
+        const view = new AirportView(Adip.airportFromData(kbfiData))
+        // console.log(v6)
+        expect(view).toBeInstanceOf(AirportView)
+        if( view) {
+            expect(view.rwys.length).toBe(2)
+            const expectedRwys = [{name:'14L-32R',mhz:118.3},{name:'14R-32L',mhz:120.6}]
+            for(let index = 0; index < expectedRwys.length; index++) {
+                const rwy = view.rwys[index]
+                expect(rwy.name).toBe(expectedRwys[index].name)
+                expect(rwy.freq).toBe(expectedRwys[index].mhz)
+            }
+        }
+    })
+
+    test('Renton view', () => {
+        const view = new AirportView(Adip.airportFromData(krntData));
+        expect(view).toBeDefined()
+        expect(view).toBeInstanceOf(AirportView)
+        expect(view?.version).toBe(AirportView.currentVersion)
+        expect(view?.code).toBe('KRNT')
+        expect(view?.name).toBe('Renton Muni')
+        const allFreq = [['CTAF','124.7'],['GND','121.6'],['ATIS','126.95'],['TWR','124.7'],['UNICOM','122.95']]
+        expect(view.freq).toHaveLength(allFreq.length)
+        // Test all frequencies
+        for(const pair of allFreq) {
+            // console.log(pair[0])
+            const freq = Airport.getFrequencyMhz(view.freq, pair[0])
+            expect(freq).toBeDefined()
+            expect(freq).toBe(Number(pair[1]))
+        }
+
+        expect(view?.elev).toBe(32)
+        expect(view?.rwys.length).toBe(1)
+        const rwy0 = view?.rwys[0]
+        expect(rwy0?.name).toBe("16-34")
+        expect(rwy0?.length).toBe(5382)
+        expect(rwy0?.width).toBe(200)
+        expect(rwy0?.surface?.type).toBe("ASPH-CONC")
+        expect(rwy0?.surface?.cond).toBe("(G) Good")
+        // check ends
+        const end0 = rwy0?.getEnd('16')
+        expect(end0?.mag).toBe(157)
+        expect(end0?.tp).toBe(PatternDirection.Left)
+        const end1 = rwy0?.getEnd('34')
+        expect(end1?.mag).toBe(337)
+        expect(end1?.tp).toBe(PatternDirection.Right)
+        // check navaids
+        const expectedNavaids = [{id:"SEA",freq:116.8,type:"VORTAC",dist:5.2,to:47.7,},{id:"PAE",freq:110.6,type:"VOR/DME",dist:25.7,to:174.4,},{id:"OLM",freq:113.4,type:"VORTAC",dist:42,to:41.5,},{id:"CVV",freq:117.2,type:"VOR/DME",dist:49.6,to:155.4,}]
+        for(let index = 0; index < expectedNavaids.length; index++) {
+            const navaid = view.navaids[index]
+            expect(navaid.id).toBe(expectedNavaids[index].id)
+            expect(navaid.freq).toBe(expectedNavaids[index].freq)
+            expect(navaid.type).toBe(expectedNavaids[index].type)
+            expect(navaid.dist).toBe(expectedNavaids[index].dist)
+            expect(navaid.to).toBe(expectedNavaids[index].to)
+        }
     })
 })
