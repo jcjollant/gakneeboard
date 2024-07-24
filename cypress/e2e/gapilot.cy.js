dcc1dab09526b8653cbf3870fc7afc192a9e2815
JC Jollant <jc@jollant.net>
2024-07-24 10:19:59 -0700

    Night flight test

--- cypress/e2e/gapilot.cy.js
+++ cypress/e2e/gapilot.cy.js
@@ -1,4 +1,4 @@
-const currentVersionNumber = '722'
+const currentVersionNumber = '724'
 
 describe('template spec', () => {
   it('Visits main page', () => {
@@ -12,14 +12,13 @@ describe('template spec', () => {
     cy.intercept({
       method: 'GET',
       url: 'https://ga-api-seven.vercel.app/airports/**',
-    }).as('getAirports');    
+    }).as('getAirports');
 
     cy.wait('@getAirports').its('response.statusCode').should('equal', 200)
 
-
-
     // remove banner
     cy.contains('Got it').click()
+
 
     // check version number
     cy.get('.versionDialog').contains(currentVersionNumber)
@@ -121,13 +120,35 @@ describe('template spec', () => {
       cy.get(`:nth-child(${index}) > .runway`).contains('Rwy')
   
     }
+
     // Test Sunlight Tile
-    cy.get('[data-v-364bf338=""][data-v-b66d1b07=""] > .header > div').contains('Sun Light')
+    cy.get('.pageTwo > :nth-child(3) > .header > div').contains('Sun Light')
     // date should be today
     const today = new Date().toLocaleString('en-US', {weekday: 'short', month: 'short', day: 'numeric'})
     cy.get('.date').contains( today)
+    // Check corners in day mode
+    cy.get('.topLeftCorner').contains('KRNT')
+    cy.get('.topRightCorner').contains('KSFF')
+    cy.get('.bottomLeftCorner').contains('Solar Noon')
+    cy.get('.bottomRightCorner').contains('Golden Hour')
+    // switch to overnight mode
+    cy.get('.pageTwo > :nth-child(3) > .header > div').click()
+    cy.get('.nightFlight').click()
+    cy.get('[aria-label="Apply"]').click()
+    cy.intercept({
+      method: 'GET',
+      url: 'https://ga-api-seven.vercel.app/sunlight/**',
+    }).as('getSunlight');    
+    cy.wait('@getSunlight').its('response.statusCode').should('equal', 200)
+    // Check corners in night mode
+    cy.get('.topLeftCorner').contains('KRNT')
+    cy.get('.topRightCorner').contains('KSFF')
+    cy.get('.bottomLeftCorner').contains('From')
+    cy.get('.bottomRightCorner').contains('To')
+    cy.get('.date').contains( 'Night Flight')
+
     // Test Tile can be replaced by Notes
-    cy.get('[data-v-364bf338=""][data-v-b66d1b07=""] > .header > div').click()
+    cy.get('.pageTwo > :nth-child(3) > .header > div').click()
     cy.get('.header > .p-button').click()
     cy.get('[aria-label="Notes"]').click()
     cy.get(':nth-child(2) > :nth-child(3) > .header > div').contains('Notes')
