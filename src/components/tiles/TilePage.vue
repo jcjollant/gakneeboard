5fc4832dcdcc796f8e9148ae22b6e3a53de58203
JC Jollant <jc@jollant.net>
2024-07-29 00:13:08 -0700

    New Page dedicated to tiles

---
+++ src/components/tiles/TilePage.vue
@@ -0,0 +1,58 @@
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+
+import Tile from './Tile.vue'
+
+const emits = defineEmits(['toast','update'])
+
+// Props management
+const props = defineProps({
+    data: { type: Object, default: null},
+})
+
+function loadProps(props) {
+  // console.log('[TilePage.loadProps]', typeof props.data, JSON.stringify(props.data))
+  if(!props.data) return;
+  tiles.value = props.data
+}
+
+onMounted(() => {
+    // console.log('Tile mounted')
+    loadProps(props)
+})
+
+watch( props, async() => {
+    // console.log("Airport props changed " + JSON.stringify(props));
+    loadProps(props)
+})
+
+// end of props management
+
+const tiles=ref()
+
+function onToast(data) {
+  emits('toast', data)
+}
+
+function onUpdate(newTileData) {
+  const newPageData = tiles.value
+  // update the correct tile
+  newPageData[newTileData.id] = newTileData;
+  emits('update', newPageData)
+}
+
+</script>
+
+<template>
+    <div class="tiles">
+        <Tile v-for="tile in tiles" :tile="tile" @update="onUpdate" @toast="onToast" />
+    </div>
+</template>
+
+<style scoped>
+.tiles {
+  display: grid;
+  grid-template-columns: auto auto;
+  gap: 5px;
+}
+</style>
