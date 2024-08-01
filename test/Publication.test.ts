ad6310139c4f7e23fc627629a226d3c4caa8f563
JC Jollant <jc@jollant.net>
2024-07-31 22:39:02 -0700

    New tests for new DAO

---
+++ test/Publication.test.ts
@@ -0,0 +1,17 @@
+
+import {describe, expect, test} from '@jest/globals';
+
+import { Publication } from '../backend/models/Publication'
+
+describe('Publication', () => {
+    test ('constructor', () => {
+        // pick a random number between 1 and 1000
+        const someId = Math.trunc(Math.random() * 1000)
+        const someCode = 'GA'
+        const someSheetId = Math.trunc(Math.random() * 1000)
+        const pub:Publication = new Publication(someId, someCode, someSheetId)
+        expect(pub.id).toBe(someId) 
+        expect(pub.code).toBe(someCode) 
+        expect(pub.sheetid).toBe(someSheetId)
+    })
+})
