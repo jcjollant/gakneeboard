0273791030bf46becf40c1bf2493a401fff8557e
JC <jc@jollant.net>
2024-08-06 14:50:47 -0700

    WiP new component to view checlist

---
+++ src/components/checklist/ChecklistViewer.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import { ref } from 'vue'
+
+const props = defineProps({
+    items: { type: Object, default: [] },
+})
+
+</script>
+
+<template>
+    <div v-for="(item, index) in props.items" class="checklist"
+        :class="(index % 2) ? theme : ''">
+        <div v-if="'s' in item" class="separator">{{ item.s }}</div>
+        <div v-else class="challenge">{{ item.c }}</div>
+        <div class="response">{{ item.r }}</div>
+    </div>
+</template>
+
+<style scoped>
+</style>
