946871d930d0fe7108db2454080c7035c2c8f421
JC <jc@jollant.net>
2024-08-06 14:51:50 -0700

    Using new Checklist tile

--- src/components/tiles/Tile.vue
+++ src/components/tiles/Tile.vue
@@ -5,6 +5,7 @@ import Header from '../shared/Header.vue';
 import Airport from '../airport/Airport.vue';
 import Atis from '../atis/Atis.vue'
 import Notes from '../notes/Notes.vue';
+import ChecklistTile from '../checklist/ChecklistTile.vue';
 import Clearance from '../clearance/Clearance.vue';
 import RadioFlow from '../radios/RadioFlow.vue';
 import SunLight from '../sunlight/SunLight.vue';
@@ -21,8 +22,9 @@ const props = defineProps({
 
 var state = {}
 const knownTiles = ref([
-    {'name':'Airport','tile':'airport', 'class':'double', 'tooltip':'Display runway and useful information'},
+    {'name':'Airport','tile':'airport', 'class':'', 'tooltip':'Display runway and useful information'},
     {'name':'ATIS','tile':'atis', 'class':'', 'tooltip':'Write down ATIS information'},
+    {'name':'Checklist','tile':'checklist', 'class':'', 'tooltip':'Short checklist'},
     {'name':'Clearance','tile':'clearance', 'class':'', 'tooltip':'Write down clearance information'},
     {'name':'Fuel','tile':'fuel', 'class':'','tooltip':'Track your fuel consumption'},
     {'name':'Notes','tile':'notes', 'class':'', 'tooltip':'A blank tile to write stuff'},
@@ -48,6 +50,14 @@ function loadProps( props) {
     tile.value = props.tile
 }
 
+// replace a tile with a new one
+function onReplace(newName = '') {
+    // widget.value = { 'id':widget.value.id,'name': newName.toLowerCase(), 'data':{}}
+    state = { 'id':tile.value.id,'name': newName.toLowerCase(), 'data':{}}
+    tile.value = state
+    // console.log( "Widget emits update with " + JSON.stringify(widget.value))
+    emits('update',state)
+}
 
 // when a tile notifies us of an update, we notify the parent to save values
 function onUpdate(params = '') {
@@ -61,40 +71,34 @@ function onToast(data) {
     emits('toast', data)
 }
 
-function replaceWidget(newName = '') {
-    // widget.value = { 'id':widget.value.id,'name': newName.toLowerCase(), 'data':{}}
-    state = { 'id':tile.value.id,'name': newName.toLowerCase(), 'data':{}}
-    tile.value = state
-    // console.log( "Widget emits update with " + JSON.stringify(widget.value))
-    emits('update',state)
-}
-
 
 </script>
 
 <template>
-    <div v-if="!tile || tile.name==''" class="widget">
+    <div v-if="!tile || tile.name==''" class="tile">
         <Header :title="'Tile Selection'" :clickable="false"></Header>
         <!-- <div class="widgetTitle">Tile Selection</div> -->
         <div class="content list">
             <Button v-for="tile in knownTiles" :label="tile.name" :class="tile.class" :title="tile.tooltip"
-                @click="replaceWidget(tile.tile)"></Button>
+                @click="onReplace(tile.tile)"></Button>
         </div>
     </div>
     <Airport v-else-if="tile.name=='airport'" :params="tile.data" 
-        @replace="replaceWidget" @update="onUpdate" />
+        @replace="onReplace" @update="onUpdate" />
     <Atis v-else-if="tile.name=='atis'" :params="tile.data"
-        @replace="replaceWidget" @update="onUpdate"/>
+        @replace="onReplace" @update="onUpdate"/>
+    <ChecklistTile v-else-if="tile.name=='checklist'" 
+        @replace="onReplace"/>
     <Clearance v-else-if="tile.name=='clearance'" 
-        @replace="replaceWidget"/>
+        @replace="onReplace"/>
     <FuelBug v-else-if="tile.name=='fuel'" :params="tile.data"
-        @replace="replaceWidget" @update="onUpdate"/>  
+        @replace="onReplace" @update="onUpdate"/>  
     <Notes v-else-if="tile.name=='notes'" 
-        @replace="replaceWidget" />
+        @replace="onReplace" />
     <RadioFlow v-else-if="tile.name=='radios'" :params="tile.data" 
-        @replace="replaceWidget" @update="onUpdate" @toast="onToast" />
+        @replace="onReplace" @update="onUpdate" @toast="onToast" />
     <SunLight v-else-if="tile.name=='sunlight'" :params="tile.data" 
-        @replace="replaceWidget" @update="onUpdate" />
+        @replace="onReplace" @update="onUpdate" />
     <!-- <List v-else-if="widget.name=='list'" @replace="updateWidgetName"/> -->
 </template>
 
@@ -110,13 +114,6 @@ function replaceWidget(newName = '') {
 
 .double  {
     grid-column: 1 / span 2;
-}
-
-.widget {
-  border: 1px solid darkgrey;
-  font-family: Verdana, sans-serif;
-  width: 240px;
-  height: 240px;
 }
 
 </style>
