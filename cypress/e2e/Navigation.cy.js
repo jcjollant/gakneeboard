a885d6812a8a32f597c8c4fc00a8b500aaab5987
JC Jollant <jc@jollant.net>
2024-08-07 19:33:42 -0700

    New test for mirror feature

--- cypress/e2e/Navigation.cy.js
+++ cypress/e2e/Navigation.cy.js
@@ -1,4 +1,4 @@
-import { visitAndCloseBanner, newPage } from './shared'
+import { visitAndCloseBanner, newPage, titleAtis } from './shared'
 
 describe('Tiles', () => {
   // ========================================================================
@@ -26,11 +26,34 @@ describe('Tiles', () => {
     
   })
 
-  it.skip('Mirrors', () => {
+  it('Mirrors', () => {
     visitAndCloseBanner()
-    // load checlist demo
+
+    // wait for airports query
+    cy.intercept({
+      method: 'GET',
+      url: 'https://ga-api-seven.vercel.app/airports/**',
+    }).as('getAirports');
+
+    cy.wait('@getAirports').its('response.statusCode').should('equal', 200)
+
+    // load checklist demo
     cy.get('.menuIcon').click()
-    cy.get('[aria-label="Load"]').click()
+    cy.get('[aria-label="Mirror"]').click()
+    cy.get('.p-confirm-dialog-accept').click()
+
+    // check tiles have been mirrored
+    cy.get('.pageTwo > :nth-child(5) > .header > div').contains(titleAtis)
+    cy.get('.pageTwo > :nth-child(6) > .header > div').contains('Clearance @')
+
+    // Modifying left page should not impact right page
+    cy.get('.pageOne > :nth-child(5) > .header > div').click()
+    // replace tile with notes
+    cy.get('.header > .p-button').click()
+    cy.get('[aria-label="Notes"]').click()
+    cy.get('.pageOne > :nth-child(5) > .header > div').contains('Notes')
+    // Page two should still be Atis
+    cy.get('.pageTwo > :nth-child(5) > .header > div').contains(titleAtis)
 
   })
 
