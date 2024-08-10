a30c92bcc7f6bf09bc2a61d209140cf1e67213cc
JC Jollant <jc@jollant.net>
2024-08-10 07:44:17 -0700

    Now supporting 'strong' section# Please enter the commit message for your changes. Lines starting

--- src/components/checklist/ChecklistViewer.vue
+++ src/components/checklist/ChecklistViewer.vue
@@ -33,7 +33,7 @@ watch(props, () => {
 <template>
     <div v-if="items.length > 0" v-for="(item, index) in items" class="checklist"
         :class="(index % 2) ? theme : ''">
-        <div v-if="'s' in item" class="section spanned" :class="{'emergent': item.t =='emer'}" >{{ item.s }}</div>
+        <div v-if="'s' in item" class="section spanned" :class="{'emergent': item.t =='emer','strong': item.t=='strong'}" >{{ item.s }}</div>
         <div v-else class="challenge" :class="{'smallFont': small, 'spanned':!('r' in item)}">{{ item.c }}</div>
         <div v-if="'r' in item" class="response" :class="{'smallFont': small}">{{ item.r }}</div>
     </div>
@@ -91,18 +91,33 @@ watch(props, () => {
 .spanned {
     grid-column: 1 / span 2
 }
+
+.strong {
+    color:white;
+}
 .theme-yellow {
     background: lightyellow;
+}
+.theme-yellow .strong {
+    background: darkorange;
 }
 .theme-blue {
     background: #b4c6e7;
 }
+.theme-blue .strong{
+    background: blue;
+}
 .theme-green {
     background: #c6e0b4;
+}
+.theme-green .strong{
+    background: darkgreen;
 }
 .theme-grey {
     background: #e9e9e9;
 }
-
+.theme-grey .strong {
+    background: #666;
+}
 
 </style>
