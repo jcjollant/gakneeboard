b137d2328846f22e6c77d83c37c7825fd373b792
JC Jollant <jc@jollant.net>
2024-08-12 21:10:10 -0700

    Fixed tests for new print options

--- cypress/e2e/Navigation.cy.js
+++ cypress/e2e/Navigation.cy.js
@@ -17,12 +17,9 @@ describe('Tiles', () => {
     cy.get('#pv_id_4_header').contains('Print')
     // Check Page options
     cy.get('[aria-label="Front Page"]')
-    cy.get('[aria-label="Both Pages"]')
+    cy.get('[aria-label="Both"]')
+    cy.get('[aria-label="Flipped"]')
     cy.get('[aria-label="Back Page"]')
-    // check options
-    cy.get('[title="So you can read back page while front page is clipped"] > .ml-2').contains('Flip Back Page')
-    cy.get('[title="That\'s the little thing in the bottom right corner"] > .ml-2').contains('Hide version number')
-    cy.get('[title="That\'s the little thing in the bottom right corner"] > .ml-2').click()
     // click do not print
     cy.get('.actionDialog > .p-button-link').click()
 
