9f90518f027559c6061f23c4e37bc95301a01dcf
JC Jollant <jc@jollant.net>
2024-08-18 10:43:24 -0700

    v818

--- cypress/e2e/shared.js
+++ cypress/e2e/shared.js
@@ -1,4 +1,4 @@
-export const currentVersionNumber = '817/814.2'
+export const currentVersionNumber = '818/818'
 const devEnv = 'http://localhost:5173/'
 const prodEnv = 'https://kneeboard.ga'
 export const environment = devEnv
