05f1f15c7ac43064cec80cb6dabbe94f796dd2c2
JC Jollant <jc@jollant.net>
2024-08-17 17:29:30 -0700

    Showing help icon only when relevant

--- src/components/shared/ActionBar.vue
+++ src/components/shared/ActionBar.vue
@@ -27,7 +27,7 @@ function onHelp() {
 </script>
 <template>
     <div class="actionBar">
-        <Button class="floatLeft" icon="pi pi-info-circle" link @click="onHelp" title="Get help on this topic"></Button>
+        <Button class="floatLeft" v-if="help" icon="pi pi-info-circle" link @click="onHelp" title="Get help on this topic"></Button>
         <Button @click="emits('cancel')" label="Cancel" link></Button>
         <Button icon="pi pi-check" @click="emits('apply')" label="Apply"></Button>
     </div>
