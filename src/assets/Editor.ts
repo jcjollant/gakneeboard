a3d40bcfbb3f5a1c6b389ef014040066da0d9e3b
JC Jollant <jc@jollant.net>
2024-08-19 21:47:42 -0700

    New class for editor actions

---
+++ src/assets/Editor.ts
@@ -0,0 +1,9 @@
+export class EditorAction {
+    public static swapPages:string = 'swap';
+    public static resetFront:string = 'resetFront';
+    public static resetBack:string = 'resetBack';
+    public static copyFront:string = 'copyFront';
+    public static copyBack:string = 'copyBack';
+    public static pasteFront:string = 'pasteFront';
+    public static pasteBack:string = 'pasteBack';
+}
