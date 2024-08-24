89c9e831248019f24b7fca3635e497a30e865ff1
JC Jollant <jc@jollant.net>
2024-08-24 00:01:37 -0700

    Error toast helper

--- src/assets/toast.js
+++ src/assets/toast.js
@@ -7,3 +7,7 @@ export function getToastData(summary,detail,severity=toastSuccess,life=3000) {
     return { severity: severity, summary: summary, detail: detail, life: life}
 }
 
+export function getToastError(summary,detail,life=3000) {
+    return { severity: toastError, summary: summary, detail: detail, life: life}
+}
+
