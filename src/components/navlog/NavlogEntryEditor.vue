89919a4730c2f4aa23dd8ee3ec5252c45220d04e
JC Jollant <jc@jollant.net>
2024-08-25 15:10:57 -0700

    Dedicated editor for Navlog Entries

---
+++ src/components/navlog/NavlogEntryEditor.vue
@@ -0,0 +1,119 @@
+<script setup>
+import { onMounted, ref, watch } from 'vue'
+
+import Button from 'primevue/button'
+import Dialog from 'primevue/dialog'
+import FloatLabel from 'primevue/floatlabel'
+import InputText from 'primevue/inputtext'
+
+import { NavlogEntry } from './NavlogEntry'
+
+import PlaceHolder from '../shared/PlaceHolder.vue'
+
+const emits = defineEmits(['close','save'])
+
+//---------------------
+// Props management
+const props = defineProps({
+  entry: { type: Object, default: null},
+  time: { type: Number, default: 0}
+})
+
+function loadProps(newProps) {
+    // console.log('[NavlogItemEditor.loadProps]', JSON.stringify(newProps))
+    if(newProps.entry) {
+        const entry = NavlogEntry.copy(newProps.entry)
+        
+        editEntry.value = entry
+    }
+}
+
+onMounted(() => {
+    loadProps(props)
+})
+
+watch(props, () => {
+    // console.log('[NavlogItemEditor.watch]')
+    loadProps(props)
+})
+// End of props management
+//------------------------
+
+const editEntry = ref(null)
+
+
+function onSave() {
+    // console.log('[NavlogEditor.onSave]', JSON.stringify(editEntry.value))
+    const entry = editEntry.value
+    // convert all number fields to number
+    const fields = ['alt','th','ld','gs','lt', 'lf']
+    for(let f of fields) {
+        if( entry[f]) entry[f] = Number(entry[f])
+    }
+    // keep heading within 0-360
+    if( entry.th) entry.th = entry.th % 360
+
+    emits('save', entry)
+}
+
+</script>
+
+<template>
+    <Dialog modal header="NavLog Leg Editor" class="editorDialog">
+        <div v-if="editEntry">
+            <div class="editorFields">
+                <FloatLabel>
+                    <InputText id="name" class="editorName" v-model="editEntry.name" />
+                    <label for="name">Name</label>
+                </FloatLabel>
+                <FloatLabel>
+                    <InputText id="alt" v-model="editEntry.alt" />
+                    <label for="alt">Altitude</label>
+                </FloatLabel>
+                <FloatLabel title="True Heading">
+                    <InputText id="th" v-model="editEntry.th" />
+                    <label for="th">TH</label>
+                </FloatLabel>
+                <FloatLabel title="Leg Distance">
+                    <InputText id="ld" v-model="editEntry.ld" />
+                    <label for="ld">Dist.</label>
+                </FloatLabel>
+                <FloatLabel title="Ground Speed">
+                    <InputText id="gs" v-model="editEntry.gs" />
+                    <label for="gs">GS</label>
+                </FloatLabel>
+                <FloatLabel title="Leg Time in decimal format. 3:30 should be 3.5">
+                    <InputText id="lt" v-model="editEntry.lt" />
+                    <label for="lt">Time</label>
+                </FloatLabel>
+                <FloatLabel title="Leg Fuel">
+                    <InputText id="lf" v-model="editEntry.lf" />
+                    <label for="lf">Fuel</label>
+                </FloatLabel>
+            </div>
+            <div class="actionDialog gap-2">
+                <Button label="Do Not Save" @click="emits('close')" link></Button>
+                <Button label="Save" @click="onSave"></Button>
+            </div>
+         </div>
+        <PlaceHolder v-else title="Nothing to edit" />
+    </Dialog>
+
+</template>
+
+<style scoped>
+.editorFields {
+    display: grid;
+    grid-template-columns: auto auto auto auto auto auto auto;
+    gap: 5px;
+    padding-top: 1.5rem;
+    justify-content: center;
+}
+:deep(.p-inputtext.editorName ) {
+    width: 6rem;
+}
+
+:deep(.p-inputtext) {
+    width: 4rem;
+}
+</style>
