dfe42ff3313c4a38ed735455d4265987ba1602f2
JC Jollant <jc@jollant.net>
2024-08-25 15:08:23 -0700

    Easier to use toast functions

--- src/assets/toast.js
+++ src/assets/toast.js
@@ -8,6 +8,13 @@ export function getToastData(summary,detail,severity=toastSuccess,life=3000) {
 }
 
 export function getToastError(summary,detail,life=3000) {
-    return { severity: toastError, summary: summary, detail: detail, life: life}
+    return { severity: toastSevError, summary: summary, detail: detail, life: life}
 }
 
+export function emitToast(emits, title,message, life=3000) {
+    emits('toast', getToastData(title, message, toastSuccess, life))
+}
+
+export function emitToastError(emits, title,message,life=3000) {
+    emits('toast', getToastData(title, message, toastError, life))
+}
