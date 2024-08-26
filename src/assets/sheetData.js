bc9b0db6e58463fb115c36a3a6f62ebe371257ca
JC Jollant <jc@jollant.net>
2024-08-26 00:39:56 -0700

    New demo sheet with navlog

--- src/assets/sheetData.js
+++ src/assets/sheetData.js
@@ -19,8 +19,9 @@ export class PageType {
 
 
 export const sheetNameDemo = 'default-demo'
-export const sheetNameDemoTiles = 'default-demo-tiles'
 export const sheetNameDemoChecklist = 'default-demo-checklist'
+export const sheetNameDemoNavlog = 'default-demo-navlog'
+export const sheetNameDemoTiles = 'default-demo-tiles'
 export const sheetNameNew = 'default-new-sheet'
 export const sheetNameReset = 'default-reset'
 const activeSheetLocal = 'sheet'
@@ -168,21 +169,68 @@ const pageDemoChecklist2 = {
     ]
   }
 }
+
 export const sheetDemoChecklist = {
   name:'Checklist Demo',
   data:[pageDemoChecklist1,pageDemoChecklist2]
 }
 
-export function describePage(sheet, pageNumber) {
+const pageDemoNavlog0 = {
+  type: PageType.navLog,
+  data: {
+    "from":"KRNT",
+    "to":"KELN",
+    "tt":52.68333333333334,
+    "td":77.80000000000001,
+    "entries":[
+        {"name":"KRNT","alt":32,"th":144,"ch":129,"ld":4.2,"lt":3.5,"lf":2.4,"att":"+","gs":71,"fr":53},
+        {"name":"TOC25","alt":2500,"th":143,"ch":128,"ld":2.9,"lt":1.65,"lf":0.2,"gs":106,"fr":50.6},
+        {"name":"Lk Youngs Zebku","alt":2500,"th":90,"ch":75,"ld":4,"lt":3.5,"lf":0.7,"att":"+","gs":68,"fr":50.4},
+        {"name":"TOC45","alt":4500,"th":90,"ch":75,"ld":1,"lt":0.6166666666666667,"lf":0.1,"gs":101,"fr":49.699999999999996},
+        {"name":"Clear B Sh50","alt":4500,"th":90,"ch":75,"ld":2.2,"lt":2,"lf":0.4,"att":"+","gs":67,"fr":49.599999999999994},
+        {"name":"TOC55","alt":5500,"th":90,"ch":75,"ld":4.5,"lt":2.8,"lf":0.4,"gs":96,"fr":49.199999999999996},
+        {"name":"Clear B Sh60","alt":5500,"th":90,"ch":75,"ld":4.4,"lt":4,"lf":0.8,"att":"+","gs":66,"fr":48.8},
+        {"name":"TOC75","alt":7500,"th":90,"ch":75,"ld":7.6,"lt":4.9,"lf":0.6,"gs":94,"fr":48},
+        {"name":"Bandera 4W0","alt":7500,"th":90,"ch":75,"ld":28.8,"lt":18.25,"lf":2.4,"gs":95,"fr":47.4},
+        {"name":"TOD","alt":7500,"th":81,"ch":66,"ld":18.2,"lt":11.466666666666667,"lf":1.5,"att":"-","gs":95,"fr":45},
+        {"name":"KELN","alt":1763.2,"ld":0,"lt":0,"lf":0}
+    ],
+    "ff":53,
+    "ft":43.5,
+    "mv":-15,
+    "md":0
+  }    
+}
+
+const pageDemoNavlog1 = {
+  type: PageType.tiles,
+  data:[
+    {"id":0,"name":"airport","data":{"code":"krnt","rwy":"16-34"}},
+    {"id":1,"name":"atis","data":{"mode":"compact"}},
+    {"id":2,"name":"radios","data":[{"mhz":"122.9","name":"CTAF"},{"mhz":"135.275","name":"Stamp. Pass"},{"mhz":"117.9","name":"ELN VOR"},{"mhz":"123","name":"KELN CTAF"},{"mhz":"116.8","name":"SEA VORTAC"},{"mhz":"126.95","name":"KRNT ATIS"},{"mhz":"124.7","name":"KRNT TWR"}]},
+    {"id":3,"name":"notes","data":{}},
+    {"id":4,"name":"airport","data":{"code":"KELN","rwy":"11-29","rwyOrientation":"magnetic","corners":["weather","twr","field","tpa"]}},
+    {"id":5,"name":"fuel","data":{}}
+  ]
+}
+
+export const sheetDemoNavlog = {
+  name: 'Navlog Demo',
+  data: [pageDemoNavlog0,pageDemoNavlog1]
+}
+
+export function describePage(sheet, pageNumber, maxLength=undefined) {
   if(!sheet) return "empty";
-  if(!sheet.data || sheet.data.length < 2) return '?'
+
+  let output = '?'
+  if(!sheet.data || sheet.data.length < 2) return output
   const page = sheet.data[pageNumber]
   // do we have a page and a type yet?
-  if(!page || !page.type) return '?'
+  if(!page || !page.type) return output
 
   try {
     if(page.type == PageType.tiles) {
-      let output = "[Tiles] "
+      output = "[Tiles] "
       const tiles = page.data.map( t => {
         if(t.name=='airport') {
           return "Airport(" + t.data.code.toUpperCase() + ")"
@@ -190,26 +238,28 @@ export function describePage(sheet, pageNumber) {
           return t.name[0].toUpperCase() + t.name.substring(1)
         }
       })
-      return output + tiles.join(',');
+      output += tiles.join(',');
     } else if(page.type == PageType.checklist) {
-      let output = "[Checklist] " + page.data.name
+      output = "[Checklist] " + page.data.name
       // build a list of sections within that list
       const sections = page.data.items.filter(i => 's' in i).map(i => i.s);
       if(sections.length) {
-        return output + " : " + sections.join(' / ')
+        output += " : " + sections.join(' / ')
       }
-      return output
     } else if(page.type == PageType.selection) {
-      return "[Selection]"
+      output = "[Selection]"
     } else if(page.type == PageType.cover ) {
-      return "[Cover] " + page.data.title;
+      output =  "[Cover] " + page.data.title;
     } else if(page.type == PageType.navLog) {
-      return '[NavLog]'
+      output =  '[NavLog] ' + page.data.from + " to " + page.data.to + " (" + page.data.entries.length + " checkpoints)"
     }
   } catch(e) {
     console.log('[sheetData.describePage] error', e)
   }
-  return "?"
+  if(maxLength && output.length > maxLength) {
+    output = output.substring(0, maxLength) + '...'
+  }
+  return output;
 }
 
 /**
@@ -241,6 +291,10 @@ export function getSheetBlank() {
 export function getSheetDemo() {
   return duplicate(sheetDemo)
 }  
+
+export function getSheetDemoNavlog() {
+  return duplicate(sheetDemoNavlog)
+}
 
 /**
  * @returns a copy of demo sheet data 
