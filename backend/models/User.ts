cb04eb52b652a510a6c4a876b76752b55c2ee136
JC Jollant <jc@jollant.net>
2024-08-28 22:34:38 -0700

    new constructor, fromJson

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -2,26 +2,23 @@ import { createHash } from 'crypto'
 
 export class User {
     id:number;
-    source:string;
-    email:string;
     sha256:string;
     name:string;
+    source:string; // 'google'
+    email:string;
     maxTemplates:number;
 
     public static defaultMaxTemplates:number = 5;
 
-    constructor( source:string, email:string, maxTemplates:number|undefined=undefined) {
-        this.id = 0;
-        this.source = source;
-        this.email = email;
-        const user = {
-            source: source,
-            email: email,
-        }
-        this.sha256 = User.createSha256(user)
-        this.name = ""
-        this.maxTemplates = maxTemplates ? maxTemplates : User.defaultMaxTemplates;
-    };
+    constructor( id:number, sha256:string) {
+        this.id = id;
+        this.sha256 = sha256;
+        this.name = '';
+        this.source = '';
+        this.email = '';
+        this.maxTemplates = User.defaultMaxTemplates;
+    }
+
 
     // public getSha256():string {
     //     return this.sha256
@@ -31,6 +28,30 @@ export class User {
         return createHash('sha256').update(JSON.stringify(input)).digest('hex')
     }
  
+    // creates a user from it's data representation
+    public static fromJson(id:number, sha256:string, rawData:string):User {
+        const user = new User(id, sha256)
+        const data = JSON.parse(rawData)
+        if(data.source) user.setSource(data.source)
+        if(data.email) user.setEmail(data.email)
+        if(data.name) user.setName(data.name)
+        if(data.maxTemplates) user.setMaxTemplates(data.maxTemplates)
+
+        return user
+    }
+
+    public setEmail(email:string) {
+        this.email = email;
+    }
+
+    public setSource(source:string) {
+        this.source = source;
+    }
+
+    public setMaxTemplates( newMax:number) {
+        this.maxTemplates = newMax;
+    }
+
     public setName( newName:string) {
         this.name = newName;
     }    
