c528828a9d0a8eebb2b04621967e1a189fb4c153
JC Jollant <jc@jollant.net>
2024-08-30 14:53:04 -0700

    Fuel Reserve and altitudes

--- src/components/navlog/NavlogEdit.vue
+++ src/components/navlog/NavlogEdit.vue
@@ -3,7 +3,7 @@ import { onMounted, ref, watch } from 'vue'
 
 import { formatAltitude } from '../../assets/format'
 import { Formatter } from '../../assets/Formatter'
-import { emitToast, emitToastError } from '../../assets/toast'
+import { emitToast, emitToastError, emitToastWarning } from '../../assets/toast'
 import { Navlog } from './Navlog'
 import { NavlogEntry } from './NavlogEntry'
 import { EditorItem } from './EditorItem'
@@ -22,6 +22,7 @@ const emits = defineEmits(['apply','cancel','toast'])
 
 const activeEntry = ref(null)
 const activeTime = ref(0)
+const altitudes = ref('')
 const codeFrom = ref(null)
 const codeTo = ref(null)
 const confirm = useConfirm()
@@ -29,6 +30,7 @@ const initialFuel = ref(null)
 const items = ref([])
 const magneticVariation = ref(null)
 const magneticDeviation = ref(0)
+const reserveFuel = ref(null)
 const showEditor = ref(false)
 const showLeg = ref(true)
 const totalDistance = ref(0)
@@ -110,8 +112,9 @@ function onAirportToInvalid() {
 
 function onApply() {
     // Create a new NavLog from entries
-    const newNavLog = new Navlog(codeFrom, codeTo)
+    const newNavLog = new Navlog(codeFrom.value, codeTo.value)
     newNavLog.setFuelFrom(initialFuel.value)
+    newNavLog.setFuelReserve(reserveFuel.value)
     newNavLog.setMagneticVariation(magneticVariation.value)
     newNavLog.setMagneticDeviation(magneticDeviation.value)
     newNavLog.setTotalDistance(totalDistance.value)
@@ -120,7 +123,29 @@ function onApply() {
     // copy Editor Items entries into navlog
     newNavLog.setEntries( items.value.map( i => NavlogEntry.copy(i.entry)))
 
+    console.log('[NavlogEdit.onApply]', JSON.stringify(newNavLog))
+    if(newNavLog.getFuelFrom() <= 0) {
+        emitToastError( emits, 'Bingo Fuel', 'Like aircrafts, navlogs need initial fuel')
+        return;
+    }
+
+    if(newNavLog.getFuelReserve() < 0) {
+        emitToastError( emits, 'Invalid Reserve', 'Fuel reserve cannot be negative')
+        return;
+    }
+    if(newNavLog.getFuelReserve() == 0) {
+        emitToastWarning(emits, 'No Reserve', 'Fuel reserve is set to 0. Remind me to take the next flight.')
+    }
     emits('apply', newNavLog)
+}
+
+function onCheat() {
+    codeFrom.value = 'KRNT'
+    elevFrom = 32
+    codeTo.value = 'KELN'
+    elevTo = 1763
+    altitudes.value = "2500 2500 4500 4500 7500 7500 5500"
+    doCreate()
 }
 
 /**
@@ -128,7 +153,7 @@ function onApply() {
  */
 function onCreate() {
     if(!codeFrom.value || !codeTo.value) {
-        emitToastError( emits, 'Invalid Airports', 'We need two airports to create the log')
+        emitToastError( emits, 'Airports Required', 'We need two airports to create the log')
         return
     }
 
@@ -148,12 +173,38 @@ function onCreate() {
         doCreate()
     }
 }
+
+// let's create a base for this log
 function doCreate() {
     const newList = []
     
+
+    const altList = altitudes.value.split(' ').map( a => Number(a)).filter( a => !isNaN(a))
+    console.log('[NavlogEdit.doCreate]', JSON.stringify(altList))
+
     newList.push( EditorItem.vanilla( codeFrom.value, elevFrom, false, true))
-    newList.push( EditorItem.vanilla('TOC', elevFrom + 2000))
-    newList.push( EditorItem.vanilla('TOD', elevTo + 2000))
+    let previousAlt = elevFrom
+    const altItems = altList.map( (alt,index) => {
+        const level = Math.trunc(alt / 100)
+        const previousAlt = index == 0 ? elevFrom : altList[index-1]
+        const lastLeg = index == altList.length - 1
+        const nextAlt = lastLeg ? elevTo : altList[index+1]
+        if( alt > previousAlt) {
+            return EditorItem.vanilla('TOC ' + level, alt)
+            // newList.push( EditorItem.vanilla('@' + level, alt))
+        } else if(alt == previousAlt) {
+            if( alt > nextAlt) {
+                return EditorItem.vanilla('TOD ' + Math.trunc(nextAlt / 100), alt)
+            }
+            return  EditorItem.vanilla('CheckPt ' + index, alt)
+        } else { // desent
+            if(lastLeg)
+                return EditorItem.vanilla('TOD ' + codeTo.value, alt)
+            return EditorItem.vanilla('TOD ' + level, alt)
+        }
+        previousAlt = alt;
+    })
+    newList.push.apply(newList, altItems)
     newList.push( EditorItem.vanilla( codeTo.value, elevTo, false, false))
     // console.log('[NavlogEdit.onCreate]', JSON.stringify(newNL))
     items.value = newList;
@@ -228,11 +279,31 @@ function onMagneticChange() {
     <NavlogEntryEditor v-model:visible="showEditor" :entry="activeEntry" :time="activeTime" :showLeg="showLeg" :magVar="compass(magneticVariation)" :magDev="compass(magneticDeviation)"
         @close="onEntryEditorClose" @save="onEntryEditorSave" />
     <div class="variables">
-        <AirportInput :code="codeFrom" :auto="true" label="From" class="airportFrom" @valid="onAirportFromValid" @invalid="onAirportFromInvalid" />
-        <AirportInput :code="codeTo" :auto="true" label="To" class="airportTo" @valid="onAirportToValid" @invalid="onAirportToInvalid"/>
-        <InputGroup class="initialFuel" title="Fuel before Taxi">
-            <InputGroupAddon>Initial Fuel</InputGroupAddon>
-            <InputText v-model="initialFuel"/>
+        <AirportInput :code="codeFrom" :auto="true" label="From" class="varAirportFrom" @valid="onAirportFromValid" @invalid="onAirportFromInvalid" />
+        <AirportInput :code="codeTo" :auto="true" label="To" class="varAirportTo" @valid="onAirportToValid" @invalid="onAirportToInvalid"/>
+        <InputGroup class="varAltitudes" title="List of Enroute Altitudes (Space separated)">
+            <InputGroupAddon>Altitudes</InputGroupAddon>
+            <InputText v-model="altitudes" placeholder="Ex: 2500 4500 7500 ..."/>
+        </InputGroup>
+        <Button label="Create" class="varCreateBtn" @click="onCreate" :disabled="(!codeFrom||!codeTo)"></Button>
+        <div class="varGroupFuel varGroup">Fuel Tank</div>
+        <!-- <div class="varGroupFuelFlows varGroup">Fuel Flow</div> -->
+        <div class="varGroupDescent varGroup">Descent</div>
+        <InputGroup class="varInitialFuel" title="Initial Fuel. Bake your taxi/runup into this number OR the first leg">
+            <InputGroupAddon>Init.</InputGroupAddon>
+            <InputText v-model="initialFuel" placeholder="Gal"/>
+        </InputGroup>
+        <InputGroup class="varReserveFuel" title="Minium fuel onboard (consider day/night)">
+            <InputGroupAddon>Res.</InputGroupAddon>
+            <InputText v-model="reserveFuel" placeholder="Gal"/>
+        </InputGroup>
+        <InputGroup class="varDescentFpm" title="Descent Rate (Feet per minute)">
+            <InputGroupAddon>FPM</InputGroupAddon>
+            <InputText :disabled="true"/>
+        </InputGroup>
+        <InputGroup class="varDescentGph" title="Descent Fuel Flow (Gallons per hour)">
+            <InputGroupAddon>GPH</InputGroupAddon>
+            <InputText :disabled="true"/>
         </InputGroup>
         <!-- <InputGroup title="Magnetic Variation for the flight">
             <InputGroupAddon>MagVar</InputGroupAddon>
@@ -242,11 +313,8 @@ function onMagneticChange() {
             <InputGroupAddon>MagDev</InputGroupAddon>
             <InputText v-model="magneticDeviation" @input="onMagneticChange" :disabled="true"/>
         </InputGroup> -->
-        <Button label="Create" @click="onCreate" :disabled="(!codeFrom||!codeTo)"></Button>
+        <!-- <Button label="..." class="varCheatBtn" @click="onCheat" ></Button> -->
     </div>
-    <!-- <div>
-        <Button label="Cheat" @click="onCheat"></Button>
-    </div> -->
     <div class="grids" v-if="items.length">
         <div class="checkpoints"><!-- checkpoints -->
             <div class="headers checkpointGrid bb">
@@ -311,12 +379,6 @@ function onMagneticChange() {
 .actionDelete {
     color: red;
 }
-.airportFrom {
-    grid-column: 1 / 3;
-}
-.airportTo {
-    grid-column: 3 / 5;
-}
 .checkpointGrid {
     display: grid;
     grid-template-columns: 4rem auto 3rem;
@@ -340,9 +402,6 @@ function onMagneticChange() {
 .headers {
     font-weight: bold;
     font-size: 0.8rem;
-}
-.initialFuel {
-    grid-column: 3;
 }
 .legGrid {
     display: grid;
@@ -375,6 +434,41 @@ function onMagneticChange() {
     grid-template-columns: repeat(4, minmax(0, 1fr));;
     gap: 5px;
     padding: 5px;
+}
+.varAirportFrom {
+    grid-column: 1 / span 2;
+}
+.varAirportTo {
+    grid-column: 3 / span 2;
+}
+.varAltitudes {
+    grid-column: 1 / span 3;
+}
+.varCreateBtn {
+    grid-column: 4 / span 1;
+}
+.varDescentFpm {
+    grid-column: 3;
+}
+.varGroup {
+    font-size: 0.7rem;
+    background-color: lightblue;
+}
+.varGroupFuel {
+    grid-column: 1 / span 2;
+}
+.varGroupFuelFlows {
+    grid-column: 4 ;
+}
+.varGroupDescent {
+    grid-column: 3 / span 2 ;
+    background-color: #DFD;
+}
+.varInitialFuel {
+    grid-column: 1;
+}
+.varTaxiFuel {
+
 }
 
 :deep(.p-component), :deep(.p-inputgroup-addon) {
