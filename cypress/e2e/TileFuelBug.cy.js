07de8add5c937b8f1857284aa589325ed71292fc
JC Jollant <jc@jollant.net>
2024-08-31 10:45:00 -0700

    Removed airport wait

--- cypress/e2e/TileFuelBug.cy.js
+++ cypress/e2e/TileFuelBug.cy.js
@@ -4,13 +4,6 @@ describe('Fuel Bug Tile', () => {
   it('Fuel Bug Tile', () => {
     visitAndCloseBanner()
 
-    cy.intercept({
-      method: 'GET',
-      url: 'https://ga-api-seven.vercel.app/airports/**',
-    }).as('getAirports');
-
-    cy.wait('@getAirports').its('response.statusCode').should('equal', 200)
-
     cy.get('.pageTwo > :nth-child(4) > .headerTitle > div').contains('Fuel Bug')
     // switch to edits mode
     cy.get('.pageTwo > :nth-child(4) > .headerTitle > div').click()
