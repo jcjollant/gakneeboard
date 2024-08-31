92412198f6719bc7e7f01dc046b2457c97b446a9
JC Jollant <jc@jollant.net>
2024-08-31 09:14:53 -0700

    Refeshed tests for new navloag creation

--- cypress/e2e/NavlogPage.cy.js
+++ cypress/e2e/NavlogPage.cy.js
@@ -18,21 +18,38 @@ describe('navlog Page', () => {
 
     // edit mode
     cy.get(':nth-child(1) > .headerTitle').click()
-    const expectedVariables = ['From', 'To', 'Initial Fuel', 'Create']
+    const expectedVariables = ['From', 'To', 
+      'Altitudes', 'Create',
+      'Fuel Tank', 'Descent', 
+      'Init.', 'Res.', 'FPM', 'GPH']
     for(const variable of expectedVariables) {
       cy.get('.variables').contains(variable)
     }
 
     // create a navlog
-    cy.get('.airportFrom > .p-inputgroup > .p-inputtext').type('KRNT')
-    cy.get('.airportTo > .p-inputgroup > .p-inputtext').type('KSFF')
-    cy.wait(1500)
+    cy.get('.varAirportFrom > .p-inputgroup > .p-inputtext').type('KRNT')
+    cy.get('.varAirportTo > .p-inputgroup > .p-inputtext').type('KSFF')
+    cy.get('.varAltitudes > .p-inputtext').type("2500 2500 4500 4500 7500 7500 5500")
+    cy.wait(500) // give it time to pull data
     // Click create
     cy.get('.variables > .p-button').click()
 
-    // we should see Felts field
-    cy.get('.checkpoints > :nth-child(5)').contains('KSFF')
-    cy.get('.checkpoints > :nth-child(5)').contains('1957')
+    // we should see Checkpoints and altitudes
+    const expectedItems = [ {c:'KRNT',a:32},
+      {c:'TOC 25',a:2500},
+      {c:'CheckPt 1',a:2500},
+      {c:'TOC 45',a:4500},
+      {c:'CheckPt 3',a:4500},
+      {c:'TOC 75',a:7500},
+      {c:'TOD 55',a:7500},
+      {c:'TOD KSFF',a:5500},
+      {c:'KSFF',a:1957},
+    ]
+    for(let index = 0; index < expectedItems.length; index++) {
+      const item = expectedItems[index]
+      cy.get(`:nth-child(${index+2}) > .checkpointName`).contains(item.c)
+      cy.get(`:nth-child(${index+2}) > .checkpointAlt`).contains(item.a)
+    }
     // totals should be undefined
     cy.get('.totalDistance').contains('0.0')
     cy.get('.totalTime').contains('0:00')
@@ -55,22 +72,42 @@ describe('navlog Page', () => {
 
     // enter values for the other two legs
 
-    cy.get(':nth-child(3) > .br').click()
-    cy.get('#ld').type('5')
-    cy.get('#gs').type('6')
-    cy.get('#lt').type('7:30')
-    cy.get('#lf').type('8')
-    cy.get('.actionDialog > [aria-label="Save"]').click()
-    cy.get(':nth-child(4) > .br').click()
-    cy.get('#ld').type('9')
-    cy.get('#gs').type('10')
-    cy.get('#lt').type('11.75') // twist, decimal format
-    cy.get('#lf').type('12')
-    cy.get('.actionDialog > [aria-label="Save"]').click()
+    const legData = [
+      {index: 3, ld: '5', gs: '6', lt: '7:30', lf: '8'},
+      {index: 4, ld: '9', gs: '10', lt: '11.75', lf: '12'},
+      {index: 5, ld: '9', gs: '10', lt: '11.75', lf: '5'},
+      {index: 6, ld: '9', gs: '10', lt: '11.75', lf: '5'},
+      {index: 7, ld: '9', gs: '10', lt: '11.75', lf: '5'},
+    ]
+    for(const data of legData) {
+      cy.get(`:nth-child(${data.index}) > .br`).click()
+      cy.get('#ld').type(data.ld)
+      cy.get('#gs').type(data.gs)
+      cy.get('#lt').type(data.lt)
+      cy.get('#lf').type(data.lf)
+      cy.get('.actionDialog > [aria-label="Save"]').click()
+    }
 
     // test totals
-    cy.get('.totalDistance').contains('15.0')
-    cy.get('.totalTime').contains('22:30')
-    cy.get('.totalFuel').contains('24.0')
+    cy.get('.totalDistance').contains('42.0')
+    cy.get('.totalTime').contains('57:45')
+    cy.get('.totalFuel').contains('39.0')
+
+    // Apply
+    cy.get('[aria-label="Apply"]').click()
+    cy.get('.p-toast-message-content').contains('Bingo Fuel')
+
+    // enter initial fuel and reserve
+    cy.get('.varInitialFuel > .p-inputtext').type(53)
+    cy.get('.varReserveFuel > .p-inputtext').type(30)
+    // and apply again
+    cy.get('[aria-label="Apply"]').click()
+
+    // Fuel reserve should pop
+    cy.get(':nth-child(5) > .fuel').should('not.have.class','fuelBingo')
+    cy.get(':nth-child(6) > .fuel').should('have.class','fuelBingo')
+    cy.get('.totalFuel').should('have.class', 'fuelBingo')
+
+    // Delete
   })
 })
