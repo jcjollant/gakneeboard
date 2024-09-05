43bb72a738474d4530566023e961da2ac09f7b5e
JC Jollant <jc@jollant.net>
2024-09-04 21:20:49 -0700

    New effective date

--- test/UserDao.test.ts
+++ test/UserDao.test.ts
@@ -1,7 +1,7 @@
 
 import { describe, expect, test} from '@jest/globals';
 import { UserDao } from '../backend/UserDao.ts'
-import { postgresUrl, jcUserId, jcHash, jcEmail, jcName, jcSource } from './constants.ts';
+import { postgresUrl, jcUserId, jcHash, jcEmail, jcName, jcSource, jcMaxTemplates } from './constants.ts';
 import { User } from '../backend/models/User.ts';
 
 process.env.POSTGRES_URL=postgresUrl
@@ -24,7 +24,7 @@ describe('UserDao', () => {
             expect(user?.name).toEqual(jcName)
             expect(user?.source).toEqual(jcSource)
             expect(user?.email).toEqual(jcEmail)
-            expect(user?.maxTemplates).toBe(User.defaultMaxTemplates)
+            expect(user?.maxTemplates).toBe(jcMaxTemplates)
         })
     })
 });
--- test/constants.ts
+++ test/constants.ts
@@ -17,7 +17,7 @@ export const jcEmail = 'jcjollant@gmail.com'
 export const jcSource = 'google'
 export const samplePublicationCode = '8Q'
 
-export const currentAsOf = 20240808;
+export const currentAsOf = 20240905;
 export const currentAirportModelVersion = 5
 export const krntAtcs =  [
     {mhz:119.2, useCount:6, name:'SEATTLE-TACOMA APPROACH CONTROL'},
