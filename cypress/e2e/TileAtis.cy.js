fc372498fac380aa475ab66c15590b8ff3ccbc52
JC Jollant <jc@jollant.net>
2024-09-05 22:18:19 -0700

    Checking title height

--- cypress/e2e/TileAtis.cy.js
+++ cypress/e2e/TileAtis.cy.js
@@ -15,10 +15,17 @@ describe('ATIS Tile', () => {
     cy.get('.sky').contains('Sky')
     cy.get('.temperature').contains('Temp')
     cy.get('.altimeter').contains('Alt')
-    // Check ATIS has all fields in compact mode
+
+    // Edit mode
     cy.get('.pageOne > :nth-child(5) > .headerTitle').click()
+    // check header size
+    const expectedHeight = 25
+    cy.get('.headerTitle').invoke('outerHeight').should('be.equal', expectedHeight)
+    cy.get('.headerTitle > .p-button').invoke('outerHeight').should('be.equal', expectedHeight - 2)
+
     cy.get('[aria-label="Full Size"]').contains('Full Size')
     cy.get('[aria-label="Compact (x4)"]').contains('Compact (x4)')
+    // Check ATIS has all fields in compact mode
     cy.get('[aria-label="Compact (x4)"]').click()
     for(let index=1; index <=4; index++) {
       cy.get(`:nth-child(${index}) > .info`).contains('Info')
