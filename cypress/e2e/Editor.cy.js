ceac8c38959bba66149d067aebbb6698cbdec238
JC <jc@jollant.net>
2024-09-06 10:20:07 -0700

    More rigorous test of editor page deletion

--- cypress/e2e/Editor.cy.js
+++ cypress/e2e/Editor.cy.js
@@ -24,16 +24,16 @@ describe('Editor', () => {
     cy.get('.editorMask').invoke('outerHeight').should('be.equal', expectedHeight)
 
     // Check we have action buttons
-    cy.get(':nth-child(1) > [aria-label="Reset"]')
+    cy.get(':nth-child(1) > [aria-label="Replace"]')
     cy.get(':nth-child(1) > [aria-label="Copy"]')
     cy.get(':nth-child(1) > [aria-label="Paste"]')
     cy.get('.middle > .p-button').should('have.class','p-button-icon-only')
-    cy.get(':nth-child(3) > [aria-label="Reset"]')
+    cy.get(':nth-child(3) > [aria-label="Replace"]')
     cy.get(':nth-child(3) > [aria-label="Copy"]')
     cy.get(':nth-child(3) > [aria-label="Paste"]')
 
     // reset left
-    cy.get(':nth-child(1) > [aria-label="Reset"]').click()
+    cy.get(':nth-child(1) > [aria-label="Replace"]').click()
     cy.get('.pageOne').contains('Page Selection')
     cy.get('.pageTwo').contains('Page Selection').should('not.exist')
 
@@ -46,7 +46,7 @@ describe('Editor', () => {
     cy.get('#btnEditor').click()
 
     // reset right
-    cy.get(':nth-child(3) > [aria-label="Reset"]').click()
+    cy.get(':nth-child(3) > [aria-label="Replace"]').click()
     cy.get('.pageOne').contains('Page Selection').should('not.exist')
     cy.get('.pageTwo').contains('Page Selection')
 
@@ -80,11 +80,24 @@ describe('Editor', () => {
 
     // Add a page
     cy.get('[title="Add 2 Pages"]').click()
+
     // select new page
     cy.get('[aria-label="3 | 4"]').click()
+    cy.get('[aria-label="1 | 2"]').should('not.have.class', 'active')
+    cy.get('[aria-label="3 | 4"]').should('have.class', 'active')
+    // Check page content
     cy.get('.pageOne').contains('Page Selection')
     cy.get('.pageTwo').contains('Page Selection')
 
+    // delete new page
+    cy.get('[title="Delete active sheet"]').click()
+    cy.get('.p-confirm-dialog-message').contains('3 and 4')
+    cy.get('.p-confirm-dialog-accept').click()
+    cy.get('[aria-label="1 | 2"]').should('have.class', 'active')
+
+    // delete last page should not be possible
+    cy.get('[title="Delete active sheet"]').click()
+    cy.get('[aria-label="1 | 2"]')
   })
   
   it('Page Selection', () => {
