d9b7a2f5d6301b15d294dd3bcb4fa2cfc8d2faac
JC Jollant <jc@jollant.net>
2024-09-08 21:32:34 -0700

    Moved to shared, may be useless

---
+++ src/components/shared/List.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { onMounted, ref } from 'vue';
+import Header from './Header.vue';
+
+const emits = defineEmits(['replace'])
+
+const props = defineProps({
+    title: { type: String, default: 'List'},
+    items: { type: Object, default: []}
+})
+const items=ref([])
+
+function replaceMe() {
+    emits('replace')
+}
+
+onMounted(()=> {
+    items.value = props.items;
+    // console.log(items.value)
+})
+</script>
+
+<template>
+    <div class="widget">
+        <Header :title="title" @click="replaceMe" />
+        <div class="content list">
+            <div v-for="item in items">{{ item }}</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.list {
+    display: grid;
+}
+</style>
