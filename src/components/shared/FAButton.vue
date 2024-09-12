6d5c7d014e28c48c9b052cd64e1ea3afd240fce2
JC Jollant <jc@jollant.net>
2024-09-11 19:57:37 -0700

    New Component for Font awesome icons

---
+++ src/components/shared/FAButton.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+
+const props = defineProps({
+    label: { type: String, default: '?'},
+    icon: { type: String, default: null},
+    disabled: { type: Boolean, default: true},
+})
+
+</script>
+
+<template>
+    <div class="fabutton" :aria-label="label">
+        <font-awesome-icon v-if="icon" :icon="['fas',icon]" />
+        <div>{{ label }}</div>
+    </div>
+</template>
+
+<style scoped>
+.fabutton {
+    display: flex;
+    cursor: pointer;
+    color: white;
+    background-color: #2196F3;
+    border-radius: 3px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    font-size: 14px;
+    font-family: var(--font-family);
+    /* font-weight: 100; */
+    transition-duration: 0.2s;
+    line-height: 2.5rem;
+}
+.fabutton:hover {
+    background: #0D89EC;
+}
+</style>
