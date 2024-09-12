b7cd511439458b6f6709645837390da9294bdd75
JC Jollant <jc@jollant.net>
2024-09-11 19:58:21 -0700

    Using new FAButton component

--- src/components/SelectionPage.vue
+++ src/components/SelectionPage.vue
@@ -3,25 +3,23 @@ import { ref } from 'vue'
 
 import { PageType } from '../assets/Templates'
 
-import Header from '../components/shared/Header.vue'
-
-import Button from 'primevue/button'
+import Header from './shared/Header.vue'
+import FAButton from './shared/FAButton.vue'
 
 const emits = defineEmits(['replace'])
 
 
 const knownPages = ref([
-    {'name':'Tiles','type':PageType.tiles, 'tooltip':'A 2x3 grid of customizable tiles like Airport, ATIS, Radios, ...'},
-    {'name':'Checklist','type':PageType.checklist, 'tooltip':'A checklist you can customize'},
-    {'name':'Cover','type':PageType.cover, 'tooltip':'A cover page for your stylish templates'},
-    {'name':'NavLog','type':PageType.navLog, 'tooltip':'A Navigation Log with checkpoints and headings'},
+    {'name':'Tiles','type':PageType.tiles, icon:'border-all', tooltip:'A 2x3 grid of customizable tiles like Airport, ATIS, Radios, ...'},
+    {'name':'Checklist','type':PageType.checklist, icon:'list-check', tooltip:'A checklist you can customize'},
+    {'name':'Cover','type':PageType.cover, icon:'image', tooltip:'A cover page for your stylish templates'},
+    {'name':'NavLog','type':PageType.navLog, icon:'route', tooltip:'A Navigation Log with checkpoints and headings'},
 ])
 
 function replacePage(type) {
     // console.log('[SelectionPage.replacePage]', type)
     emits('replace',type)
 }
-
 
 </script>
 
@@ -30,8 +28,10 @@ function replacePage(type) {
         <Header :title="'Page Selection'" :clickable="false"></Header>
         <!-- <div class="widgetTitle">Tile Selection</div> -->
         <div class="list">
-            <Button v-for="page in knownPages" :label="page.name" :title="page.tooltip"
-                @click="replacePage(page.type)"></Button>
+            <FAButton v-for="page in knownPages" :label="page.name" :title="page.tooltip" :icon="page.icon"
+                @click="replacePage(page.type)"/>
+            <!-- <Button v-for="page in knownPages" :label="page.name" :title="page.tooltip"
+                @click="replacePage(page.type)"></Button> -->
         </div>
     </div>
 </template>
