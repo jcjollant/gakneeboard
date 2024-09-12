924cde7f69304934f53a3b03d4a0b065efd202a5
JC Jollant <jc@jollant.net>
2024-09-11 19:58:36 -0700

    Using font awesome

--- src/main.js
+++ src/main.js
@@ -6,11 +6,21 @@ import 'primeicons/primeicons.css'
 import 'primevue/resources/themes/saga-blue/theme.css'
 import ConfirmationService from 'primevue/confirmationservice'
 import ToastService from 'primevue/toastservice'
-import GoogleSignInPluggin from 'vue3-google-signin';
+import GoogleSignInPluggin from 'vue3-google-signin'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faPlaneDeparture, faCloudSunRain, faListCheck, faPlaneCircleCheck, faGasPump,
+    faRoute, faPenToSquare, faHeadset, faSun, 
+    faBorderAll,
+    faImage} from '@fortawesome/free-solid-svg-icons'
+
+library.add(faPlaneDeparture, faCloudSunRain, faListCheck, faPlaneCircleCheck, faGasPump,
+    faRoute, faPenToSquare, faHeadset, faSun, faBorderAll, faImage)
 
 createApp(App)
     .use(PrimeView)
     .use(ConfirmationService)
     .use(ToastService)
     .use(GoogleSignInPluggin, {clientId: '864395393673-li5elss3gtbhipp6pdjs1pbgbl0866si.apps.googleusercontent.com'})
+    .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
