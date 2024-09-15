55fb8149d4175e1462ec1cb945e966df1aa63d71
JC Jollant <jc@jollant.net>
2024-09-14 17:30:31 -0700

    New replace behavior and fixed text overflow

--- src/components/shared/Header.vue
+++ src/components/shared/Header.vue
@@ -42,57 +42,62 @@ watch( props, async() => {
 
 
 <template>
-    <div class="headerTitle" :class="{ clickable: clickable, left: left, page: page, stealth: stealth }">
-        <div>{{ title }}</div>
-        <Button class="replaceButton" :class="{ 'hidden' : !replace }" icon="pi pi-eject" title="Replace Tile" link
+    <div class="headerTitle" :class="{ clickable: clickable, left: left, page: page }">
+        <div class="titleText" :class="{ stealth: stealth}">{{ title }}</div>
+        <Button v-if="replace" class="replaceButton hidden" icon="pi pi-eject" title="Replace Tile" link
             @click="emits('replace')"></Button>
     </div>
 </template>
 
 <style scoped>
-    .headerTitle {
-        text-align: center;
-        padding: 0 5px;
-        border-bottom: 1px dashed darkgrey;
-        position: relative;
-        overflow: hidden;
-        height: 25px;
-        line-height: 25px;
-    }
-    .clickable:hover .replaceButton {
-        display: inline-flex;
-    }
-    .clickable:hover {
-        color: darkblue;
-        font-weight: bolder;
-    }
-    .page {
-        font-size: 1.2rem;
-        font-weight: bolder;
-    }
-    .replaceButton {
-        position:absolute;
-        font-size: 15px;
-        right: 1px;
-        top:1px;
-        width: 23px;
-        height: 23px;
-        /* padding: 5px; */
-        margin: 0;
-        /* background-color: darkred; */
-        /* color: green; */
-        border: 0
-    }
-    .replaceButton:hover {
-        background-color: darkblue;
-        color:white;
-    }
-    .left {
-        text-align: left;
-        padding-left: 20px;
-    }
-    .stealth {
-        opacity: 0.3;
-    }
+.headerTitle {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.1rem;
+    /* padding: 0 5px; */
+    border-bottom: 1px dashed darkgrey;
+    position: relative;
+    /* line-height: 25px; */
+}
+.clickable:hover .replaceButton {
+    display: inline-flex;
+}
+.clickable:hover {
+    color: darkblue;
+    font-weight: bolder;
+}
+.page {
+    /* font-size: 1.2rem; */
+    font-weight: bolder;
+}
+.replaceButton {
+    position:absolute;
+    font-size: 15px;
+    right: 1px;
+    top:1px;
+    width: 23px;
+    height: 23px;
+    /* padding: 5px; */
+    margin: 0;
+    /* background-color: darkred; */
+    /* color: green; */
+    border: 0
+}
+.replaceButton:hover {
+    background-color: darkblue;
+    color:white;
+}
+.left {
+    justify-content: flex-start;
+    padding-left: 20px;
+}
+.stealth {
+    opacity: 0.3;
+}
 
+.titleText {
+    height: 25px;
+    overflow: hidden;
+}
 </style>
