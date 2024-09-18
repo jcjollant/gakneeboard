3769bca3850a7a89448ec09ca869e64b93a4b79e
JC Jollant <jc@jollant.net>
2024-09-18 00:31:02 -0700

    Aded item class

--- src/assets/EditorItem.ts
+++ src/assets/EditorItem.ts
@@ -4,24 +4,35 @@ export class EditorItem {
     entry:NavlogEntry
     canDelete:boolean
     canAdd:boolean
+    attitudeClass:string
 
-    constructor(entry:NavlogEntry, canDelete:boolean=true, canAdd:boolean=true) {
+    constructor(entry:NavlogEntry, canDelete:boolean=true, canAdd:boolean=true, attClass:string='') {
         this.entry = entry
         this.canDelete = canDelete
         this.canAdd = canAdd
+        this.attitudeClass = attClass
     }
 
     // create a boundary item with its airport code, elevation and cannot be deleted
     public static boundary(airport:any, canAdd:boolean=true) {
-        return new EditorItem(new NavlogEntry(airport.code, airport.elev), false, canAdd)
+        return new EditorItem(new NavlogEntry(airport.code, airport.elev), false, canAdd, '')
+    }
+
+    // first entry cannot be deleted, can be amended
+    public static first(name:string, elevation:number, attClass:string='') {
+        return new EditorItem(new NavlogEntry(name, elevation), false, true, attClass)
     }
 
     // a vanilla item only has a name and can be deleted
-    public static vanilla(name:string,elevation:number|undefined=undefined,canDelete:boolean=true,canAdd:boolean=true) {
-        return new EditorItem(new NavlogEntry(name,elevation), canDelete, canAdd)
+    public static leg(name:string,elevation:number|undefined=undefined,attClass:string='') {
+        return new EditorItem(new NavlogEntry(name,elevation), true, true, attClass)
     }
 
-    public static naked(name:string) {
-        return new EditorItem(new NavlogEntry(name), true, true)
+    public static last(name:string, elevation:number) {
+        return new EditorItem(new NavlogEntry(name, elevation), false, false)
+    }
+
+    public static naked(name:string, attClass:string='') {
+        return new EditorItem(new NavlogEntry(name), true, true, attClass)
     }
 }
