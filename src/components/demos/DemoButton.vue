25c1b4acff9efbd3803c4f718807915d5190a4a9
JC Jollant <jc@jollant.net>
2024-09-22 21:43:36 -0700

    New Demo selection window with custom buttons

---
+++ src/components/demos/DemoButton.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+const props = defineProps({
+  template: { type: Object, default: null},
+  icon: {type: String, default: 'plane-departure'}
+})
+
+function getName(template) {
+    if(!template || !template.name) return '?'
+    if( !template.name.endsWith(' Demo')) return template.name
+    return template.name.substring( 0, template.name.length - 5)
+}
+
+</script>
+<template>
+    <div class="demoButton" :title="template.desc">
+        <font-awesome-icon class="icon" :icon="['fas',icon]" />
+        <div>{{ getName(template) }}</div>
+    </div>
+</template>
+<style scoped>
+.demoButton {
+    cursor: pointer;
+    display: flex;
+    flex-flow: column;
+    gap: 5px;
+    align-items: center;
+    color: white;
+    background-color: #2196f3;
+    border-radius: 3px;
+    padding: 10px 15px;
+}
+.icon {
+    font-size: xx-large;
+}
+</style>
---
+++ src/components/demos/DemoSelection.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { getTemplateDemo, getTemplateDemoChecklist, getTemplateDemoNavlog, getTemplateDemoTiles } from '../../assets/sheetData';
+
+import Dialog from 'primevue/dialog'
+
+import DemoButton from './DemoButton.vue';
+
+const emits = defineEmits(['load'])
+
+</script>
+<template>
+  <Dialog modal header="GA Kneeboard Demos">
+    <div class="demoGrid">
+        <DemoButton :template="getTemplateDemo()" icon="pen-to-square" class="defaultDemo"
+          @click="emits('load', getTemplateDemo())"></DemoButton>
+        <DemoButton :template="getTemplateDemoChecklist()" icon="list-check"
+          @click="emits('load', getTemplateDemoChecklist())"></DemoButton>
+        <DemoButton :template="getTemplateDemoTiles()" icon="border-all"
+          @click="emits('load', getTemplateDemoTiles())"></DemoButton>
+        <DemoButton :template="getTemplateDemoNavlog()" icon="route"
+          @click="emits('load', getTemplateDemoNavlog())"></DemoButton>
+    </div>
+  </Dialog>
+</template>
+<style scoped>
+.demoGrid {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 10px;
+}
+</style>
