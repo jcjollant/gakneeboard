ff20a73ffa8a34da312e72abf807750a9f74d826
JC Jollant <jc@jollant.net>
2024-09-22 17:17:23 -0700

    Added description

--- backend/models/Template.ts
+++ backend/models/Template.ts
@@ -6,10 +6,12 @@ export class Template {
     data:any;
     publish:boolean;
     code:string|undefined;
+    desc:string|undefined;
 
-    constructor(id:number, name:string, dataParam:any, publish:boolean=false, code:string|undefined=undefined) {
+    constructor(id:number, name:string, dataParam:any, description:string|undefined=undefined, publish:boolean=false, code:string|undefined=undefined) {
         this.id = id;
         this.name = name;
+        this.desc = description ? description : undefined;
         // whatever is passed, we want data to be an object
         if( typeof dataParam == 'string') {
             this.data = JSON.parse(dataParam);
