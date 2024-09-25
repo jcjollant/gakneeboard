2349481beaa19764b8099e72b91941c213b79648
JC Jollant <jc@jollant.net>
2024-09-24 21:53:33 -0700

    new prop to capture activation status

--- backend/models/Publication.ts
+++ backend/models/Publication.ts
@@ -2,9 +2,11 @@ export class Publication {
     id: number;
     code:string|undefined;
     templateId:number|undefined;
-    constructor(id:number, code:string|undefined, templateId:number|undefined) {
+    active:boolean;
+    constructor(id:number, code:string|undefined, templateId:number|undefined, active:boolean|undefined) {
         this.id = id;
         this.code = code;
         this.templateId = templateId;
+        this.active = active ?? true; // active if undefined
     }
 }
