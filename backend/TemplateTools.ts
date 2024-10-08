a1ba97cde8c43c69ddf6e9194246549183d11dc3
JC Jollant <jc@jollant.net>
2024-10-07 18:16:15 -0700

    New Class

---
+++ backend/TemplateTools.ts
@@ -0,0 +1,8 @@
+export class PageType {
+  static selection = 'selection'
+  static tiles = 'tiles'
+  static checklist = 'checklist'
+  static cover = 'cover'
+  static navLog = 'navlog'
+  static notes = 'notes'
+}
