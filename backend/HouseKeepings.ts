77b075e8d2e020cde96b18e4dd52c677130a75e8
JC Jollant <jc@jollant.net>
2024-10-07 18:23:02 -0700

    New Class

---
+++ backend/HouseKeepings.ts
@@ -0,0 +1,10 @@
+import { AdipDao } from "./AdipDao"
+import { Metric } from "./Metrics"
+
+export class HouseKeeping {
+    public static async perform():Promise<Metric> {
+        return new Promise((resolve, reject) => {
+            AdipDao.cleanUpStaleData().then( (count) => resolve(new Metric('Adip Clean Up', count)))
+        })
+    }
+}
