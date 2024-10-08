fb3baafd3fc69d21bc23b61f667783ac494e35fd
JC Jollant <jc@jollant.net>
2024-10-07 18:10:02 -0700

    Method to clean up stale data

--- backend/AdipDao.ts
+++ backend/AdipDao.ts
@@ -1,13 +1,19 @@
 import { sql } from  "@vercel/postgres";
+import { Adip } from "./Adip";
 
 export class AdipDao {
-    
+
+    public static async cleanUpStaleData():Promise<number> {
+        const result = await sql`UPDATE adip SET data=NULL WHERE code in (SELECT DISTINCT code FROM adip WHERE create_time > ${Adip.currentEffectiveDate}) AND create_time < ${Adip.currentEffectiveDate} AND data NOTNULL`
+        return Promise.resolve(result.rowCount)
+    }
+
     public static async count():Promise<number> {
         const result = await sql`SELECT count(*) FROM Adip`;
         return Number(result.rows[0].count)
     }
 
     public static async save(code:string, data:any) {
-        await sql`INSERT INTO Adip (Code, Data) VALUES (${code}, ${data})`;
+        await sql`INSERT INTO Adip (code, data) VALUES (${code}, ${data})`;
     }
 }
