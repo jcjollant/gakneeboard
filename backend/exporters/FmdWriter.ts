9afd51ef197b41c38e7208d525727e3da3dc07ed
JC Jollant <jc@jollant.net>
2024-11-11 23:01:09 -0800

    FF checklist support

--- backend/exporters/FmdWriter.ts
+++ backend/exporters/FmdWriter.ts
@@ -1,45 +1,76 @@
+import { setTextRange } from "typescript";
 import { Template } from "../models/Template"
 import { FmdChecklist } from "./FmdChecklist"
+import crypto from 'crypto'
+
+enum KeyUsage {
+  Encrypt = 'encrypt',
+  Decrypt = 'decrypt',
+}
 
 export class FmdWriter {
-    // public static async encodeTemplate(template:Template):Promise<ArrayBuffer> {
-    //     const checklist:FmdChecklist = FmdChecklist.fromTemplate(template)
-    //     return FmdWriter.encode(checklist)
-    // }
+  static CIPHER_BLOCK_SIZE:number = 16;
+  static CIPHER_TYPE = 'AES-CBC';
+  static CIPHER_KEY = Buffer.from('81e06e41a93f3848', 'ascii');
 
-    // public static async encode(checklist:FmdChecklist):Promise<ArrayBuffer> {
-    //     {
-    //         type: 'checklist',
-    //         payload: {
-    //             objectId: uuidV4().replaceAll('-', ''),
-    //             schemaVersion: '1.0',
-    //             metadata: {
-    //                 name: file.metadata?.name,
-    //                 detail: file.metadata?.makeAndModel,
-    //                 tailNumber: file.metadata?.aircraftInfo.toUpperCase(),
-    //             }:ForeFlightChecklistMetadata 
-    //             // Normal / Abnormal / Emergency
-    //             // groups > subgroups > 
-    //             groups: [
-    //                 objectId: uuidV4().replaceAll('-', ''),
-    //                 groupType: ChecklistGroup_Category.normal,
-    //                 items: [
-    //                     objectId: uuidV4().replaceAll('-', ''),
-    //                     title: checklistTitle,
-    //                     items: [{
-    //                         objectId: uuidV4().replaceAll('-',''),
-    //                         type: 'comment', // 
-    //                         title: challenge,
-    //                         detail: response    
-    //                     }
+  public static async encodeTemplate(template:Template):Promise<ArrayBuffer> {
+    const checklist:FmdChecklist = FmdChecklist.fromTemplate(template)
+    // console.log('[FmdWriter.encodeTemplate] checklist', JSON.stringify(checklist))
+    return FmdWriter.encode(checklist)
+  }
 
-    //                     ]:ForeFlightChecklistItem[]
-    //                 ]:ForeFlightChecklistSubgroup[]
-    //             ]:ForeFlightChecklistGroup[]
-    //         }:ForeFlightChecklistPayload
-    //     }:ForeFlightChecklistContainer
+  /**
+    * transforms checklist into an FMD formatted ArrayBuffer
+    * Checklist > Group > Subgroup > Item
+    * @param checklist 
+    * @returns 
+    */
+  public static async encode(checklist:FmdChecklist):Promise<ArrayBuffer> {
+      const data:any = {
+        type: 'checklist',
+        payload: checklist
+      }
+      const strData:string = JSON.stringify(data)
+      console.log('[FmdWriter.encode]', strData)
 
-    //     const ab:ArrayBuffer = new ArrayBuffer(1)
-    //     return ab
-    // }
+      // generate 16 random bytes
+      const rb = new Uint8Array(FmdWriter.CIPHER_BLOCK_SIZE);
+      const iv = crypto.getRandomValues(rb);
+      const blah = new Blob(
+        [
+          iv,
+          await crypto.subtle.encrypt(
+            { name: FmdWriter.CIPHER_TYPE, iv: iv },
+            await FmdWriter.getKey(KeyUsage.Encrypt),
+            new TextEncoder().encode(strData),
+          ),
+        ],
+        { type: 'application/octet-stream' },
+      );
+
+      return await blah.arrayBuffer();
+  }
+
+  public static async decode(source:ArrayBuffer):Promise<string> {
+    const decrypted = await crypto.subtle.decrypt(
+      {
+        name: FmdWriter.CIPHER_TYPE,
+        iv: new Uint8Array(source.slice(0, FmdWriter.CIPHER_BLOCK_SIZE)),
+      },
+      await FmdWriter.getKey(KeyUsage.Decrypt),
+      new Uint8Array(source.slice(FmdWriter.CIPHER_BLOCK_SIZE)),
+    );
+    return new TextDecoder().decode(new Uint8Array(decrypted));    
+  }
+
+  static async getKey(usage:KeyUsage):Promise<crypto.webcrypto.CryptoKey> {
+    return crypto.subtle.importKey(
+      'raw',
+      FmdWriter.CIPHER_KEY,
+      { name: FmdWriter.CIPHER_TYPE },
+      false,
+      [usage],
+    );
+  }
+
 }
---
+++ backend/exporters/TemplateChecklist.ts
@@ -0,0 +1,119 @@
+import { it } from "node:test";
+import { PageType, Template, Tile } from "../models/Template";
+
+
+export class TemplateChecklistItem {
+    c:string // challenge
+    r:string // response
+    s:string // section
+    t:string // type
+    constructor() {
+        this.c = ''
+        this.r = ''
+        this.s = ''
+        this.t = ''
+    }
+}
+
+export class TemplateChecklistSection {
+    name:string
+    items:any[]
+
+    constructor(name:string) {
+        this.name = name
+        this.items = []
+    }
+
+    addItem(item:TemplateChecklistItem) {
+        this.items.push(item)
+    }
+
+    addItems(items:any[]) {
+        this.items = this.items.concat(items)
+    }
+}
+
+export class TemplateChecklist {
+    // Structure : List > Section > Item
+    name:string
+    sections:TemplateChecklistSection[]
+
+    constructor(name:string) {
+        this.name = name
+        this.sections = []
+    }
+
+    addItems(items:TemplateChecklistItem[]) {
+        let section:TemplateChecklistSection|undefined;
+        for(const item of items) {
+            // ignore blank lines
+            if( item.t == 'blank') continue;
+
+            if(item.s) {
+                section = this.addSection(item.s)
+            } else {
+                if(!section) {
+                    section = this.addSection('Default')
+                }
+                section.addItem(item)
+            }
+        }
+    }
+
+    /**
+     * Create a new section append it to the list
+     * @param name New Section name
+     * @returns the newly created section
+     */
+    addSection(name:string):TemplateChecklistSection {
+        const newSection = new TemplateChecklistSection(name)
+        this.sections.push(newSection)
+        return newSection
+    }
+
+
+    // this format unifies all checklists from a template between checklist page and tiles
+    static fromTemplate(template:Template):TemplateChecklist[] {
+        const output:TemplateChecklist[] = []
+        
+        const checklists:any[] = []
+        // extract all checlists from pages and tiles
+        for( const page of template.data) {
+            if(page.type == PageType.checklist) {
+                checklists.push({name:page.data.name,items:page.data.items})
+                if('items2' in page.data) { // two columns list
+                    checklists.push({name:page.data.name, items:page.data.items2})
+                }   
+
+            } else if(page.type == PageType.tiles) {
+                for(const tile of page.data) {
+                    if(tile.name == Tile.checklist) {
+                        checklists.push({name:tile.data.name, items:tile.data.items})
+                    }
+                }
+            }
+        }
+
+        // no checklists found
+        if(!checklists.length) return output;
+
+        // transform these checklists
+        let currentList:TemplateChecklist|undefined=undefined;
+
+        for(const checklist of checklists) {
+            // is this a new list?
+            if(currentList && currentList.name == checklist.name) { 
+                // continue existing list
+                currentList.addItems(checklist.items)
+            } else {
+                // start new list
+                currentList = new TemplateChecklist(checklist.name)
+                currentList.addItems(checklist.items)
+                output.push(currentList)
+            }
+        }
+
+
+        return output;
+    }
+}
