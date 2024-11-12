dc195f28f9661785f88b5ab4cc7f65e9c9210140
JC Jollant <jc@jollant.net>
2024-11-11 23:00:50 -0800

    Decrypt test

--- housekeeping/manual2.ts
+++ housekeeping/manual2.ts
@@ -1,18 +1,20 @@
-import { HouseKeeping } from "../backend/HouseKeepings";
-import { Metrics } from "../backend/Metrics";
-import { postgresUrl } from "../test/constants"
+// import { HouseKeeping } from "../backend/HouseKeepings";
+// import { Metrics } from "../backend/Metrics";
+// import { postgresUrl } from "../test/constants"
 
-process.env.POSTGRES_URL=postgresUrl;
+import { FmdWriter } from "../backend/exporters/FmdWriter"
+
+// process.env.POSTGRES_URL=postgresUrl;
 
 
 // Metrics.prints().then(metric => {
 //     console.log(metric.name, metric.value)
 // })
 
-Metrics.templateDetails().then(metrics => {
-    for(const metric of metrics)
-        console.log(metric.name, metric.value)
-})
+// Metrics.templateDetails().then(metrics => {
+//     for(const metric of metrics)
+//         console.log(metric.name, metric.value)
+// })
 
 // new Dao('prints').count().then( r => console.log(r))
 // Adip.fetchAirport('OG14').then(airport => console.log(JSON.stringify(airport)));
@@ -20,3 +22,11 @@ Metrics.templateDetails().then(metrics => {
 //     const metric = await HouseKeeping.performAdipCleanup();
 //     console.log(metric.name, metric.value)
 // })
+
+
+// load local file source.fmd into ArrayBuffer
+var fs = require('fs');
+fs.readFile('source.fmd', function(err:any, data:any) {
+    if (err) throw err;
+    FmdWriter.decode(data.buffer).then(text => console.log(text))
+});
