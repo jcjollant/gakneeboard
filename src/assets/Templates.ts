02b70688563f5856454817071c5fe4298041c265
JC Jollant <jc@jollant.net>
2024-11-23 12:10:59 -0800

    Added version

--- src/assets/Templates.ts
+++ src/assets/Templates.ts
@@ -20,17 +20,20 @@ export class Template {
     desc:string
     publish:boolean
     data:any
-    constructor(name:string, description:string, publish:boolean=false, data:any=[]) {
+    ver:number
+    constructor(name:string, description:string, publish:boolean=false, data:any=[], version:number=0) {
         this.name = name
         this.desc = description
         this.publish = publish
         this.data = data
+        this.ver = version
     }
 
     static describe(template:any):string {
         if(!template) return 'n/a'
         let output = template.desc??'(none)'
         if(template.publish) output += ' / Public [' + template.code + ']'
+        if(template.ver > 0) output += ' v' + template.ver;
         return output
     }
 
