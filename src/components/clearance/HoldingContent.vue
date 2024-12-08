ceb2fe1e9be40d4401361baf800fcd7c0ad131e6
JC Jollant <jc@jollant.net>
2024-12-08 11:01:33 -0800

    Independant control

---
+++ src/components/clearance/HoldingContent.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="hold tileContent">
+        <div class="params">&nbsp;
+            <div class="label labelTopLeft">
+                <div class="direction">
+                    <div>&#x25AB;</div>
+                    <div>N</div>
+                    <div>&#x25AB;</div>
+                    <div>W</div>
+                    <div>&nbsp;</div>
+                    <div>E</div>
+                    <div>&#x25AB;</div>
+                    <div>S</div>
+                    <div>&#x25AB;</div>
+                </div>
+            </div>
+            <div class="label labelTopRight">RAD/CRS</div>
+            <div class="label labelBottomLeft">&#x021B6 Turns &#x021B7</div>
+            <div class="label labelBottomRight">EFC</div>
+        </div>
+        <div class="row brg">CRS</div>
+        <!-- <div class="">Left</div> -->
+        <div class="row wca">WCA</div>
+        <div class="row heading">
+            <div class="box">&nbsp;</div>
+            <div class="name">HDG</div>
+            <div class="box">&nbsp;</div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.direction {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    align-items: center;
+    justify-items: center;
+    gap: 2px;
+}
+.hold {
+    display: flex;
+    flex-flow: column;
+}
+.hold .bottom {
+    grid-column: 2;
+    border: 2px dashed darkgrey;
+    border-top: none;
+    border-bottom-left-radius: 24px;
+    border-bottom-right-radius: 24px;
+    margin-bottom: 36px;
+}
+.hold .box {
+    border: 2px solid black;
+    margin: 2px;
+}
+.hold .brg {
+    border-bottom: 1px dashed grey;
+}
+
+.hold .params {
+    position: relative;
+    height: 120px;
+    border-bottom: 1px dashed grey;
+}
+.hold .row {
+    line-height: 40px;
+}
+
+.hold .sep {
+    position: relative;
+    border-bottom: 1px dashed grey;
+}
+.hold .time {
+    position: relative;
+}
+.hold .top {
+    padding-top: 16px;
+    border-top: 2px solid black;
+    border-top-left-radius: 24px;
+    border-top-right-radius: 24px;
+}
+.heading {
+    border-top: 1px dashed grey;
+    display: grid;
+    grid-template-columns: auto 50px auto;
+    align-items: center;
+}
+
+.label {
+  position: absolute;
+  font-size: 10px;
+}
+.labelBottomLeft {
+  left: 3px;
+  bottom: 0;
+  font-size: 15px;
+}
+.labelBottomRight {
+  right: 3px;
+  bottom: 0;
+}
+.labelTopLeft {
+  left: 5px;
+  top: 2px;
+}
+.labelTopRight {
+  right: 3px;
+  top: 0;
+}
+
+
+</style>
