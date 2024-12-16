b08e330d283fccb08702d29f0f9f1ce2651f9c82
JC Jollant <jc@jollant.net>
2024-12-15 20:57:07 -0800

    Moved to its own file

---
+++ src/assets/PageType.ts
@@ -0,0 +1,10 @@
+export class PageType {
+  static selection = 'selection';
+  static tiles = 'tiles';
+  static diagram = 'diagram';
+  static checklist = 'checklist';
+  static cover = 'cover';
+  static navLog = 'navlog';
+  static notes = 'notes';
+  static approach = 'approach';
+}
