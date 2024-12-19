6c6c2ba6f1ec75476134e4c9ddcf55d277adfd68
JC Jollant <jc@jollant.net>
2024-12-18 22:12:32 -0800

    Added warning error and success

--- src/assets/Toaster.ts
+++ src/assets/Toaster.ts
@@ -7,7 +7,20 @@ export class Toaster {
         this.toast = toast
     }
 
+    error(title:string, message:string) {
+        this.toast.add({ severity: 'error', summary: title, detail: message, life: 3000 });
+    }
+
     info(title:string, message:string) {
         this.toast.add({ severity: 'info', summary: title, detail: message, life: 3000 });
     }
+
+    success(title:string, message:string) {
+        this.toast.add({ severity: 'success', summary: title, detail: message, life: 3000 });
+    }
+
+    warning(title:string, message:string) {
+        this.toast.add({ severity: 'warn', summary: title, detail: message, life: 3000 });
+    }
+
 }
