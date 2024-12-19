5e0d1415bfca4fe6f15ff1c20bd960035b832ba3
JC Jollant <jc@jollant.net>
2024-12-18 22:12:53 -0800

    Unified Menu

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -2,19 +2,19 @@
     <div class="menu">
         <div class="left">
             <Logo @click="router.push('/')" />
+            <font-awesome-icon v-if="name" class="icon" icon="fa-chevron-right" />
+            <div v-if="name" title="Active Template Name">{{name}}</div>
         </div>
 
         <div v-if="route.name=='Template'" class="actions">
-            <!-- <router-link to="/print">Print</router-link> -->
             <FAButton icon="fa-print" label="Print" :menu="true" title="Export Template" @click="onPrint" />
             <FAButton icon="fa-circle-info" label="Save" :menu="true" title="Export Template"></FAButton>
-            <!-- <div>Save</div> -->
             <FAButton icon="fa-file-export" label="Export" :menu="true" title="Export Template"></FAButton>
         </div>
 
         <div class="right">
-            <FAButton icon="fa-circle-info" :menu="true"></FAButton>
-            <div>Session</div>
+            <!-- <FAButton icon="fa-circle-info" :menu="true" @click="router.push('/about')" title="About GA Kneeboard"></FAButton> -->
+            <Session />
         </div>
     </div>
 </template>
@@ -23,10 +23,16 @@
 // import { ref } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import FAButton from '../shared/FAButton.vue';
-import Logo from './Logo.vue'
+import Logo from './Logo.vue';
+import Session from './Session.vue';
 
+const emits = defineEmits(['about'])
+const props = defineProps({
+    name: String, default: null
+})
 const route = useRoute()
 const router = useRouter()
+
 
 const onPrint = () => router.push('/print')
 
@@ -37,8 +43,15 @@ const onPrint = () => router.push('/print')
     display: flex;
     gap: 10px;
 }
+.icon {
+    color: black;
+}
 .left {
     display: flex;
+    align-items: center;
+    gap: 10px;
+    font-size: var(--logo-font-size);
+    font-weight: bold;
 }
 .menu {
     height: 2rem;
@@ -46,7 +59,6 @@ const onPrint = () => router.push('/print')
     justify-content: space-between;
     align-items: center;
     padding: 0 10px;
-    /* border: 1px solid red; */
     width: 100%;
     background-color: lightgrey;
 }
