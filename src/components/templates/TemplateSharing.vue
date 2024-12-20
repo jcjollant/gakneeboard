e22ef21a9ba5ed0bd6ef66779c3a2292b5708d01
JC Jollant <jc@jollant.net>
2024-12-19 23:19:50 -0800

    Reorg

--- src/components/templates/TemplateSharing.vue
+++ src/components/templates/TemplateSharing.vue
@@ -1,32 +1,44 @@
+<template>
+    <div class="sharing">
+        <div class="sharingText">Sharing</div>
+        <OneChoice v-model="publishChoice" :choices="[pubPrivate,pubPublic]" />
+        <div v-if="directLink" class="directlink">
+            <div>with code</div>
+            <div class="bold"><a :href="directLink" target="_blank" title="Open link in new tab">{{ template?.code ? template.code:'(none)' }}</a></div>
+            <Button icon="pi pi-clipboard" title="Copy link to clipboard" @click="onCopyURL" link></Button>
+        </div>
+    </div>
+</template>
+
 <script setup>
 import { onMounted, ref, watch } from 'vue'
-
-import { emitToast, emitToastError } from '../../assets/toast'
+import { Toaster } from '../../assets/Toaster'
+import { UserUrl } from '../../lib/UserUrl';
+import { useToast } from 'primevue/usetoast'
 
 import Button from 'primevue/button'
 import OneChoice from '../shared/OneChoice.vue';
-import { UserUrl } from '../../lib/UserUrl';
 
-const emits = defineEmits(["toast"]);
 
 const directLink = ref('')
 const pubPublic = {label:'Public'}
 const pubPrivate = {label:'Private'}
-const publish = ref(pubPrivate)
+const publishChoice = ref(pubPublic)
+const publish = defineModel()
 const template = ref(null)
+const toaster = new Toaster(useToast())
 
 //-----------------
 // Props management
-const props = defineProps({ 
+const props = defineProps({
   template: { type: Object, default: null},
 })
 
-
 function loadProps(props) {
   // Active template
-  // console.log('[TemplateDialog.loadProps]', JSON.stringify(props.template))
+  // console.log('[TemplateSharing.loadProps]', JSON.stringify(props.template))
   template.value = props.template;
-  publish.value = template.value?.publish ? pubPublic : pubPrivate
+  publishChoice.value = (publish.value ? pubPublic : pubPrivate)
   if( template.value && template.value.code) {
     directLink.value = UserUrl.main + '?t=' + template.value.code
   } else {
@@ -43,10 +55,10 @@ watch( props, async() => {
 })
 
 // keep template publish in sync with publish option
-watch(publish, () => {
+watch(publishChoice, () => {
     if(template.value){
-        template.value.publish = (publish.value.label == pubPublic.label)
-        // console.log('[TemplateSharing.watch]', template.value.publish)
+        publish.value = (publishChoice.value.label == pubPublic.label)
+        // console.log('[TemplateSharing.watch]', publish.value)
     } 
 })
 
@@ -56,33 +68,19 @@ watch(publish, () => {
 async function onCopyURL() {
   const toastTitle = 'Copy to Clipboard'
     if(!directLink.value) {
-      emitToastError(emits, toastTitle, 'Nothing to copy')
+      toaster.error(toastTitle, 'Nothing to copy')
       return;
     }
     try {
       await navigator.clipboard.writeText(directLink.value);
-      emitToast(emits, toastTitle, 'URL copied to clipboard')
+      toaster.info(toastTitle, 'URL copied to clipboard')
     } catch(e) {
       console.log('[TemplateSharing.onCopyURL]' + e)
-      emitToastError(emits, toastTitle, 'Could not copy to clipboard')
+      toaster.error(toastTitle, 'Could not copy to clipboard')
     } 
 }
 
-
-
 </script>
-
-<template>
-    <div class="sharing">
-        <div class="sharingText">Sharing</div>
-        <OneChoice v-model="publish" :choices="[pubPrivate,pubPublic]" />
-        <div v-if="directLink" class="directlink">
-            <div>with code</div>
-            <div class="bold"><a :href="directLink" target="_blank" title="Open link in new tab">{{ template?.code ? template.code:'(none)' }}</a></div>
-            <Button icon="pi pi-clipboard" title="Copy link to clipboard" @click="onCopyURL" link></Button>
-        </div>
-    </div>
-</template>
 
 <style>
 .directlink {
