ef1e0d36250414f9e7a307ef12ed557235174478
JC Jollant <jc@jollant.net>
2024-12-20 19:22:54 -0800

    Removed about route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import About from '../views/About.vue';
 import Template from '../views/Template.vue';
 import Home from '../views/Home.vue';
 import Print from '../views/Print.vue';
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
-    { path: '/about', name: 'About', component: About },
     { path: '/print', name: 'Print', component: Print },
     { path: '/template/:id', name: 'Template', component: Template }
 ]
