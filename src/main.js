c474c260979f368ce9d79f4b7d1daae65bcf3c57
JC Jollant <jc@jollant.net>
2024-12-26 22:19:04 -0800

    Google Sign-in cleanup

--- src/main.js
+++ src/main.js
@@ -7,20 +7,21 @@ import {
     faGasPump, faGear, faHeadset, faImage, faJetFighter,
     faLink, faListCheck,
     faPenToSquare, faPlaneArrival, faPlaneCircleCheck, faPlaneDeparture, faPrint,
-    faQuestion, 
-    faRoadCircleCheck, faRoute, faRotateLeft,
-    faSave, faScrewdriverWrench, faStarOfLife, faSun, 
+    faQuestion,
+    faRoadCircleCheck,
+    faRotateLeft,
+    faRoute,
+    faSave, faScrewdriverWrench, faStarOfLife, faSun,
     faTrash, faUser,
     faVideo, faWalkieTalkie, faXmark
 } from '@fortawesome/free-solid-svg-icons';
-import { createApp } from 'vue';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import ConfirmationService from 'primevue/confirmationservice';
-import PrimeView from 'primevue/config';
-import ToastService from 'primevue/toastservice';
 import 'primeicons/primeicons.css';
+import PrimeView from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
 import 'primevue/resources/themes/saga-blue/theme.css';
-import GoogleSignInPluggin from 'vue3-google-signin';
+import ToastService from 'primevue/toastservice';
+import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './style.css';
@@ -41,7 +42,7 @@ createApp(App)
     .use(PrimeView)
     .use(ConfirmationService)
     .use(ToastService)
-    .use(GoogleSignInPluggin, {clientId: '864395393673-li5elss3gtbhipp6pdjs1pbgbl0866si.apps.googleusercontent.com'})
+    // .use(GoogleSignInPluggin, {clientId: '864395393673-li5elss3gtbhipp6pdjs1pbgbl0866si.apps.googleusercontent.com'})
     .use(router)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
