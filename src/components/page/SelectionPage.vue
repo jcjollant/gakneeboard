6a0b3ab2231859078a0c4b3af4396905f6c85fca
JC Jollant <jc@jollant.net>
2024-12-30 23:56:32 -0800

    Selection Page improvments

--- src/components/page/SelectionPage.vue
+++ src/components/page/SelectionPage.vue
@@ -1,3 +1,24 @@
+<template>
+    <div class="contentPage">
+        <Header :title="'New Page Selection'" :replace="false" :clickable="false"></Header>
+        <!-- <div class="widgetTitle">Tile Selection</div> -->
+        <div class="list">
+            <Separator name="Basics" />
+            <FAButton v-for="page in topPages" :label="page.name" :title="page.tooltip" :icon="page.icon"
+                @click="replacePage(page.type)"/>
+            <Separator name="Navigation" />
+            <FAButton v-for="page in knownPages" :label="page.name" :title="page.tooltip" :icon="page.icon"
+                @click="replacePage(page.type)"/>
+            <Separator name="Charts" />
+            <FAButton v-for="page in chartsPages" :label="page.name" :title="page.tooltip" :icon="page.icon"
+                @click="replacePage(page.type)"/>
+            <Separator name="Cosmetics" />
+            <FAButton v-for="page in cosmeticPages" :label="page.name" :title="page.tooltip" :icon="page.icon"
+                @click="replacePage(page.type)"/>
+        </div>
+    </div>
+</template>
+
 <script setup lang="ts">
 import { ref } from 'vue'
 
@@ -5,19 +26,25 @@ import { PageType } from '../../assets/PageType'
 
 import Header from '../shared/Header.vue'
 import FAButton from '../shared/FAButton.vue'
+import Separator from './Separator.vue'
 
 const emits = defineEmits(['replace'])
 
 const topPages = ref([
     {name:'Tiles',type:PageType.tiles, icon:'border-all', tooltip:'A 2x3 grid of customizable tiles like Airport, ATIS, Radios, ...'},
     {name:'Checklist',type:PageType.checklist, icon:'list-check', tooltip:'A customizable checklist'},
+    {name:'Notes',type:PageType.notes, icon:'pen-to-square', tooltip:'A blank page to write down instructions'},
 ])
 const knownPages = ref([
-    {name:'Airport Diagram',type:PageType.diagram, icon:'road-circle-check',tooltip:'Airport Diagram (FAA)'},
-    {name:'Cover',type:PageType.cover, icon:'image', tooltip:'A cover page for your stylish templates'},
-    {name:'Instrument Approach',type:PageType.approach, icon:'plane-arrival', tooltip:'Instrument Approach Plates (FAA)'},
     {name:'NavLog',type:PageType.navLog, icon:'route', tooltip:'A Navigation Log with checkpoints and headings'},
-    {name:'Notes',type:PageType.notes, icon:'pen-to-square', tooltip:'A blank page to write down instructions'},
+    {name:'IFR Flight Notes',type:PageType.flightNotes, icon:'pen-to-square', tooltip:'Aviation101 IFR flight notes page'},
+])
+const chartsPages = ref([
+    {name:'Airport Diagram',type:PageType.diagram, icon:'road-circle-check',tooltip:'Airport Diagram (FAA)'},
+    {name:'Instrument Approach',type:PageType.approach, icon:'plane-arrival', tooltip:'Instrument Approach Plates (FAA)'},
+])
+const cosmeticPages = ref([
+    {name:'Cover',type:PageType.cover, icon:'image', tooltip:'A cover page for your stylish templates'},
 ])
 
 function replacePage(type) {
@@ -27,20 +54,6 @@ function replacePage(type) {
 
 </script>
 
-<template>
-    <div class="contentPage">
-        <Header :title="'Page Selection'" :replace="false" :clickable="false"></Header>
-        <!-- <div class="widgetTitle">Tile Selection</div> -->
-        <div class="list">
-            <FAButton v-for="page in topPages" :label="page.name" :title="page.tooltip" :icon="page.icon"
-                @click="replacePage(page.type)"/>
-            <div class="separator"></div>
-            <FAButton v-for="page in knownPages" :label="page.name" :title="page.tooltip" :icon="page.icon"
-                @click="replacePage(page.type)"/>
-        </div>
-    </div>
-</template>
-
 <style scoped>
 .list {
     display: flex;
@@ -48,11 +61,5 @@ function replacePage(type) {
     padding: 10px;
     gap:10px;
     /* padding-top: 50px; */
-}
-.separator {
-    height: 1px;
-    border: 1px solid lightblue;
-    width: 20%;
-    margin: 10px auto;
 }
 </style>
---
+++ src/components/page/Separator.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="separator">{{  name  }}</div>
+</template>
+
+<script setup>
+defineProps({
+    name: String
+})
+</script>
+
+<style scoped>
+.separator {
+  display: flex;
+  color: var(--bg);
+
+  &:before,
+  &:after {
+    content: "";
+    flex: 1;
+    height: 1px;
+    margin: 0 1em;
+    background: lightblue;
+  }
+
+  align-items: center;
+  padding-top: 5px;
+  margin: 0;
+
+}
+
+</style>
