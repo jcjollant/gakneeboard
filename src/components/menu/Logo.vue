5c7a47de821db834e389bc167faa487d2ac76d7e
JC <jc@jollant.net>
2025-01-09 13:24:48 -0800

    BETA logo

--- src/components/menu/Logo.vue
+++ src/components/menu/Logo.vue
@@ -2,6 +2,7 @@
     <div class="logo">
         <img src="/favicon.png" class="logo-img" alt="GA Kneeboard">
         <div class="logo-name">GA Kneeboard</div>
+        <div class="logo-beta">BETA</div>
     </div>
 </template>
 
@@ -25,5 +26,11 @@
     font-weight: bold;
     padding-left: 10px;
     line-height: 30px;
+}
+.logo-beta {
+    font-size: calc(var(--logo-font-size) / 2);
+    font-weight: bold;
+    height: 25px;
+    color: #777;
 }
 </style>
