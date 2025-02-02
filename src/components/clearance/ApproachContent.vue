88b5b1b199fcbe65f553aa28b846e3f2bb1fd387
JC Jollant <jc@jollant.net>
2025-02-01 21:56:21 -0800

    Added predefined frequencies

--- src/components/clearance/ApproachContent.vue
+++ src/components/clearance/ApproachContent.vue
@@ -1,25 +1,93 @@
 <template>
     <div class="tileContent approach">
         <div class="row bb">
-            <div class="atis tileBoxLabel br">ATIS</div>
+            <div v-if="airportMode" class="airportFreq">
+                <FrequencyBox :freq="freqWeather"  :small="true"/>
+            </div>
+            <div v-else class="atis tileBoxLabel br">ATIS</div>
             <div class="iloc tileBoxLabel br">ILOC</div>
             <div class="crs tileBoxLabel">CRS</div>
         </div>
         <div class="row bb">
-            <div class="atc tileBoxLabel br">App Con</div>
-            <div class="twr tileBoxLabel br">Tower / CTAF</div>
-            <div class="gnd tileBoxLabel">Ground</div>
+            <div v-if="airportMode" class="airportFreq">
+                <FrequencyBox :freq="freqAtc" :small="true"/>
+            </div>
+            <div v-else class="atc tileBoxLabel br">App Con</div>
+            <div class="fixes tileBoxLabel br">IAF</div>
         </div>
         <div class="row bb">
-            <div class="fixes tileBoxLabel br">IAF</div>
-            <div class="alt tileBoxLabel br">IAF Alt</div>
+            <div v-if="airportMode" class="airportFreq">
+                <FrequencyBox :freq="freqTower" :small="true"/>
+            </div>
+            <div v-else class="twr tileBoxLabel br">Tower / CTAF</div>
             <div class="min tileBoxLabel">Minimum</div>
-        </div> 
-        <div class="row">
-            <div class="missed tileBoxLabel">Missed<span class="climb">&#8593;</span></div>
         </div>
+        <div class="row">
+            <div v-if="airportMode" class="airportFreq">
+                <FrequencyBox :freq="freqGround" :small="true"/>
+            </div>
+            <div v-else class="gnd tileBoxLabel br">Ground</div>
+            <div class="missed tileBoxLabel">Missed<span class="climb">&#8593;</span></div>
+        </div> 
     </div>
 </template>
+
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+import { Airport } from '../../model/Airport';
+import { Frequency } from '../../model/Frequency';
+
+import FrequencyBox from '../shared/FrequencyBox.vue';
+import { FrequencyType } from '../../model/FrequencyType';
+
+const airportMode = ref(false)
+const noFreq = Frequency.noFreq()
+const freqAtc = ref(noFreq)
+const freqWeather = ref(noFreq)
+const freqGround = ref(noFreq)
+const freqTower = ref(noFreq)
+const props = defineProps({
+    airport: { type: Airport, default: null},
+})
+
+
+function loadProps(props:any) {
+    // console.log('[ApproachContent.loadProps]', props.airport)
+  
+    if (props.airport && props.airport.code != '') {
+        airportMode.value = true
+        const airport:Airport = props.airport
+        // console.log('[DepartureContent] loadProps', props.airport)
+        const cdFreq = airport.getFreqClearance()
+        freqAtc.value = Frequency.fromType(cdFreq,FrequencyType.clearance)
+        // console.log('[DepartureContent] loadProps', freqClearance.value)
+        const gndFreq = airport.getFreqGround()
+        freqGround.value = Frequency.fromType(gndFreq,FrequencyType.ground)
+        const twrFreq = airport.getFreqTower()
+        const ctafFreq = airport.getFreqCtaf()
+        freqTower.value = twrFreq ? Frequency.fromType(twrFreq,FrequencyType.tower) : ( ctafFreq ? Frequency.fromType(ctafFreq,FrequencyType.ctaf) : noFreq)
+        const weatherFreq = airport.getFreqWeather()
+        freqWeather.value = Frequency.fromType(weatherFreq?.mhz, FrequencyType.weather)
+    } else {
+        airportMode.value = false
+        freqWeather.value = noFreq
+        freqAtc.value = noFreq
+        freqGround.value = noFreq
+        freqTower.value = noFreq
+    }
+    // console.log('[DepartureContent] loadProps', freqClearance.value)
+}
+
+onMounted(() => {
+    loadProps(props)
+})
+
+watch(props, async() => {
+    loadProps(props)
+})
+
+
+</script>
 
 <style scoped>
 .approach {
@@ -41,14 +109,21 @@
     padding-left: 2px;
 }
 .bb {
-    border-bottom: 1px dashed grey;
+    border-bottom: 1px dashed darkgrey;
 }
 .br {
-    border-right: 1px dashed grey;
+    border-right: 1px dashed darkgrey;
 }
-.atis, .atc, .twr, .iloc, .apch, .crs, .gnd, .fixes, .alt, .min {
-    /* flex-grow: 1; */
+.atis, .atc, .twr, .gnd {
+    flex: 0.9 1 0px;
+}
+.iloc, .apch, .crs, .alt {
     flex: 1 1 0px;
+}
+
+.fixes, .min, .missed {
+    position: relative;
+    flex: 2 2 0px;
 }
 
 .missed {
@@ -60,4 +135,16 @@
     left: 5px;
     top: 10px;
 }
+
+.airportFreq {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 0.9 1 0px;
+    gap: 5px;
+    padding: 5px;
+    background-color: lightgrey;
+}
+
+
 </style>
