0b605cc74c36a452afa49b5233e36d0f36dc6866
JC Jollant <jc@jollant.net>
2025-02-01 21:59:07 -0800

    display mode is now the model

--- src/components/shared/DisplayModeSelection.vue
+++ src/components/shared/DisplayModeSelection.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="modesList">
-        <Button v-for="mode in modes" :label="mode.label" :severity="mode.value==activeMode ? 'primary' : 'secondary'" class="entry"
+        <Button v-for="mode in modes" :label="mode.label" :severity="mode.value==model ? 'primary' : 'secondary'" class="entry"
             @click="emits('selection', mode.value)"></Button>
     </div>
 
 </template>
 
 <script setup lang="ts">
-import { DisplayModeChoice } from '@/model/DisplayModeChoice'
+import { DisplayModeChoice } from '../../model/DisplayMode'
 import Button from 'primevue/button'
 
 const emits = defineEmits(['selection'])
+const model = defineModel<string>()
 const props = defineProps({
     modes: { type: Array<DisplayModeChoice>, default: []},
-    activeMode: { type: String, default: ''}
 })
 
 </script>
