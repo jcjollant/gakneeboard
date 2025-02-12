f9e96dd605a9484cd1e59d1f874d16028c78f58e
JC Jollant <jc@jollant.net>
2025-02-11 19:08:51 -0800

    Added account type to data model

--- backend/dao/UserDao.ts
+++ backend/dao/UserDao.ts
@@ -4,7 +4,7 @@ import { Dao } from './Dao';
 import { AccountType } from '../models/AccountType';
 
 export class UserDao extends Dao {
-    static modelVersion:number = 1;
+    static modelVersion:number = 2;
 
     constructor() {
         super('users')
@@ -15,7 +15,7 @@ export class UserDao extends Dao {
             const result = await sql`SELECT id,sha256,data FROM users`;
             const users:User[] = []
             for(const row of result.rows) {
-                users.push(User.fromJson(row.id, row.sha256, row.data))
+                users.push(User.fromJson(row.id, row.sha256, row.data, row.accountType))
             }
             resolve(users)
         })
@@ -39,9 +39,9 @@ export class UserDao extends Dao {
 
     // builds a user using the sha256 as a key
     public static async getUserFromHash(sha256:string):Promise<User | undefined> {
-        const result = await sql`SELECT id,data FROM users WHERE sha256=${sha256}`;
+        const result = await sql`SELECT id,data,account_type FROM users WHERE sha256=${sha256}`;
         if( result.rowCount == 0) return undefined
-        return User.fromJson(result.rows[0].id, sha256, result.rows[0].data)
+        return User.fromJson(result.rows[0].id, sha256, result.rows[0].data, result.rows[0].account_type)
     }
 
     /**
@@ -54,17 +54,19 @@ export class UserDao extends Dao {
         // console.log( '[UserDao.save]  ' + JSON.stringify(user))
         return new Promise<User>(async (resolve, reject) => {
             if( !user.sha256) throw new Error('sha256 missing')
+
+            // Do we know this user?
             const result = await sql`SELECT id from Users WHERE sha256 = ${user.sha256}`;
             // console.log( '[UserDao.save] match count ' + result.rowCount)
             if(result.rowCount == 0) {
                 // console.log( '[UserDao.save] adding ' + user.sha256)
-                const insert = await sql`INSERT INTO Users (sha256,data,version) VALUES (${user.sha256},${JSON.stringify(user)},${this.modelVersion}) RETURNING id`
+                const insert = await sql`INSERT INTO Users (sha256,data,version,account_type) VALUES (${user.sha256},${JSON.stringify(user)},${this.modelVersion},${user.accountType}) RETURNING id`
                 user.id = insert.rows[0].id
                 // console.log( '[UserDao.save] ' + user.sha256)
             } else if( overwrite){ // this user is known
                 // console.log( '[UserDao.save] known user ' + user.sha256)
                 user.id = result.rows[0].id
-                await sql`UPDATE Users SET data = ${JSON.stringify(user)}, version=${this.modelVersion} WHERE id = ${user.id}`
+                await sql`UPDATE Users SET data = ${JSON.stringify(user)}, version=${this.modelVersion}, account_type=${user.accountType} WHERE id = ${user.id}`
             } else {
                 reject('Cannot save existing user without overwrite')
                 return
