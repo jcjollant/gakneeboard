a8743e83e574a9f5357544652bc29e82eba69cc1
JC Jollant <jc@jollant.net>
2025-02-23 08:43:26 -0800

    New test for SubscriptDao class

---
+++ test/SubscriptionDao.test.ts
@@ -0,0 +1,30 @@
+
+import { describe, expect, test} from '@jest/globals';
+import { SubscriptionDao } from '../backend/dao/SubscriptionDao.ts';
+
+require('dotenv').config();
+
+describe('SubscriptionDao', () => {
+
+    test('parseRow', async () => {
+        const subId = 34;
+        const someCustomerId = 'cus_SomeCustomerId'
+        const somePlanId = 'plan_SomePalInd'
+        const now = new Date()
+
+        const row = {
+            id: subId,
+            customer_id: someCustomerId,
+            plan_id: somePlanId,
+            period_end: now.getTime() / 1000,
+            ended_at: null
+        }
+        const subscription = SubscriptionDao.parseRow(row)
+        expect(subscription.id).toBe(subId);
+        expect(subscription.customerId).toBe(someCustomerId);
+        expect(subscription.planId).toBe(somePlanId);
+        expect(subscription.periodEnd?.getTime()).toBe(now.getTime());
+        expect(subscription.endedAt).toBeUndefined();
+    })
+});
+
