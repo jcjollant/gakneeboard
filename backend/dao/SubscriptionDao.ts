dd9a49b50cb8f2d6f4c216e83f5d61599ab1840e
JC Jollant <jc@jollant.net>
2025-02-23 08:10:36 -0800

    Subscription updates

--- backend/dao/SubscriptionDao.ts
+++ backend/dao/SubscriptionDao.ts
@@ -1,39 +1,53 @@
 import { sql } from "@vercel/postgres"
 import { Subscription } from "../models/Subscription"
-import { UserDao } from "./UserDao"
 
 export class SubscriptionDao {
-    
     /**
-     * Update an existing subscription
+     * Create a new subscription record
      * @param subscriptionId 
      * @param customerId 
      * @param planId 
-     * @returns 
+     * @param periodEnd End of the current billing period
+     * @returns The corresponding subscription
      */
-    public static async update(subscriptionId:string, customerId:string, planId:string|undefined, userId:number|undefined):Promise<Subscription> {
+    public static async update(subscriptionId:string, customerId:string, planId:string, periodEnd:number, cancelAt:number|null):Promise<Subscription> {
         // console.log('[SubsriptionDao.update] subscriptionId', subscriptionId, 'customerId', customerId, 'planId', planId, 'userId', userId)
         return new Promise<Subscription>(async (resolve, reject) => {
-            if(!planId && !userId) reject( 'Need either plan id or user id provided')
-
-            const result = await sql`SELECT plan_id, user_id FROM subscriptions WHERE id=${subscriptionId}`
             // console.log('[SubsriptionDao.update] matches', result.rowCount)
-            let sub:Subscription = new Subscription(subscriptionId, customerId, planId, userId);
-            if(result.rowCount > 0) { // update
-                if(planId && userId) {
-                    await sql`UPDATE subscriptions SET plan_id=${planId}, user_id=${userId} WHERE id=${subscriptionId}`
-                } else if( planId) {
-                    await sql`UPDATE subscriptions SET plan_id=${planId} WHERE id=${subscriptionId}`
-                    sub.userId = result.rows[0].user_id
-                } else {
-                    await sql`UPDATE subscriptions SET user_id=${userId} WHERE id=${subscriptionId}`
-                    sub.planId = result.rows[0].plan_id
-                }
-            } else { // initial creation
-                await sql`INSERT INTO subscriptions (id, customer_id, plan_id, user_id) VALUES (${subscriptionId},${customerId},${planId},${userId})`
-                console.log('[SubsriptionDao.update] created new subscription ' + subscriptionId)
+            let sub:Subscription = new Subscription(subscriptionId, customerId, planId, periodEnd);
+            const result = await sql`SELECT id FROM subscriptions WHERE id=${subscriptionId}`
+            if(result.rowCount == 0) {
+                await sql`INSERT INTO subscriptions (id, customer_id, plan_id, period_end, cancel_at) VALUES (${subscriptionId},${customerId},${planId},${periodEnd}, ${cancelAt})`
+                console.log('[SubscriptionDao.create] created new subscription ')
+            } else {
+                await sql `UPDATE subscriptions SET customer_id=${customerId}, plan_id=${planId}, period_end=${periodEnd}, cancel_at=${cancelAt} WHERE id=${subscriptionId}`
+                console.log('[SubscriptionDao.create] updated subscription ' + subscriptionId)
             }
+
             resolve(sub)
         })
     }
+
+    public static async updateEndedAt(subscriptionId:string, endedAt:number|null):Promise<Subscription> {
+        return new Promise<Subscription>(async (resolve, reject) => {
+            try {
+                const result = await sql`SELECT * FROM subscriptions WHERE id=${subscriptionId}`
+                if(result.rowCount == 0) {
+                    reject('Subscription not found')
+                }
+                await sql`UPDATE subscriptions SET ended_at=${endedAt} WHERE id=${subscriptionId}`
+                const sub = SubscriptionDao.subscriptionFromRow(result.rows[0]);
+                sub.setEnededAt(endedAt ? endedAt : 0)
+                resolve( sub)
+            } catch(e) {
+                reject(e)
+            }
+        })
+    }
+
+    // create a Subscription from a result row (*)
+    private static subscriptionFromRow(row:any):Subscription {
+        return new Subscription(row.id, row.customer_id, row.plan_id, row.period_end, row.ended_at)
+    }
+
 }
--- backend/models/Subscription.ts
+++ backend/models/Subscription.ts
@@ -1,13 +1,20 @@
 export class Subscription {
     id:string;
     customerId:string;
-    planId:string|undefined;
-    userId:number|undefined;
+    planId:string;
+    periodEnd:Date|undefined;
+    endedAt:Date|undefined;
 
-    constructor(id:string, customerId:string, planId:string|undefined=undefined, userId:number|undefined=undefined) {
+    constructor(id:string, customerId:string, planId:string, periodEnd:number=0, endedAt:number=0) {
         this.id = id;
         this.customerId = customerId;
         this.planId = planId;
-        this.userId = userId;
+        this.periodEnd = periodEnd ? new Date( periodEnd * 1000) : undefined;
+        this.setEnededAt(endedAt)
     }
+
+    setEnededAt(timestamp:number) {
+        this.endedAt = timestamp ? new Date(timestamp * 1000) : undefined;
+    }
+
 }
