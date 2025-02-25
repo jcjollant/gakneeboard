8a72122ca8df69043c19c5fbb2e5a61f712e0e2e
JC Jollant <jc@jollant.net>
2025-02-24 21:24:04 -0800

    Test backend tag during test mode

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="menu">
+    <div class="menu" >
         <div class="left">
             <Logo @click="router.push('/')" />
             <font-awesome-icon v-if="name" class="icon" icon="fa-chevron-right" />
             <div v-if="name" title="Active Template Name" class="templateName">{{name}}</div>
+            <div v-if="test" class="test">Test Backend</div>
         </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 // import { ref } from 'vue'
+import { onMounted, ref } from 'vue';
 import { useRouter } from 'vue-router'
+import { GApiUrl } from '../../lib/GApiUrl';
 import Logo from './Logo.vue';
 
 const emits = defineEmits(['about'])
@@ -18,7 +21,12 @@ const props = defineProps({
     name: String, default: null
 })
 const router = useRouter()
+const test = ref(false)
 
+onMounted(() => {
+    test.value = GApiUrl.root.indexOf('localhost') > -1
+    console.log('menu mounted')
+})
 
 </script>
 
@@ -45,6 +53,14 @@ const router = useRouter()
     padding: 0 10px;
     width: 100%;
     background-color: lightgrey;
+}
+.test {
+    border-radius: 5px;;
+    font-size: 0.8rem;
+    font-weight: 500;
+    padding: 2px 5px;
+    color: white;
+    background-color: red;
 }
 .right {
     display: flex;
