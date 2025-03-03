8bd3d67efcc9944b1b69057a7fefcb1342505710
JC Jollant <jc@jollant.net>
2025-03-02 18:51:04 -0800

    Refreshed account types

--- backend/models/AccountType.ts
+++ backend/models/AccountType.ts
@@ -1,7 +1,8 @@
 export enum AccountType {
     beta = 'beta', 
-    private = 'pp', 
-    instrument = 'ip',
+    // private = 'pp', 
+    // instrument = 'ip',
+    hobbs = 'hob',
     simmer='sim',
     unknown = "?"
 }
