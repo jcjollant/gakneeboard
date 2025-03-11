39b2b45c4f0d930baa5cad4293607df4434675f8
JC Jollant <jc@jollant.net>
2025-03-11 06:14:37 -0700

    Google analytics limited to prod

--- src/main.js
+++ src/main.js
@@ -55,3 +55,10 @@ createApp(App)
     .use(router)
     .component('font-awesome-icon', FontAwesomeIcon)
     .mount('#app')
+
+if(process.env.NODE_ENV === 'production') {
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){ dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-M7NJWLEVMG'); 
+}
