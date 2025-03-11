7cef29cf3d81cf20014f6190c9856d33ea6982f2
JC Jollant <jc@jollant.net>
2025-03-10 19:48:00 -0700

    Update is not static anymore

--- backend/dao/SubscriptionDao.ts
+++ backend/dao/SubscriptionDao.ts
@@ -1,6 +1,7 @@
 import { sql } from "@vercel/postgres"
 import { Subscription } from "../models/Subscription"
 import { Dao } from "./Dao";
+import { table } from "console";
 
 export class SubscriptionDao extends Dao<Subscription> {
     protected tableName: string = 'subscriptions';
@@ -9,21 +10,22 @@ export class SubscriptionDao extends Dao<Subscription> {
      * Create a new subscription record
      * @param subscriptionId 
      * @param customerId 
-     * @param planId 
+     * @param priceId 
      * @param periodEnd End of the current billing period
      * @returns The corresponding subscription
      */
-    public static async update(subscriptionId:string, customerId:string, planId:string, periodEnd:number, cancelAt:number|null, endedAt:number|null):Promise<Subscription> {
+    public async update(subscriptionId:string, customerId:string, priceId:string, periodEnd:number, cancelAt:number|null, endedAt:number|null):Promise<Subscription> {
         // console.log('[SubsriptionDao.update] subscriptionId', subscriptionId, 'customerId', customerId, 'planId', planId, 'userId', userId)
         return new Promise<Subscription>(async (resolve, reject) => {
             // console.log('[SubsriptionDao.update] matches', result.rowCount)
-            let sub:Subscription = new Subscription(subscriptionId, customerId, planId, periodEnd);
-            const result = await sql`SELECT id FROM subscriptions WHERE id=${subscriptionId}`
+            let sub:Subscription = new Subscription(subscriptionId, customerId, priceId, periodEnd);
+            const result = await this.db.query(`SELECT id FROM ${this.tableName} WHERE id='${subscriptionId}'`)
             if(result.rowCount == 0) {
-                await sql`INSERT INTO subscriptions (id, customer_id, plan_id, period_end, cancel_at, ended_at) VALUES (${subscriptionId},${customerId},${planId},${periodEnd}, ${cancelAt}, ${endedAt})`
+                await this.db.query(`INSERT INTO ${this.tableName} (id, customer_id, price_id, period_end, cancel_at, ended_at) VALUES ('${subscriptionId}','${customerId}','${priceId}',${periodEnd}, ${cancelAt}, ${endedAt})`)
+                sub.setBrandNew(true)
                 // console.log('[SubscriptionDao.create] created new subscription ')
             } else {
-                await sql `UPDATE subscriptions SET customer_id=${customerId}, plan_id=${planId}, period_end=${periodEnd}, cancel_at=${cancelAt}, ended_at=${endedAt} WHERE id=${subscriptionId}`
+                await this.db.query(`UPDATE ${this.tableName} SET customer_id='${customerId}', price_id='${priceId}', period_end=${periodEnd}, cancel_at=${cancelAt}, ended_at=${endedAt} WHERE id='${subscriptionId}'`)
                 // console.log('[SubscriptionDao.create] updated subscription ' + subscriptionId)
             }
 
