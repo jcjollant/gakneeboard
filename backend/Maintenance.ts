4464df669e01006b4663eb57defca3656a9e7d6f
JC Jollant <jc@jollant.net>
2025-03-10 19:54:07 -0700

    willie has his own method

--- backend/Maintenance.ts
+++ backend/Maintenance.ts
@@ -1,6 +1,11 @@
-import { HealthCheck } from './HealthChecks'
+import { Check, HealthCheck } from './HealthChecks'
 import { Metrics } from '../backend/Metrics'
 import { UserMiniView } from './models/UserMiniView';
+import { Business } from './business/Business';
+import { sql } from '@vercel/postgres';
+import { UserDao } from './dao/UserDao';
+import { Email, EmailType } from './Email';
+import { send } from 'process';
 
 export class Maintenance {
     code:string;
@@ -21,13 +26,14 @@ export class Maintenance {
 
     async perform():Promise<any> {
         return new Promise<any>((res,rej) => {
-            if(this.code == Maintenance.codeHousekeeping) {
+            if(this.code == Maintenance.codeHousekeeping) { // This is Willie
+            Maintenance.willie()
                 HealthCheck.perform().then( (result) => {
                     res(result)
                 }).catch(e => {
                     rej(e)
                 })
-            } else if(this.code == Maintenance.codeLogin) {
+            } else if(this.code == Maintenance.codeLogin) { 
                 const hash =  "357c3920bbfc6eefef7e014ca49ef12c78bb875c0826efe90194c9978303a8d3"
                 UserMiniView.fromHash(hash).then( (umv:UserMiniView|undefined) => {
                     if(umv) {
@@ -38,7 +44,7 @@ export class Maintenance {
                 }).catch(e => {
                     rej(e)
                 })
-            } else if(this.code == Maintenance.codeMetrics) {
+            } else if(this.code == Maintenance.codeMetrics) { // This is Waylon
                 Metrics.perform().then( () =>{
                     res( 'OK' )
                 }).catch(e => {
@@ -50,5 +56,34 @@ export class Maintenance {
                 rej( 'Invalid Code')
             }
         })
+    }
+
+    static async willie(sendEmail:boolean=true, persistRecord:boolean=true) {
+        let failedChecks:number = 0
+        let messages:string[] = []
+        await Promise.all([
+            HealthCheck.perform().then( async (allChecks) => {
+                const data:string = JSON.stringify(allChecks)
+                // console.log( '[HealthCheck.perform]', data, 'failures', failedChecks)
+                failedChecks = allChecks.filter((check) => check.status === Check.FAIL).length
+                messages.push('Found ' + failedChecks + ' fail(s)')
+                messages.push(data)
+                // save record
+                if(persistRecord) {
+                    await sql`INSERT INTO health_checks (data,failures) VALUES (${data},${failedChecks})`
+                }
+            }).catch(e => {
+                return e
+            }),
+            Business.printRefills(new UserDao()).then( r => {
+                messages.push('Refilled ' + r.length + ' users')
+            })])
+
+        // cook email with business
+        if(sendEmail) {
+            await Email.send(messages.join('\n\n'), EmailType.Housekeeping)
+        } else {
+            console.log('[Maintenance.willie] not sending email \n' + messages.join('\n\n'))
+        }
     }
 }
