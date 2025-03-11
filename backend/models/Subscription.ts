b69a802eb1985c298928edf652116c17773dd9ac
JC Jollant <jc@jollant.net>
2025-03-10 19:52:39 -0700

    added brand new field

--- backend/models/Subscription.ts
+++ backend/models/Subscription.ts
@@ -1,20 +1,29 @@
 export class Subscription {
     id:string;
     customerId:string;
-    planId:string;
+    priceId:string;
     periodEnd:Date|undefined;
     endedAt:Date|undefined;
+    private brandNew:boolean;
 
     constructor(id:string, customerId:string, planId:string, periodEnd:number=0, endedAt:number=0) {
         this.id = id;
         this.customerId = customerId;
-        this.planId = planId;
+        this.priceId = planId;
         this.periodEnd = periodEnd ? new Date( periodEnd * 1000) : undefined;
         this.setEnededAt(endedAt)
+        this.brandNew = false;
+    }
+
+    isBrandNew():boolean {
+        return this.brandNew;
     }
 
     setEnededAt(timestamp:number) {
         this.endedAt = timestamp ? new Date(timestamp * 1000) : undefined;
     }
 
+    setBrandNew(value:boolean) {
+        this.brandNew = true;
+    }
 }
