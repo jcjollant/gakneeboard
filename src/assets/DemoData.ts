42091ac4463f118c0005749811320a5a0425d688
JC Jollant <jc@jollant.net>
2025-03-12 19:53:43 -0700

    New fiel with demo data

---
+++ src/assets/DemoData.ts
@@ -0,0 +1,66 @@
+import { FrequencyType } from "../model/Frequency"
+import { Template, TemplatePage } from "../model/Template"
+import { PageType } from "./PageType"
+
+export class DemoData {
+
+    static demoRadioData = [
+        {mhz:116.8,name:'SEA VOR',type:FrequencyType.navaid},
+        {mhz:113.4,name:'OLM VOR',type:FrequencyType.navaid},
+        {mhz:124.7,name:'KRNT CTAF', type:FrequencyType.ctaf},
+        {mhz:126.95,name:'KRNT ATIS',type:FrequencyType.weather},
+        {mhz:123.0,name:'S43 CTAF',type:FrequencyType.ctaf},
+        {mhz:128.65,name:'KPAE',type:FrequencyType.weather},
+        {mhz:120.2,name:'PAE TWR 34R', type:FrequencyType.tower},
+        {mhz:132.95,'name':'PAE TWR 34L', type:FrequencyType.tower}
+    ]
+
+
+    static page0DemoTiles = new TemplatePage( PageType.tiles, "Tiles Demo",
+        [
+            {id:0,name:'airport','data':{'code':'krnt','rwy':'16-34'}},
+            {id:1,name:'airport','data':{'code':'kbfi','rwy':'14L-32R'}},
+            {id:2,name:'airport','data':{'code':'w39','rwy':'NE-SW','rwyOrientation':'magnetic'}},
+            {id:3,name:'airport','data':{'code':'O26','rwy':'13-31'}},
+            {id:4,name:'atis','data':{}},
+            {id:5,name:"clearance","data":{mode:'dep',airport:'kbfi'}}
+        ]) 
+
+    static page1DemoTiles = new TemplatePage( PageType.tiles, "Tiles",
+        [
+            {'id':0,'name':'checklist','data':{name:'Power OFF stalls', items:[{"c":"Clearing Turns+Calls","r":"Made"},{"c":"Visual Reference","r":"Bugged"},{"c":"Altitude","r":"3,000"},{"c":"Power=1,600 Flaps > Full"},{"c":"Hold 65 3s, Level off until stall"},{"c":"Full Power + Right Rudder"},{"c":"Flaps 20 > 10 > 0"},{"c":"ACS HDG/Bank","r":"±10°/20°"}],theme:"blue"}},
+            {'id':1,'name':'airport','data':{'code':'kawo','rwy':'all'}},
+            {'id':2,'name':'sunlight','data':{'from':'KRNT','to':'KSFF'}},
+            {'id':3,'name':'fuel'},
+            {'id':4,'name':'notes','data':{}},
+            {'id':5,'name':'radios','data':DemoData.demoRadioData},
+        ])
+
+    static page0DemoDefault = new TemplatePage( PageType.tiles, 'Default',
+        [
+            {id:0,name:"airport","data":{"code":"KBFI","rwy":"14L-32R","rwyOrientation":"magnetic","corners":["weather","twr","field","tpa"]}},
+            {id:1,name:"airport","data":{"code":"KSFF","rwy":"04L-22R","rwyOrientation":"vertical","corners":["weather","twr","field","tpa"],"pattern":2}},
+            {id:2,name:"radios","data":DemoData.demoRadioData},
+            {id:3,name:"notes","data":{}},
+            {id:4,name:"atis","data":{}},
+            {id:5,name:"clearance","data":{mode:'dep',airport:'kbfi'}}
+        ])
+
+    static page1DemoDefault = new TemplatePage( PageType.checklist, 'Default', 
+        {
+            "name":"Flight",
+            "items":[{"s":"Climb","t":"strong"},{"c":"Power","r":"FULL"},{"c":"Mixture","r":"RICH"},{"c":"Flaps","r":"UP"},{"c":"Engine","r":"GREEN"},{"s":"Approach","t":"strong"},{"c":"Direct","r":"SET"},{"c":"ATIS","r":"GET"},{"c":"Altimeter","r":"SET"},{"c":"RWY HDG","r":"SET"},{"c":"Calls","r":"MADE"},{"c":"Briefing","r":"DONE"},{"s":"Engine FAILURE","t":"emer"},{"c":"Airspeed","r":"68"},{"c":"Fuel Pump","r":"ON"},{"c":"Mixture","r":"RICH"},{"c":"Fuel Shutoff","r":"ON"},{"c":"Fuel Selector","r":"BOTH"},{"s":"Engine FIRE","t":"emer"},{"c":"Mixture","r":"CUTOFF"},{"c":"Fuel Shutoff","r":"OFF"},{"c":"Fuel Pump","r":"OFF"},{"c":"Masters","r":"OFF"},{"c":"Vents, Heat, Air","r":"CLOSED"},{"c":"Emergency Descent","r":"120@30"},{"c":""}],
+            "items2":[{"s":"Cruise","t":"strong"},{"c":"Power","r":"SET"},{"c":"Flaps","r":"UP"},{"c":"Trim","r":"SET"},{"c":"Heading","r":"BUGGED"},{"s":"Before Landing","t":"strong"},{"c":"Fuel Selector","r":"BOTH"},{"c":"Mixture","r":"RICH"},{"c":"Landing Lights","r":"ON"},{"c":"Safety Belts","r":"ON"},{"c":"Auto Pilot","r":"OFF"},{"c":"Cabin Power","r":"OFF"},{"s":"After Landing"},{"c":"Flaps","r":"UP"},{"c":"Light","r":"TAXI"},{"c":"Mixture","r":"Lean"},{"c":"Trim","r":"T/O"},{"c":"Pitot","r":"OFF"},{"c":"Comms","r":"GND"},{"s":"Shutdown"},{"c":"Avionics","r":"OFF"},{"c":"Mixture","r":"IDLE"},{"c":"Magnetos","r":"OFF"},{"c":"Electrical Equipment","r":"OFF"},{"c":"Hobbs & Tach","r":"RECORD"},{"c":"Master & Stby","r":"OFF"}],
+            "theme":"blue"
+        }
+    )
+
+    static default():Template {
+        return new Template('Default Demo', 'Six Tiles and a Flight Checklist', false, [DemoData.page0DemoDefault, DemoData.page1DemoDefault])
+    }
+
+    static tiles():Template {
+        return new Template('Tiles Demo', 'Every Tile Available on GA Kneeboard', false, [DemoData.page0DemoTiles,DemoData.page1DemoTiles])
+    } 
+    
+}
