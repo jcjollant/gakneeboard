13fa2967e098464d10577a65bc7fb84266e55b1a
JC Jollant <jc@jollant.net>
2025-03-13 06:37:34 -0700

    newTestUser can take userId

--- test/common.ts
+++ test/common.ts
@@ -32,11 +32,11 @@ export function getMockBrandNewSubscription():Subscription {
     return mockSubscription;
 }
 
-export function newTestUser():User {
+export function newTestUser(userId:number=0):User {
     const userName = Math.random().toString(36).substring(5)
     const newEmail = Math.random().toString(36).substring(7) + '@test.com'
     const newHash = UserTools.createUserSha('test', newEmail)
-    const newUser = new User(0, newHash)
+    const newUser = new User(userId, newHash)
     newUser.email = newEmail
     newUser.name = userName
     // assign random value between 1 and 25
