a32cdaedffe2da9f9354436ab5c7c169976f437a
JC Jollant <jc@jollant.net>
2025-03-12 19:55:14 -0700

    Added time as a parameter

--- src/assets/Toaster.ts
+++ src/assets/Toaster.ts
@@ -11,20 +11,20 @@ export class Toaster {
         this.toast = toast
     }
 
-    error(title:string, message:string) {
-        this.toast.add({ severity: 'error', summary: title, detail: message, life: 3000 });
+    error(title:string, message:string, life:number=3000) {
+        this.toast.add({ severity: 'error', summary: title, detail: message, life: life });
     }
 
-    info(title:string, message:string) {
-        this.toast.add({ severity: 'info', summary: title, detail: message, life: 3000 });
+    info(title:string, message:string, life:number=3000) {
+        this.toast.add({ severity: 'info', summary: title, detail: message, life: life });
     }
 
-    success(title:string, message:string) {
-        this.toast.add({ severity: 'success', summary: title, detail: message, life: 3000 });
+    success(title:string, message:string, life:number=3000) {
+        this.toast.add({ severity: 'success', summary: title, detail: message, life: life });
     }
 
-    warning(title:string, message:string) {
-        this.toast.add({ severity: 'warn', summary: title, detail: message, life: 3000 });
+    warning(title:string, message:string, life:number=3000) {
+        this.toast.add({ severity: 'warn', summary: title, detail: message, life: life });
     }
 
 }
