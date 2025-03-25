a6b0e6ac5a4beead633c0251aaa45cc79e27abc2
JC Jollant <jc@jollant.net>
2025-03-24 19:45:20 -0700

    Using URL

--- backend/AirportSketch.ts
+++ backend/AirportSketch.ts
@@ -69,7 +69,11 @@ export class AirportSketch {
     try {
       const pdfjsLib = await import("pdfjs-dist/legacy/build/pdf.min.mjs");
       // const value = new URL('./pdf.worker.min.mjs', 'file://').toString();
-      const value = 'pdfjs-dist/legacy/build/pdf.worker.min.mjs'
+      // const value = 'pdfjs-dist/legacy/build/pdf.worker.min.mjs'
+      // const value = '//mozilla.github.io/pdf.js/build/pdf.worker.mjs'
+      // const value = new URL(`//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.mjs`).toString();
+      // const value = new URL(`//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.mjs`).toString();
+      const value = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjsLib.version}/pdf.worker.min.mjs`;
       // const value = 'pdf.worker.min.mjs'
       console.log('[AirportSketch.pdfFirstPageToPng] worker', value);
       pdfjsLib.GlobalWorkerOptions.workerSrc = value
