cfa3faf688f03526735edfecc86869b8b87b7539
JC Jollant <jc@jollant.net>
2025-04-03 12:55:25 -0700

    Dead import

--- test/Exporter.test.ts
+++ test/Exporter.test.ts
@@ -1,6 +1,5 @@
 import { describe, expect, test} from '@jest/globals';
 import { Exporter } from '../backend/Exporter';
-import { Template } from '../backend/models/Template';
 import { TemplateView } from '../backend/models/TemplateView';
 
 const templateData:any = [{"type":"checklist","data":{"name":"Checklist Page","items":[{"c":"a","r":"b"},{"c":"c","r":"d"}],"items2":[{"c":"e","r":"f"},{"c":"g","r":"h"}],"theme":"yellow"}},{"type":"tiles","data":[{"id":0,"name":"checklist","data":{"name":"Checklist Tile A","items":[{"c":"i","r":"j"},{"c":"k","r":"l"}],"theme":"blue"}},{"id":1,"name":"atis","data":{}},{"id":2,"name":"atis","data":{}},{"id":3,"name":"atis","data":{}},{"id":4,"name":"atis","data":{}},{"id":5,"name":"checklist","data":{"name":"Checklist Tile B","items":[{"c":"m","r":"n"},{"c":"o","r":"p"}],"theme":"blue"}}]}]
