dd2c72b8e066ec78d566b8185d656356065f57a2
JC Jollant <jc@jollant.net>
2025-04-08 11:31:26 -0700

    Added isTest method

--- src/components/menu/Menu.vue
+++ src/components/menu/Menu.vue
@@ -24,7 +24,7 @@ const router = useRouter()
 const test = ref(false)
 
 onMounted(() => {
-    test.value = GApiUrl.root.indexOf('localhost') > -1
+    test.value = GApiUrl.isTest()
     // console.log('[Menu.onMounted]')
 })
 
