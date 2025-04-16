d21258c0a1c34d8934c750670bb6ae44d2a70384
JC Jollant <jc@jollant.net>
2025-04-16 10:22:39 -0700

    Manually querying Metrics

--- housekeeping/manual.ts
+++ housekeeping/manual.ts
@@ -5,7 +5,11 @@ dotenv.config()
 //================
 // Metrics
 //================
-// import { Metrics } from "../backend/Metrics";
+import { Metrics } from "../backend/Metrics";
+Metrics.perform().then( metrics => {
+    for(const metric of metrics)
+        console.log(metric.name, metric.value)
+})
 // Metrics.airports().then(metrics => {
 //     for(const metric of metrics)
 //         console.log(metric.name, metric.value)
@@ -93,47 +97,3 @@ dotenv.config()
 // import { Adip } from "../backend/adip/Adip";
 // import { AirportDao } from "../backend/AirportDao";
 // AirportDao.readCurrent(Adip.currentEffectiveDate).then(list => console.log(list.map(a => a.code).join(',')))
-
-
-//===============================================================================
-// Update all airports that do not have a sketch, with version 15 and are current
-import { sql } from "@vercel/postgres";
-import { AirportSketch } from "../backend/AirportSketch";
-import { AirportDao } from "../backend/AirportDao";
-import { Airport } from "../backend/models/Airport";
-import { Adip } from "../backend/adip/Adip";
-// declare and execute
-async function doIt() {
-    let updated = 0;
-    const response = await sql`select * from airports where sketch ISNULL and version != -1`
-    // const response = await sql`SELECT * FROM airports where sketch isnull and version=15`
-    console.log('found rows', response.rowCount)
-    await new Promise(resolve => setTimeout(resolve, 2000))
-    for(const row of response.rows) {
-        const airport = AirportDao.parse(row)
-        airport.code = row.code
-        console.log('Getting', airport.code)
-        // if(airport.effectiveDate != Adip.currentEffectiveDate) {
-        //     console.log('skipping', airport.code)
-        //     continue;
-        // }
-        if(!airport.iap || airport.iap.length < 1) {
-            console.log('skipping', airport.code)
-            continue;
-        }
-        const before = airport.iap[0].pdf
-        const iap = before.split('/')[1]
-        airport.iap[0].pdf = '2503/' + iap
-        console.log('airport', airport.code, 'before', before, 'after', airport.iap[0].pdf)
-        await AirportSketch.resolve(airport)
-        updated++;
-        // wait random time between 1 and 5 seconds
-        const time = Math.floor(Math.random() * 4000) + 3000
-        console.log('updated', updated, 'out of', response.rowCount)
-        console.log('Waiting', time, 'ms')
-        await new Promise(resolve => setTimeout(resolve, time))
-    }
-}
-doIt().then(() => {
-    console.log('done')
-})
