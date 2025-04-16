c2ec889fa9d9f672994d857ae9521a1391c2a371
JC Jollant <jc@jollant.net>
2025-04-15 20:33:58 -0700

    v5162

--- Release.md
+++ Release.md
@@ -1,4 +1,6 @@
 # User Experience updates
+## 04/15/2025 5162
+* Fixed Prints on iPad via PDF
 ## 04/12/2025 5161
 * Removed feedback from prints
 ## 04/11/2025 5160
--- src/assets/data.js
+++ src/assets/data.js
@@ -1,4 +1,4 @@
-export const version = 5161
+export const version = 5162
 import axios from 'axios'
 import { Airport } from '../model/Airport.ts'
 import { Backend } from './Backend.ts'
