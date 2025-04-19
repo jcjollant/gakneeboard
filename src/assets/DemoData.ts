5491d1e2c49c3fb22016f21756b91d1f1e8b8eee
JC Jollant <jc@jollant.net>
2025-04-19 13:48:21 -0700

    Added demoNavlog to list

--- src/assets/DemoData.ts
+++ src/assets/DemoData.ts
@@ -3,6 +3,7 @@ import { FrequencyType } from "../model/Frequency"
 import { Template, TemplatePage } from "../model/Template"
 import { TileType } from "../model/TileType"
 import { PageType } from "./PageType"
+import { SheetName } from "./sheetData"
 
 export class DemoData {
 
@@ -118,6 +119,49 @@ export class DemoData {
         ]
     )
 
+    static page0DemoNavlog = new TemplatePage( PageType.navLog, 'Front Page', {
+          "from":"KRNT",
+          "to":"KELN",
+          "tt":52.68333333333334,
+          "td":77.80000000000001,
+          "entries":[
+              {"name":"KRNT","alt":32,"th":144,"mh":129,"ld":4.2,"lt":3.5,"lf":2.4,"att":"+","gs":71,"fr":53},
+              {"name":"TOC25","alt":2500,"th":143,"mh":128,"ld":2.9,"lt":1.65,"lf":0.2,"gs":106,"fr":50.6},
+              {"name":"Lk Youngs Zebku","alt":2500,"th":90,"mh":75,"ld":4,"lt":3.5,"lf":0.7,"att":"+","gs":68,"fr":50.4},
+              {"name":"TOC45","alt":4500,"th":90,"mh":75,"ld":1,"lt":0.6166666666666667,"lf":0.1,"gs":101,"fr":49.699999999999996},
+              {"name":"Clear B Sh50","alt":4500,"th":90,"mh":75,"ld":2.2,"lt":2,"lf":0.4,"att":"+","gs":67,"fr":49.599999999999994},
+              {"name":"TOC55","alt":5500,"th":90,"mh":75,"ld":4.5,"lt":2.8,"lf":0.4,"gs":96,"fr":49.199999999999996},
+              {"name":"Clear B Sh60","alt":5500,"th":90,"mh":75,"ld":4.4,"lt":4,"lf":0.8,"att":"+","gs":66,"fr":48.8},
+              {"name":"TOC75","alt":7500,"th":90,"mh":75,"ld":7.6,"lt":4.9,"lf":0.6,"gs":94,"fr":48},
+              {"name":"Bandera 4W0","alt":7500,"th":90,"mh":75,"ld":28.8,"lt":18.25,"lf":2.4,"gs":95,"fr":47.4},
+              {"name":"TOD","alt":7500,"th":81,"mh":66,"ld":18.2,"lt":11.466666666666667,"lf":1.5,"att":"-","gs":95,"fr":45},
+              {"name":"KELN","alt":1763.2,"ld":0,"lt":0,"lf":0}
+          ],
+          ff:53,
+          fr:13.5,
+          ft:43.5,
+          mv:-15,
+          md:0,
+          cta: 105,
+          cff: 9,
+          dr: 500,
+          dff :6,
+        })
+
+      
+    static page1DemoNavlog = new TemplatePage( PageType.tiles, 'Back Page', [
+        {"id":0,"name":"airport","data":{"code":"krnt","rwy":"16-34"}},
+        {"id":1,"name":"atis","data":{"mode":"compact"}},
+        {"id":2,"name":"radios","data":[{"mhz":"122.9","name":"CTAF"},{"mhz":"135.275","name":"Stamp. Pass"},{"mhz":"117.9","name":"ELN VOR"},{"mhz":"123","name":"KELN CTAF"},{"mhz":"116.8","name":"SEA VORTAC"},{"mhz":"126.95","name":"KRNT ATIS"},{"mhz":"124.7","name":"KRNT TWR"}]},
+        {"id":3,"name":"notes","data":{}},
+        {"id":4,"name":"airport","data":{"code":"KELN","rwy":"11-29","rwyOrientation":"magnetic","corners":["weather","twr","field","tpa"]}},
+        {"id":5,"name":"navlog","data":{}}
+    ])
+
+    static blank():Template {
+        return new Template('Blank','', false, [TemplatePage.SELECTION,TemplatePage.SELECTION])
+    }
+
     static charts():Template {
         const page0DemoCharts = new TemplatePage( PageType.diagram, 'Airport Diagram', {airport:"KRNT"})
         const page1DemoCharts = new TemplatePage( PageType.approach, 'Approach Plate', {airport:"KRNT",pdf:0})
@@ -132,6 +176,29 @@ export class DemoData {
         return new Template('Default Demo', 'Six Tiles and a Flight Checklist', false, [DemoData.page0DemoDefault, DemoData.page1DemoDefault])
     }
 
+    static fromName(name:string):Template|undefined {// turn a default name into its data or null if the name is unkown
+        if( name == SheetName.default) {
+            return DemoData.default()
+        } else if( name == SheetName.tiles) {
+            return DemoData.tiles()
+        } else if( name == SheetName.checklist) {
+            return DemoData.checklist()
+        } else if( name == SheetName.navlog) {
+            return DemoData.navlog()
+        } else if( name == SheetName.new) { 
+            return DemoData.blank()
+        } else if( name == SheetName.skyhawk) { 
+            return DemoData.skyhawk()
+        } else if( name == SheetName.charts) { 
+            return DemoData.charts()
+        } else if( name == SheetName.holds) { 
+            return DemoData.holds()
+        } else if( name == SheetName.ifrflight) { 
+            return DemoData.ifrflight()
+        }
+        return undefined;
+    }
+
     static holds():Template {
         return new Template('Holds Demo', 'Two sheets of Holds and Compass', false, Array(4).fill(DemoData.page0DemoHold))
     }
@@ -139,6 +206,10 @@ export class DemoData {
     static ifrflight():Template {
         const pageDemoIFRFlight = new TemplatePage(PageType.strips, 'Strips', {list:['radio','atis','craft','notes','radio','atis','craft','notes']})
         return new Template('IFR Flight', 'Two pages of Strips for IFR Flights', false, [pageDemoIFRFlight,pageDemoIFRFlight])
+    }
+
+    static navlog():Template {
+        return new Template('Navlog Demo', 'Navlog page along with six tiles', false, [DemoData.page0DemoNavlog,DemoData.page1DemoNavlog])
     }
 
     static skyhawk():Template {
