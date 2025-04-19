4256bc7162b69899412a65b8ffb016532b904075
JC Jollant <jc@jollant.net>
2025-04-19 14:45:59 -0700

    New Cosmetic page

---
+++ src/components/page/BlankPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="contentPage blankPage">
+        <Header title="" :stealth="true" :page="true" :displayMode="false"
+            @replace="emits('replace')"></Header>
+    </div>
+
+</template>
+
+<script setup lang="ts">
+import Header from '../shared/Header.vue';
+const emits = defineEmits(['replace'])
+</script>
+
