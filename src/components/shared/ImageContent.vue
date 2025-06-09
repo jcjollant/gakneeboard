4726f021c1fabcd358dca3c04c4f92552d90c6c5
JC Jollant <jc@jollant.net>
2025-06-09 13:59:28 -0700

    New ImageContent component

---
+++ src/components/shared/ImageContent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="vorrange">
+        <img :src="source" class="imageContent" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+
+const source = ref('')
+
+const props = defineProps({
+    src: { type: String, default: ''},
+})
+
+onMounted(() => {
+    loadProps(props)
+})
+
+watch( props, () => {
+    loadProps(props)
+})
+
+
+function loadProps(props:any) {
+    source.value = '/tiles/' + props.src
+}
+
+</script>
+
+<style scoped>
+.imageContent {
+    width: var(--tile-width);
+    object-fit: contain;
+}
+</style>
