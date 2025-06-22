e5ca6a2d24f2b3cf0e92be242b17dbdc721d0a94
JC Jollant <jc@jollant.net>
2025-06-22 13:45:06 -0700

    Added format support

--- src/model/Template.ts
+++ src/model/Template.ts
@@ -1,4 +1,5 @@
 import { PageType } from "../assets/PageType";
+import { TemplateFormat } from "./TemplateFormat";
 
 export class Template {
     id:number
@@ -11,17 +12,21 @@ export class Template {
     code:string
     thumbUrl:string|undefined
     thumbHash:string|undefined
-    constructor(name:string, description:string, publish:boolean=false, data:TemplatePage[]=[], version:number=0) {
+    format:TemplateFormat
+    constructor(name:string, description:string, publish:boolean=false, data:TemplatePage[]=[], format:TemplateFormat=TemplateFormat.Kneeboard, version:number=0) {
         this.id = 0
         this.name = name
         this.desc = description
         this.publish = publish
         this.data = data
         this.pages = data.length
+        this.format = format
         this.ver = version
         this.code = ''
         this.thumbUrl = undefined
         this.thumbHash = undefined
+        
+        // console.log('[Template.constructor] Created template with format:', this.format);
     }
 
     static describe(template:any):string {
@@ -45,24 +50,35 @@ export class Template {
         return !this.isInvalid()
     }
 
-    static noTemplate(pageCount:number = 2): Template {
+    static noTemplate(pageCount:number = 2, format:TemplateFormat = TemplateFormat.Kneeboard): Template {
         const loadingPage = new TemplatePage()
         const data = Array<TemplatePage>(pageCount).fill(loadingPage)
-        const template = new Template('', '', false, data)
-
+        const template = new Template('', '', false, data, format)
+        
+        // console.log('[Template.noTemplate] Created template with format:', template.format, 'and', pageCount, 'pages');
         return template;
     }
 
     static parse(data:any):Template {
-        // console.log('[Template.parse]', data)
-        const template = new Template(data.name, data.desc, data.publish, data.data, data.ver)
+        // console.log('[Template.parse] Input data format:', data.format);
+        // Ensure format is explicitly set and preserved
+        const format = data.format || TemplateFormat.Kneeboard
+        // console.log('[Template.parse] Using format:', format);
+        const template = new Template(data.name, data.desc, data.publish, data.data, format, data.ver)
         template.id = data.id
         template.code = data.code
         template.ver =  data?.ver ?? template.ver
         template.thumbUrl = data?.thumbUrl
         template.thumbHash = data?.thumbHash
         template.pages = data.data.length || data.pages
-
+        
+        // Double-check that format is set correctly
+        if (!template.format) {
+            template.format = format;
+            // console.log('[Template.parse] Format was missing, set to:', template.format);
+        }
+        
+        // console.log('[Template.parse] Final template format:', template.format);
         return template
     }
 }
@@ -89,4 +105,3 @@ export class TemplatePage {
         return new TemplatePage(data.type, data.name, data.data)
     }
 }
-
