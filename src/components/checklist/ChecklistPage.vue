5c6c0758b817ef6b43def1e8b7fa28096761e85a
JC Jollant <jc@jollant.net>
2025-06-22 13:07:50 -0700

    Added font selection and full page support

--- src/components/checklist/ChecklistPage.vue
+++ src/components/checklist/ChecklistPage.vue
@@ -1,27 +1,36 @@
 <template>
-    <div class="contentPage pageChecklist">
+    <div class="contentPage pageChecklist" :class="{'fullpage': isFullPage}">
         <Header :title="page.name" :showReplace="editMode" :page="true"
             @click="onHeaderClick" @replace="emits('replace')"></Header>
         <div v-if="editMode" class="settings">
-            <InputGroup>
-                <InputGroupAddon class="checklistNameAddon">Name</InputGroupAddon>
-                <InputText v-model="page.name" />
-            </InputGroup>
-            <div class="columnsChoice">
-                <div>Columns</div>
-                <OneChoice v-model="columns" :choices="[colSingle, colDouble, colTriple]"/>
+            <div class="nameAndCols">
+                <InputGroup>
+                    <InputGroupAddon class="checklistNameAddon">Name</InputGroupAddon>
+                    <InputText v-model="page.name" />
+                </InputGroup>
+                <div class="columnsChoice">
+                    <div>Cols</div>
+                    <OneChoice v-model="columns" :choices="[colSingle, colDouble, colTriple]"/>
+                </div>
             </div>
             <div class="columns">
                 <Textarea v-for="(checklist, index) in [0,1,2]" v-model="textData[index]" v-show="index < columns.value"
                     :PlaceHolder="index == 0 ? getPlaceHolder() : ''"
-                    :class="['editList', 'textArea' + (index+1), 'text' + columns.value]"></Textarea>
+                    :class="['editList', 'textArea' + (index+1), font]"></Textarea>
             </div>
-            <ThemeSelector @change="onThemeChange" :theme="theme" />
+            <div class="theme">
+                <div>Theme</div>
+                <ThemeSelector @change="onThemeChange" :theme="theme" />
+            </div>
+            <div class="font">
+                <div>Font</div>
+                <FontSizeSelector v-model="font" />
+            </div>
             <ActionBar @cancel="onCancel" @apply="onApply" :help="UserUrl.checklistGuide" />
         </div>
         <div v-else class="viewMode">
             <div class="columns">
-                <ChecklistViewer v-for="(checklist,index) in page.lists" :list="checklist" :theme="theme"
+                <ChecklistViewer v-for="(checklist,index) in page.lists" :list="checklist" :theme="theme" :font="font"
                     :size="columns.value" :class="['list','list'+index]" />
             </div>
             <div v-if="version > 0" class="version">v{{version}}</div>
@@ -30,12 +39,15 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
+import { computed, onMounted, ref, watch } from 'vue'
+import { Checklist } from '../../model/Checklist'
 import { ChecklistPage } from '../../model/ChecklistPage'
 import { UserUrl } from '../../lib/UserUrl'
+import { TemplateFormat } from '../../model/TemplateFormat'
 
 import ActionBar from '../shared/ActionBar.vue'
 import ChecklistViewer from './ChecklistViewer.vue'
+import FontSizeSelector from './FontSizeSelector.vue'
 import Header from '../shared/Header.vue'
 import ThemeSelector from './ThemeSelector.vue'
 
@@ -44,39 +56,43 @@ import InputGroupAddon from 'primevue/inputgroupaddon'
 import InputText from 'primevue/inputtext'
 import Textarea from 'primevue/textarea'
 import OneChoice from '../shared/OneChoice.vue'
-import { Checklist } from '../../model/Checklist'
 
 class Column {
     label: string
     value: number
-    constructor(label: string, value: number) {
+    title: string|undefined
+    constructor(label: string, value: number, title: string|undefined = undefined) {
         this.label = label
         this.value = value
+        this.title = title
     }
 }
-const colSingle = new Column('One', 1)
-const colDouble = new Column('Two', 2)
-const colTriple = new Column('Three', 3)
+const colSingle = new Column('1', 1)
+const colDouble = new Column('2', 2)
+const colTriple = new Column('3', 3)
 const columns = ref<Column>(colSingle)
 const editMode = ref(false)
+// Default to medium font
+const font = ref('font-medium')
+let fontBeforeEdit = 'font-medium'
 const emits = defineEmits(['replace','update'])
 let nameBeforeEdit = ''
 const page = ref<ChecklistPage>(new ChecklistPage())
 const props = defineProps({
     data: { type: Object, default: null },
-    version: { type: Number, default: 0 }
+    version: { type: Number, default: 0 },
+    format: { type: String, default: TemplateFormat.Kneeboard }
 })
+
+const isFullPage = computed(() => props.format === TemplateFormat.FullPage)
 const textData = ref(['','',''])
-// const textData = ref('')
-// const textData2 = ref('')
-// const textData3 = ref('')
 const theme = ref('theme-yellow')
 let themeBeforeEdit = 'theme-yellow'
 const version = ref(props.version)
 
 // Props management
 function loadProps(newProps:any) {
-    // console.log('[ChecklistPage.loadProps]', JSON.stringify(newProps))
+    // console.log('[ChecklistPage.loadProps]', newProps)
     const newPage = new ChecklistPage()
     if (newProps.data) {
         if(newProps.data.name) {
@@ -101,11 +117,16 @@ function loadProps(newProps:any) {
         // console.log('[ChecklistPage.loadProps] columns', columns.value)
         // console.log('[ChecklistPage.loadProps] newPage', newPage)
 
-        // Restore theme
+        // Restore theme and font
         if( 'theme' in newProps.data) {
             theme.value = 'theme-' + newProps.data.theme
         } else {
             theme.value = 'theme-yellow'
+        }
+        if( 'font' in newProps.data) {
+            font.value = 'font-' + newProps.data.font
+        } else {
+            font.value = 'font-medium'
         }
     }
     page.value = newPage
@@ -121,11 +142,18 @@ watch(props, () => {
     loadProps(props)
 })
 
+// watch(fontSize, () => {
+//     console.log('[CheclistPage.watch fontClass]', fontSize.value)
+// })
+
 // End of props management
 
 function getPlaceHolder() {
-    const items = columns.value.value == 1 ? 33 : columns.value.value == 2 ? 35 : 40;
-    return `Up to ${items} items will fit vertically.
+    // Increase the number of items for full page format (approximately 50% more)
+    const items = [[50,66],[46,61],[35,46],[30,40],[26,34]];
+    const fontSizes = ['font-smaller', 'font-small', 'font-medium', 'font-large', 'font-larger']
+    const fontIndex = Math.max(fontSizes.indexOf(font.value),0)
+    return `Up to ${items[fontIndex][isFullPage.value?1:0]} items will fit vertically.
 
 Separate Challenge and Response with '##':
 Master Switch##ON
@@ -148,7 +176,9 @@ function onApply() {
         const key = 'items' + (index == 0 ? '' : index + 1)
         newData[key] = page.value.lists[index].toParams()
     }
+    // simplify values for theme and font
     newData['theme'] = theme.value.replace('theme-', '')
+    newData['font'] = font.value.replace('font-', '')
     // console.log('[ChecklistPage.onApply] newData', newData)
 
     editMode.value = false
@@ -160,6 +190,7 @@ function onCancel() {
     editMode.value = false
     page.value.name = nameBeforeEdit;
     theme.value = themeBeforeEdit;
+    font.value = fontBeforeEdit;
 }
 
 function onHeaderClick() {
@@ -170,6 +201,7 @@ function onHeaderClick() {
     }
     nameBeforeEdit = page.value.name
     themeBeforeEdit = theme.value
+    fontBeforeEdit = font.value
     editMode.value = true
 }
 
@@ -237,16 +269,9 @@ function onThemeChange(newTheme) {
     display: grid;
     gap: 5px;
     padding: 5px;
-    grid-template-rows: 2.5rem 2.5rem auto 2rem 50px;
+    grid-template-rows: 2.5rem auto 2rem 2rem 50px;
 }
 
-.text2 {
-    font-size: 0.8rem;
-}
-
-.text3 {
-    font-size: 0.7rem;
-}
 .twoLists {
     display: grid;
     grid-template-columns: repeat(2, 1fr);
@@ -271,5 +296,20 @@ function onThemeChange(newTheme) {
 }
 .viewMode {
     height: var(--page-content);
+}
+
+.fullpage .viewMode {
+    height: var(--fullpage-content);
+}
+
+.nameAndCols {
+    display: flex;
+    gap: 10px;
+}
+.theme, .font {
+    display: grid;
+    grid-template-columns: 60px auto;
+    gap: 5px;
+    align-items: center;
 }
 </style>
