6a158cf8af05694464c887f7e7fa80422fc2e493
JC Jollant <jc@jollant.net>
2025-06-22 13:38:06 -0700

    Able to show using full page format

--- src/components/page/LoadingPage.vue
+++ src/components/page/LoadingPage.vue
@@ -1,8 +1,16 @@
 <template>
-    <div class="contentPage">
+    <div class="contentPage" :class="{'fullpage': isFullPage}">
         <PlaceHolder title="Loading" subtitle="Page Content Will Show Here"></PlaceHolder>
     </div>
 </template>
 <script lang="ts" setup>
+import { computed } from 'vue';
 import PlaceHolder from '../shared/PlaceHolder.vue';
+import { TemplateFormat } from '../../model/TemplateFormat';
+
+const props = defineProps({
+    format: { type: String, default: TemplateFormat.Kneeboard }
+})
+
+const isFullPage = computed(() => props.format === TemplateFormat.FullPage)
 </script>
