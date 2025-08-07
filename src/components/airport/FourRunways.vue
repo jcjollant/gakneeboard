3b2695153a1ef7390c6bfe5d586ecfadd4719a2a
JC Jollant <jc@jollant.net>
2025-08-06 22:55:29 -0700

    New control to show 4 runways with shared settings

---
+++ src/components/airport/FourRunways.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <Runway v-for="index in [0,1,2,4]" :settings="rwySettings[index]" :small="true"
+            class="clickable" @click="emits('rwyclick',index)"/>
+    </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { RunwaySettings } from './RunwaySettings';
+
+const emits = defineEmits(['rwyclick'])
+const noRunways = new Array<RunwaySettings>(4).fill(new RunwaySettings())
+const rwySettings = ref<RunwaySettings[]>(noRunways)
+
+</script>
+<style scoped>
+.container {
+    display: grid;
+    grid-template-columns: 120px 120px;
+    grid-template-rows: 120px 120px;
+}
+</style>
