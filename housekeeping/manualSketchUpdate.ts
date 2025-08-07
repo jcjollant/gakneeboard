6f8eb6ed7999dbb01a6993753893a9993b4e3bfc
JC Jollant <jc@jollant.net>
2025-08-07 13:20:31 -0700

    Refreshed effective date

--- housekeeping/manualSketchUpdate.ts
+++ housekeeping/manualSketchUpdate.ts
@@ -8,7 +8,7 @@ import { sql } from "@vercel/postgres";
 import { AirportSketch } from "../backend/AirportSketch";
 import { AirportDao } from "../backend/AirportDao";
 
-const cycle = '2507'
+const cycle = '2508'
 
 // declare and execute
 async function doIt() {
