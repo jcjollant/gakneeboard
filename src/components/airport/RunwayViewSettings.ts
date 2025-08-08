a08787b16cbf1c5b69f480020c841e0a405eaeb3
JC Jollant <jc@jollant.net>
2025-08-07 19:39:11 -0700

    Renamed for clarity

--- src/components/airport/RunwayViewSettings.ts
+++ src/components/airport/RunwayViewSettings.ts
@@ -1,14 +1,14 @@
 import { Runway } from "../../model/Airport";
 
-export class RunwaySettings {
+export class RunwayViewSettings {
     static defaultOrientation:string = 'vertical'
     runway: Runway|undefined
     patternMode: number
     orientation: string
-    label: string
+    label: string|undefined
     headings: boolean
 
-    constructor(runway:Runway|undefined = undefined, patternMode:number = 0, orientation:string = RunwaySettings.defaultOrientation, label:string = '', headings:boolean = true) {
+    constructor(runway:Runway|undefined = undefined, patternMode:number = 0, orientation:string = RunwayViewSettings.defaultOrientation, label:string|undefined = undefined, headings:boolean = true) {
         this.runway = runway
         this.patternMode = patternMode
         this.orientation = orientation
