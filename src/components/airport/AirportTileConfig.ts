4ce0cdc86de4e37459f2a61bcfced632c0f13d07
JC Jollant <jc@jollant.net>
2025-08-09 11:29:04 -0700

    Added constructor

--- src/components/airport/AirportTileConfig.ts
+++ src/components/airport/AirportTileConfig.ts
@@ -1,11 +1,30 @@
-import { DisplayModeAirport } from "../../model/DisplayMode"
+import { DisplayModeAirport } from "../../model/DisplayMode";
+import { RunwayOrientation } from "./RunwayOrientation";
 
 export class AirportTileConfig {
-    code: string
-    rwys: string[]
-    pattern: number
-    corners: string[]
-    rwyOrientation: string
-    headings: boolean
-    mode: DisplayModeAirport
+  code: string;
+  rwys: string[];
+  pattern: number;
+  corners: string[];
+  rwyOrientation: RunwayOrientation;
+  headings: boolean;
+  mode: DisplayModeAirport;
+
+  constructor(
+    code: string = "",
+    rwys: string[] = [],
+    pattern: number = 0,
+    corners: string[] = [],
+    rwyOrientation: RunwayOrientation = RunwayOrientation.Vertical,
+    headings: boolean = true,
+    mode: DisplayModeAirport = DisplayModeAirport.RunwaySketch
+  ) {
+    this.code = code;
+    this.rwys = rwys;
+    this.pattern = pattern;
+    this.corners = corners;
+    this.rwyOrientation = rwyOrientation;
+    this.headings = headings;
+    this.mode = mode;
+  }
 }
