ed108a5bdba585405ab4b7a8abad8bc60fc1382d
JC Jollant <jc@jollant.net>
2025-08-15 14:54:24 -0700

    New Expand mode

--- src/components/shared/DisplayModeSelection.vue
+++ src/components/shared/DisplayModeSelection.vue
@@ -2,24 +2,34 @@
     <div class="modesList">
         <div v-for="(mode,index) in modes" class="choice">
             <Button  :label="mode.label" :severity="mode.value==model ? 'primary' : 'secondary'" class="labelButton" :title="mode.description"
-                @click="emits('selection', mode.value)"></Button>
-            <div v-if="expandable && mode.expandable" class="expandable" :class="'expand'+index"  title="Select mode and expand tile"
-                @click="emits('expand',mode.value)">
-                <font-awesome-icon class="expandableIcon" icon="fa-solid fa-left-right"></font-awesome-icon>
-            </div>
+            @click="model=mode.value"></Button>
         </div>
+        <EitherOr v-if="expandable" either="Normal" or="Wide" v-model="notExpanded" />
     </div>
 </template>
 
 <script setup lang="ts">
+import { onMounted, ref, watch } from 'vue'
 import { DisplayModeChoice } from '../../model/DisplayMode'
 import Button from 'primevue/button'
+import EitherOr from './EitherOr.vue'
 
-const emits = defineEmits(['selection','expand'])
+const emits = defineEmits(['expand'])
+const notExpanded = ref(true)
 const model = defineModel<string>()
 const props = defineProps({
     modes: { type: Array<DisplayModeChoice>, default: []},
-    expandable: { type: Boolean, default: false}
+    expandable: { type: Boolean, default: false},
+    expanded: { type: Boolean, default: false}
+})
+
+onMounted(() => {
+    notExpanded.value = !props.expanded
+})
+
+watch(notExpanded, (newVal) => {
+    // console.debug('[DisplayModeSelection.watch] expanded', !newVal)
+    emits('expand', !newVal)
 })
 
 </script>
@@ -33,7 +43,7 @@ const props = defineProps({
 }
 .labelButton {
     width:100%;
-    line-height: 29px;
+    line-height: 22px;
 }
 .choice {
     display: flex;
