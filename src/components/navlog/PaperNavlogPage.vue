5be7b0884d377f90eecc57c68dfefc1494466f8a
JC Jollant <jc@jollant.net>
2025-08-29 21:35:47 -0700

    New small format for paper navlog

--- src/components/navlog/PaperNavlogPage.vue
+++ src/components/navlog/PaperNavlogPage.vue
@@ -1,6 +1,6 @@
 
 <template>
-  <div class="contentPage landscape">
+  <div v-if="format==TemplateFormat.FullPage" class="contentPage landscape">
     <div class="grid">
       <div class="heading br" title="True Course">True Course</div>
       <div class="heading br" title="Altitude">Altitude</div>
@@ -29,7 +29,7 @@
       <div class="headingBottom fuelLeg bl">Leg</div>
       <div class="headingBottom fuelRem bl">Rem.</div>
     </div>
-    <div v-for="(line,index) in lines" class="grid line" :class="{'grey' : !(index % 2)}">
+    <div v-for="(line,index) in 16" class="grid line" :class="{'grey' : !(index % 2)}">
       <div v-for="cell in cells" :class="cell">&nbsp;</div>
     </div>
     <div class="grid">
@@ -38,24 +38,134 @@
       <div v-for="row in [0,1,2,3,4,5]" class="bl">&nbsp;</div>
     </div>
   </div>
+  <div v-else class="contentPage">
+    <Header title="Paper Navlog" :display-mode="false" @replace="emits('replace')" @click="onHeaderClick" />
+    <div v-if="editMode" class="pad5">
+      <InputGroup>
+          <InputGroupAddon class="checklistNameAddon">Waypoints</InputGroupAddon>
+          <InputText v-model="numlinesText" />
+      </InputGroup>
+      <ActionBar @cancel="onCancel" @apply="onApply" />
+    </div>
+    <div v-else>
+      <div class="smallGrid">
+        <div class="headingTop">Waypoint</div>
+        <div class="heading compass bl" title="Compass Heading">Compass Heading</div>
+        <div class="headingTop bl" title="Leg Distance">Dist.</div>
+        <div class="headingTop bl" title="Ground Speed">GS</div>
+        <div class="headingTop bl" title="Estimated Time Enroute">ETE</div>
+        <div class="headingTop bl" title="Estimated Time of Arrival">ETA</div>
+        <div class="headingTopSpan bl">Fuel</div>
+        <div class="from waypointText">From</div>
+        <div class="headingBottom distRemaining bl" title="Remaining Distance">Rem</div>
+        <div class="headingBottom actualGS bl" title="Actual Ground Speed">Act.</div>
+        <div class="headingBottom bl" title="Actual Time Enroute">Act.</div>
+        <div class="headingBottom bl" title="Actual Time of Arrival">Act.</div>
+        <div class="headingBottom fuelLeg bl">Leg</div>
+        <div class="headingBottom fuelRem bl">Rem.</div>
+      </div>
+      <div v-for="(line,index) in numlines + 1 " class="smallGrid line" :class="{'grey' : !(index % 2)}">
+        <div v-for="cell in cells" :class="cell">&nbsp;</div>
+      </div>
+      <div class="smallGrid bb">
+        <div class="to waypointText">To</div>
+        <div class="total bl">Totals</div>
+        <div v-for="row in [0,1,2,3,4,5]" class="bl">&nbsp;</div>
+      </div>
+      <div class="waypointText">Notes</div>
+    </div>
+  </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue'
+import { TemplateFormat } from '../../model/TemplateFormat'
+import { useToaster } from '../../assets/Toaster';
+import { useToast } from 'primevue/usetoast';
+
+import ActionBar from '../shared/ActionBar.vue'
+import Header from '../shared/Header.vue'
+import InputGroup from 'primevue/inputgroup'
+import InputGroupAddon from 'primevue/inputgroupaddon'
+import InputText from 'primevue/inputtext'
+
+
+const editMode = ref(false)
+const emits = defineEmits(['update','replace'])
+const props = defineProps({
+    format: { type: String, default: TemplateFormat.FullPage},
+    data: { type: Object, default: null }
+})
 
 // create an array of 10 elements filled with 0
-const lines = Array(16).fill(0)
-//const rows = Array(16).fill(0)
-// concatenate arrays
-const cells =  Array(7).fill('double  br')
+const cells = ref(<any>[])
+const minWaypoints = 2
+const maxWaypoints = 15
+const numlines = ref(maxWaypoints)
+const numlinesText = ref('')
+const toaster = useToaster( useToast())
+
+onMounted(() => {
+  loadProps(props)
+})
+
+watch(props, (newVal) => {
+  loadProps(newVal)
+})
+
+function loadProps(props:any) {
+  if(props.data) {
+    numlines.value = props.data.waypoints
+  }
+  // console.debug('[PaperNavlogPage.onMounted')
+  if(props.format == TemplateFormat.FullPage) {
+    cells.value = Array(7).fill('double  br')
             .concat(['single br','single','waypointBottom blf','double bl'])
-            .concat(Array(4).fill('single bl'))
+            .concat(Array(4).fill('single bl'))  // Distance GS ETE ETA
             .concat(['double bl','double bl','mvCell br','single','waypoint blf'])
             .concat(Array(4).fill('single bl'))
+  } else { // Small Format
+    cells.value = ['waypointBottom','double bl'] // waypoint and Compass Heading
+            .concat(Array(4).fill('single bl')) // Distance GS ETE ETA
+            .concat(['double bl','double bl','waypoint'])
+            .concat(Array(4).fill('single bl'))
+  }
+}
 
-// onMounted(() => {
-//   // console.debug('[PaperNavlogPage.onMounted')
-// })
+class NavlogConfig {
+  waypoints:number;
+  constructor(waypoints:number) {
+    this.waypoints = waypoints
+  }
+}
+
+function onApply() {
+  try {
+    const parsed = parseInt(numlinesText.value)
+    // force a value between 3 and 16
+    if(parsed < minWaypoints || parsed > maxWaypoints) {
+      throw new Error('Invalid value')
+    }
+    numlines.value = parseInt(numlinesText.value)
+    editMode.value = false
+    const config = new NavlogConfig( numlines.value)
+    emits('update', config)
+  } catch( err) {
+    toaster.error('Coming out broken', `Waypoints value out of range (${minWaypoints} ... ${maxWaypoints})`)
+  }
+}
+
+function onCancel() {
+  editMode.value = false
+}
+
+function onHeaderClick() {
+  if(editMode.value) {
+  } else {
+    numlinesText.value = numlines.value.toString()
+  }
+  editMode.value = !editMode.value
+}
 </script>
 
 <style scoped>
@@ -63,6 +173,14 @@ const cells =  Array(7).fill('double  br')
   display: grid;
   grid-template-columns: repeat(9, 1fr) 2fr repeat(7, 1fr);
   font-size: 0.8rem;
+}
+.smallGrid {
+  display: grid;
+  grid-template-columns: 2fr repeat(7, 1fr);
+  font-size: 0.8rem;
+}
+.bb {
+  border-bottom: 1px solid darkgrey;
 }
 .bl {
   border-left: 1px solid darkgrey;
@@ -73,7 +191,7 @@ const cells =  Array(7).fill('double  br')
 .br {
   border-right: 1px solid darkgrey;
 }
-.grid.line {
+.line {
   line-height: 20px;
 }
 .grey {
@@ -109,16 +227,19 @@ const cells =  Array(7).fill('double  br')
 .from {
   grid-row: 2;
 }
-.fuelLeg {
+.grid .fuelLeg {
   grid-column: 16;
 }
-.fuelRem {
+.grid .leg {
+  grid-column: 9;
+}
+.grid .fuelRem {
   grid-column: 17;
 }
 .compass {
   font-size: 0.7rem;
 }
-.distRemaining {
+.grid .distRemaining {
   grid-column: 12;
 }
 
@@ -130,13 +251,19 @@ const cells =  Array(7).fill('double  br')
   border-bottom: 1px solid darkgrey;
 }
 
-.waypoint {
+.grid .waypoint {
   grid-column: 10;
+}
+.waypoint {
   background-color: white;
-  /* border-bottom: 1px solid white; */
+}
+.grid .waypointBottom {
+  grid-column: 10;
+}
+.smallGrid .waypointBottom {
+  grid-column: 1;
 }
 .waypointBottom {
-  grid-column: 10;
   border-bottom: 1px solid black;
   background-color: white;
 }
@@ -145,22 +272,26 @@ const cells =  Array(7).fill('double  br')
   border-bottom: 1px solid darkgrey;
 }
 
+.grid .waypointText {
+  grid-column: 10;
+  background-color: white;
+}
 .waypointText {
   font-size: 1rem;
   display: flex;
   justify-content: flex-start;
   padding-left: 5px;
   color: lightgrey;
-  grid-column: 10;
   font-weight: bold;
 }
 .to {
   align-items: end;
   padding-bottom: 5px;
-  /* border-bottom: 1px solid darkgrey; */
+}
+.grid .total {
+  grid-column: 11;
 }
 .total {
-  grid-column: 11;
   font-weight: bold;
   line-height: 38px;
 }
