74b640188c6b81ce34b8cf9f2aaf926a0582fd0f
JC Jollant <jc@jollant.net>
2025-09-09 09:51:16 -0700

    New Dialog to pick airport codes

---
+++ src/components/airport/AirportSelectionDialog.vue
@@ -0,0 +1,22 @@
+<template>
+    <Dialog header="Select Airport" :style="{ width: '35rem' }">
+        <AirportInput code="KBFI" :auto="true" :expanded="true"
+            @valid="onValid" @invalid="onInvalid" />
+    </Dialog>
+</template>
+<script setup lang="ts">
+import { Airport } from '../../model/Airport';
+import AirportInput from '../shared/AirportInput.vue';
+import Dialog from 'primevue/dialog';
+
+const emits = defineEmits(['invalid','valid']);
+
+function onInvalid() {
+    emits('invalid');
+}
+
+function onValid(airport: Airport) {
+    emits('valid', airport);
+}
+
+</script>
