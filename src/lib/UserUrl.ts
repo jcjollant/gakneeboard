5bddd21f1c66ca9749decd6b2ea3702f2cde2ce6
JC Jollant <jc@jollant.net>
2025-09-11 21:40:54 -0700

    New strips page video link

--- src/lib/UserUrl.ts
+++ src/lib/UserUrl.ts
@@ -22,7 +22,7 @@ export class UserUrl {
     static editorVideo:string = 'https://youtu.be/v9C5Eyr_A3E'
     static ffExportVideo:string = 'https://youtu.be/BNRY8u1hNmo'
     static noteTilesVideo:string = 'https://youtu.be/nqgDI-CpFUU'
-    static stripPageVideo:string = 'https://youtu.be/l0Ea1awGHZQ'
+    static stripPageVideo:string = 'https://youtu.be/So5kDcN-vKc'
 
     // other external link
     static dtpp:string = 'https://aeronav.faa.gov/d-tpp/'
