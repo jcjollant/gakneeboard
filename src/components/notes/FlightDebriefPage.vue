980d7fcfa28ad173b6aac048d589f9ba4d6733b6
JC Jollant <jc@jollant.net>
2025-09-12 12:16:40 -0700

    User can save custom categories

--- src/components/notes/FlightDebriefPage.vue
+++ src/components/notes/FlightDebriefPage.vue
@@ -1,8 +1,16 @@
 <template>
     <div class="flightDebrief contentPage">
         <Header title="Flight Debrief" :page="true" :displayMode="false"
-            @replace="emits('replace')"></Header>
-        <div class="fdGrid">
+            @replace="emits('replace')" @click="toggleEditMode"></Header>
+        <div v-if="editMode" class="editMode">
+            <Textarea class='list' rows="25" cols="48" v-model="categoriesText" placeholder="Enter one category per line, up to 25 lines" style="resize:none"></Textarea>
+            <div class="autoCategories">
+                <Button label="VFR Maneuvers" @click="addCategories(vfrManeuvers)"></Button>
+                <Button label="IFR Maneuvers" @click="addCategories(ifrManeuvers)"></Button>
+            </div>
+            <ActionBar @apply="onApply" @cancel="editMode=false" />
+        </div>
+        <div v-else class="fdGrid">
             <div class="fdLine">
                 <div class="fdIcons brb">
                     <font-awesome-icon icon="fa-regular fa-face-smile" style="color: darkgreen;"/>
@@ -24,11 +32,76 @@
 </template>
 <script lang="ts" setup>
 
-import { ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import Header from '../shared/Header.vue';
+import ActionBar from '../shared/ActionBar.vue';
+import Button from 'primevue/button'
+import Textarea from 'primevue/textarea';
 
 const categories = ref([])
-const emits = defineEmits(['replace']);
+const categoriesText = ref()
+const editMode = ref(false)
+const emits = defineEmits(['replace', 'update']);
+const props = defineProps({
+    data: {type:Object, default: null}
+})
+const ifrManeuvers = ["RNAV LPV Apch", "RNAV LNAV Apch", "ILS Apch", "LOC Apch", "VOR/DME Apch", "Holds", "Track", "Intercept"]
+const vfrManeuvers = [
+    "Slow Flight",
+    "Power Off Stall",
+    "Power On Stall",
+    "Turn Around a Point",
+    "Rectangular Course",
+    "S Turns",
+    "Short Field Landing",
+    "Soft Field Landing"
+    ];
+
+
+onMounted( () => {
+    loadProps(props)
+})
+
+watch( props, () => {
+    loadProps(props)
+})
+
+function addCategories(list) {
+    let newText = categoriesText.value
+    if(newText.length) {
+        newText += '\n'
+    }
+    newText += list.join('\n')
+    categoriesText.value = newText;
+}
+
+function loadProps(props:any) {
+    // console.debug('[FlightDebriefPage]', props)
+    // restore categories
+    if(props && props.data && props.data.categories && props.data.categories.length > 0) {
+        categories.value = props.data.categories
+    } else {
+        categories.value = []
+    }
+}
+
+function onApply() {
+    // extract categories from text
+    categories.value = categoriesText.value.split('\n')
+
+    // TODO save new values
+    const data = { categories: categories.value}
+    emits('update', data)
+
+    editMode.value = false;
+}
+
+function toggleEditMode() {
+    editMode.value = !editMode.value
+    if(editMode.value) {
+        categoriesText.value = categories.value.join('\n')
+    }
+}
 
 </script>
 <style scoped>
@@ -59,6 +132,15 @@ const emits = defineEmits(['replace']);
 }
 .category {
     padding-left: 10px;
+    line-height: 28px;
     text-align: left;
+}
+.editMode {
+    padding: 5px;
+}
+.autoCategories {
+    display: flex;
+    gap: 10px;
+    justify-content: center;
 }
 </style>
