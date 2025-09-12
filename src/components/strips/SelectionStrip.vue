f4b26d391839835bbe51fa95cc44d7a97d3a5e6f
JC Jollant <jc@jollant.net>
2025-09-12 11:36:42 -0700

    Improved strip addition

--- src/components/strips/SelectionStrip.vue
+++ src/components/strips/SelectionStrip.vue
@@ -2,12 +2,19 @@
     <div class="stripContent">
         <div class="list">
             <div class="add">Add</div>
-            <Button v-for="s in strips" :label="s.name" @click="emits('selection', s.type)" :title="s.title" :class="s.class"></Button>
+            <!-- <Button v-for="s in strips" :label="s.name" @click="emits('selection', s.type)" :title="s.title" :class="s.class"></Button> -->
+             <div class="allStrips">
+                <div v-for="s in strips" @click="emits('selection', s.type)" class="clickable">
+                    <font-awesome-icon :icon="['fas', s.icon]" :class="s.class" :title="s.title" class="stripIcon"></font-awesome-icon>
+                    <div>{{ s.name }}</div>
+                </div>
+             </div>
             <!-- <Button label="Done" @click="emits('done')" link></Button> -->
         </div>
         <div class="actionButtons">
-            <font-awesome-icon :icon="['fas', 'check']" class="videoBtn"
-                @click="emits('done')" title="Done Adding Strips"></font-awesome-icon>
+            <Button label="Done" @click="emits('done')"></Button>
+            <!-- <font-awesome-icon :icon="['fas', 'check']" class="videoBtn"
+                @click="emits('done')" title="Done Adding Strips"></font-awesome-icon> -->
             <font-awesome-icon :icon="['fas', 'video']" class="videoLink"
                 @click="UserUrl.open( UserUrl.stripPageVideo)" title="Quick Demo Video"></font-awesome-icon>
         </div>
@@ -22,11 +29,11 @@ import { UserUrl } from '../../lib/UserUrl';
 
 const emits = defineEmits(['selection','done'])
 const strips = ref([
-    {name: 'Radio', type: StripType.radio, class: 'stripRadio', title: 'Facility Frequencies strip'},
-    {name: 'ATIS', type: StripType.atis, class: 'stripAtis', title: 'ATIS information strip'},
-    {name: 'CRAFT', type: StripType.craft, class: 'stripCraft', title: 'Departure Clearance'},
-    {name: 'Taxi', type: StripType.taxi, class: 'stripTaxi', title: 'Taxi Instructions strip'},
-    {name: 'Notes', type: StripType.notes, class: 'stripNotes', title: 'An expandable strip to take notes'},
+    {name: 'Radio', type: StripType.radio, class: 'stripRadio', title: 'Facility Frequencies strip', icon:'headset'},
+    {name: 'ATIS', type: StripType.atis, class: 'stripAtis', title: 'ATIS information strip', icon:'cloud-sun-rain'},
+    {name: 'CRAFT', type: StripType.craft, class: 'stripCraft', title: 'Departure Clearance', icon:'plane-circle-check'},
+    {name: 'Taxi', type: StripType.taxi, class: 'stripTaxi', title: 'Taxi Instructions strip', icon:'road-circle-check'},
+    {name: 'Notes', type: StripType.notes, class: 'stripNotes', title: 'An expandable strip to take notes', icon:'pen-to-square'},
 ])
 </script>
 
@@ -62,5 +69,13 @@ const strips = ref([
     gap: 5px;
     align-items: center;
     padding: 5px 10px;
+}
+.stripIcon {
+    color: var(--bg);
+    cursor: pointer;
+}
+.allStrips {
+    display: flex;
+    gap: 20px;
 }
 </style>
