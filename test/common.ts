3858ba2ba86eb8cae28ad6fd354a0676440ad3e1
JC Jollant <jc@jollant.net>
2025-10-04 16:18:51 -0700

    Test users use test.gak as a domain

--- test/common.ts
+++ test/common.ts
@@ -55,7 +55,7 @@ export function getTemplateView(numPages:number, id:number=0):TemplateView {
 
 export function newTestUser(userId:number=0, accoutType:AccountType=AccountType.simmer):User {
     const userName = Math.random().toString(36).substring(5)
-    const newEmail = Math.random().toString(36).substring(7) + '@test.com'
+    const newEmail = Math.random().toString(36).substring(7) + '@test.gak'
     const newHash = UserTools.createUserSha('test', newEmail)
     const newUser = new User(userId, newHash)
     newUser.email = newEmail
