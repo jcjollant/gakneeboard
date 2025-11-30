0aab888637fdc30eaa1be0984a7fc348dedeb9dc
JC Jollant <jc@jollant.net>
2025-11-29 22:01:50 -0800

    Extracted Display mode labels

---
+++ src/components/atis/AtisTileDisplayModeLabel.ts
@@ -0,0 +1,6 @@
+export class AtisTileDisplayModeLabels {
+    static fullATIS = "Full Notepad"
+    static compactATIS = "4 Lines Notepads"
+    static categories = "Flight Categories"
+    static cloudClearance = "VFR Cloud Clearance"
+}
