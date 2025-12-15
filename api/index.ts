4ccfb60c24c35eb099d98c9af433002e0ca18b4f
JC Jollant <jc@jollant.net>
2025-12-15 11:05:21 -0800

    Do not start the server in test

--- api/index.ts
+++ api/index.ts
@@ -448,7 +448,7 @@ app.post('/userImage', async (req: Request, res: Response) => {
 })
 
 
-if (process.env.__VERCEL_DEV_RUNNING != "1") {
+if (process.env.__VERCEL_DEV_RUNNING != "1" && process.env.NODE_ENV != 'test') {
     app.listen(port, () => console.log("[index] Server ready on port " + port));
 }
 
