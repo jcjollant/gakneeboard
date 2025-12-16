58665e75813c16d83deb0a4ad1e27cb0e7b8db2b
JC Jollant <jc@jollant.net>
2025-12-15 22:55:32 -0800

    Indentation

--- src/lib/GApiUrl.ts
+++ src/lib/GApiUrl.ts
@@ -1,36 +1,36 @@
 export class GApiUrl {
-    static root:string = 'https://api.kneeboard.ga/'
+    static root: string = 'https://api.kneeboard.ga/'
     // static root:string = 'https://ga-api-seven.vercel.app/'
-    //static root:string = 'http://localhost:3000/'
+    // static root: string = 'http://localhost:3000/'
 
     static flightPlanToNavlog() {
         return GApiUrl.root + 'fp2nl'
     }
-    
+
     static isTest(): boolean {
         return GApiUrl.root.indexOf('localhost') > -1
     }
 
-    static publications():string {
+    static publications(): string {
         return GApiUrl.root + 'publications'
     }
 
-    static publicationWithCode(code: string):string {
+    static publicationWithCode(code: string): string {
         return GApiUrl.root + 'publication/' + code
     }
 
-    static template(id:any=undefined):string {
-        if(id) {
+    static template(id: any = undefined): string {
+        if (id) {
             return GApiUrl.root + 'template/' + id
         }
         return GApiUrl.root + 'template'
     }
 
-    static templateExport(id:any, format:string):string {
+    static templateExport(id: any, format: string): string {
         return GApiUrl.root + 'export/template/' + id + '/' + format.toLowerCase();
     }
 
-    static templateThumbnail():string {
+    static templateThumbnail(): string {
         return GApiUrl.root + 'templateThumbnail'
     }
 }
