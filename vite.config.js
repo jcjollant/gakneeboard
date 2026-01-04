017ce11e81a39485b63d94a8b12e8f7511278d9c
JC Jollant <jc@jollant.net>
2026-01-03 23:01:22 -0800

    v6.2.3

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,29 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
-  },  
+  },
+  build: {
+    chunkSizeWarningLimit: 600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('jspdf') || id.includes('html2canvas-pro')) {
+              return 'jspdf';
+            }
+            if (id.includes('pdfjs-dist')) {
+              return 'pdfjs';
+            }
+            if (id.includes('primevue') || id.includes('primeicons')) {
+              return 'primevue';
+            }
+            if (id.includes('@fortawesome')) {
+              return 'fontawesome';
+            }
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
 })
