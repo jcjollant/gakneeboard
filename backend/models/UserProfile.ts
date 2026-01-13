7429bf2195843306fdddc663464608357990fa40
JC Jollant <jc@jollant.net>
2026-01-13 08:00:09 -0800

    Added create_tile to user profile

--- backend/models/UserProfile.ts
+++ backend/models/UserProfile.ts
@@ -2,16 +2,18 @@ import { UsageCount } from "./UsageCount";
 import { User } from "./User";
 
 export class UserProfile {
-    id:number
-    email:string;
-    name:string;
-    accountType:string;
-    eula:number;
-    templateCount:number;
-    pageCount:number;
+    id: number
+    email: string;
+    name: string;
+    accountType: string;
+    eula: number;
+    templateCount: number;
+    pageCount: number;
 
-    usage:UsageCount[]
-    constructor(user:User, usage:UsageCount[], templateCount:number, pageCount:number) {
+    usage: UsageCount[]
+    create_time: Date | undefined;
+
+    constructor(user: User, usage: UsageCount[], templateCount: number, pageCount: number) {
         this.id = user.id
         this.email = user.email
         this.name = user.name
@@ -19,6 +21,7 @@ export class UserProfile {
         this.eula = user.eula
         this.templateCount = templateCount
         this.pageCount = pageCount
+        this.create_time = user.createDate;
 
         this.usage = usage
     }
