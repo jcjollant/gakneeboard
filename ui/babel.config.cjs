465e34cf1113f956f9fe7eb007f666ee7c88ed30
JC Jollant <jc@jollant.net>
2026-01-16 22:11:41 -0800

    Test fix

--- ui/babel.config.cjs
+++ ui/babel.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
+    ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-typescript',
   ],
+  plugins: ['transform-vite-meta-env'],
 
 };
