9bd05d59ed48334ef77022fd6abdeb18ea2fb240
JC Jollant <jc@jollant.net>
2026-01-18 22:40:37 -0800

    Attempt to fix CORS

--- server/api/index.ts
+++ server/api/index.ts
@@ -475,7 +475,7 @@ app.post('/userImage', async (req: Request, res: Response) => {
 })
 
 
-if (process.env.__VERCEL_DEV_RUNNING != "1" && process.env.NODE_ENV != 'test') {
+if (process.env.__VERCEL_DEV_RUNNING != "1" && process.env.VERCEL != "1" && process.env.NODE_ENV != 'test') {
     app.listen(port, () => console.log("[index] Server ready on port " + port));
 }
 
