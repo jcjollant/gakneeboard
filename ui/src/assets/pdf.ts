9474acc085fcbbd54a8c70782a4b79e122583221
JC Jollant <jc@jollant.net>
2026-01-29 09:20:53 -0800

    Fixed print issue

--- ui/src/assets/pdf.ts
+++ ui/src/assets/pdf.ts
@@ -1,87 +1,84 @@
-export async function exportToPDF(elements:NodeListOf<Element>, landscape:boolean): Promise<void> {
-    // console.debug('[pdf.exportToPDF]', elements.length, landscape);
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas-pro';
 
-    const [{ jsPDF }, html2canvasModule] = await Promise.all([
-      import('jspdf'),
-      import('html2canvas-pro')
-    ]);
-  
-    const html2canvas = html2canvasModule.default;
-  
-    if (!elements.length) {
-      console.error('[pdf.exportToPDF] Element not found');
-      return;
-    }
-  
-    const pdf = new jsPDF({
-        orientation: landscape ? 'landscape' : 'portrait',
-        unit: 'in',
-        format: 'letter'
-      });
+export async function exportToPDF(elements: NodeListOf<Element>, landscape: boolean): Promise<void> {
+  // console.debug('[pdf.exportToPDF]', elements.length, landscape);
 
-    const margin = 0.25
-    const longSide = 11
-    const shortSide = 8.5
-    const pageWidth = landscape ? longSide : shortSide;
-    const printableWidth = pageWidth - 2 * margin;
-    const pageHeight = landscape ? shortSide : longSide;
-    const printableHeight = pageHeight - 2 * margin;
 
-    const printApectRatio = printableWidth / printableHeight;
-
-    // console.debug('[pdf.exportToPDF] WxH', pageWidth, pageHeight)
-
-    let imageCount = 0;
-    for( const element of elements) {
-        // add a page if
-        if( imageCount > 0) pdf.addPage();
-
-        const canvas = await html2canvas(element as HTMLElement, { 
-          scale: 2, 
-          allowTaint : true, 
-          useCORS: true,
-          // proxy: "https://cloudfront.foreflight.com",
-          // onclone: (doc) => {
-          //   let span;
-          //   for(span of doc.getElementsByClassName('coverImage')) {
-          //     console.debug('[pdf.exportToPDF] coverImage', span)
-          //     // get first child img element 
-          //     const img = span.querySelector('img');
-          //     // console.debug('[pdf.exportToPDF] coverImage', img)
-          //     // img.src = "https://cloudfront.foreflight.com/diagrams/2508/f70.jpg"
-          //     // img.src += '&t=' + Date.now()
-          //     // img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/500px-Google_2015_logo.svg.png"
-          //     img.crossOrigin = 'anonymous'
-
-          //     console.debug('[pdf.exportToPDF] img', img)
-          //   }
-          // } 
-        });
-        const imgData = canvas.toDataURL('image/png');
-        const imgProps = pdf.getImageProperties(imgData);
-        // console.debug('[pdf.exportToPDF] image', imgProps.width, imgProps.height)
-        const imgAspectRatio = imgProps.width / imgProps.height;
-        let scaledWidth = printableWidth;
-        let scaledHeight = printableHeight;
-        if( printApectRatio < imgAspectRatio) {
-          // Reduce height to scale
-          scaledHeight = printableHeight * printApectRatio / imgAspectRatio;
-        } else if( printApectRatio > imgAspectRatio) {
-          // Reduce width to scale
-          scaledWidth = printableWidth * imgAspectRatio / printApectRatio;
-        }
-        // center image
-        const xOffset = margin + (printableWidth - scaledWidth) / 2
-        const yOffset = margin + (printableHeight - scaledHeight) / 2
-        // const scaledWidth = printableHeight * imgProps.width / imgProps.height;
-        // console.debug('[pdf,exportToPDF] scaledW', scaledWidth, 'scaledH', scaledHeight, 'printableW', printableWidth, 'printableH', printableHeight)
-        pdf.addImage(imgData, 'PNG', xOffset, yOffset, scaledWidth, scaledHeight);
-        imageCount++;
-    }
-  
-    // At the end of exportToPDF
-    const blob = pdf.output('blob');
-    const url = URL.createObjectURL(blob);
-    window.open(url, '_blank'); // Open PDF in a new browser tab
-    
+  if (!elements.length) {
+    console.error('[pdf.exportToPDF] Element not found');
+    return;
   }
+
+  const pdf = new jsPDF({
+    orientation: landscape ? 'landscape' : 'portrait',
+    unit: 'in',
+    format: 'letter'
+  });
+
+  const margin = 0.25
+  const longSide = 11
+  const shortSide = 8.5
+  const pageWidth = landscape ? longSide : shortSide;
+  const printableWidth = pageWidth - 2 * margin;
+  const pageHeight = landscape ? shortSide : longSide;
+  const printableHeight = pageHeight - 2 * margin;
+
+  const printApectRatio = printableWidth / printableHeight;
+
+  // console.debug('[pdf.exportToPDF] WxH', pageWidth, pageHeight)
+
+  let imageCount = 0;
+  for (const element of elements) {
+    // add a page if
+    if (imageCount > 0) pdf.addPage();
+
+    const canvas = await html2canvas(element as HTMLElement, {
+      scale: 2,
+      allowTaint: true,
+      useCORS: true,
+      // proxy: "https://cloudfront.foreflight.com",
+      // onclone: (doc) => {
+      //   let span;
+      //   for(span of doc.getElementsByClassName('coverImage')) {
+      //     console.debug('[pdf.exportToPDF] coverImage', span)
+      //     // get first child img element 
+      //     const img = span.querySelector('img');
+      //     // console.debug('[pdf.exportToPDF] coverImage', img)
+      //     // img.src = "https://cloudfront.foreflight.com/diagrams/2508/f70.jpg"
+      //     // img.src += '&t=' + Date.now()
+      //     // img.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/500px-Google_2015_logo.svg.png"
+      //     img.crossOrigin = 'anonymous'
+
+      //     console.debug('[pdf.exportToPDF] img', img)
+      //   }
+      // } 
+    });
+    const imgData = canvas.toDataURL('image/png');
+    const imgProps = pdf.getImageProperties(imgData);
+    // console.debug('[pdf.exportToPDF] image', imgProps.width, imgProps.height)
+    const imgAspectRatio = imgProps.width / imgProps.height;
+    let scaledWidth = printableWidth;
+    let scaledHeight = printableHeight;
+    if (printApectRatio < imgAspectRatio) {
+      // Reduce height to scale
+      scaledHeight = printableHeight * printApectRatio / imgAspectRatio;
+    } else if (printApectRatio > imgAspectRatio) {
+      // Reduce width to scale
+      scaledWidth = printableWidth * imgAspectRatio / printApectRatio;
+    }
+    // center image
+    const xOffset = margin + (printableWidth - scaledWidth) / 2
+    const yOffset = margin + (printableHeight - scaledHeight) / 2
+    // const scaledWidth = printableHeight * imgProps.width / imgProps.height;
+    // console.debug('[pdf,exportToPDF] scaledW', scaledWidth, 'scaledH', scaledHeight, 'printableW', printableWidth, 'printableH', printableHeight)
+    pdf.addImage(imgData, 'PNG', xOffset, yOffset, scaledWidth, scaledHeight);
+    imageCount++;
+  }
+
+  // At the end of exportToPDF
+  const blob = pdf.output('blob');
+  const url = URL.createObjectURL(blob);
+  window.open(url, '_blank'); // Open PDF in a new browser tab
+
+}
--- ui/vite.config.js
+++ ui/vite.config.js
@@ -14,26 +14,7 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 600,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('jspdf') || id.includes('html2canvas-pro')) {
-              return 'jspdf';
-            }
-            if (id.includes('pdfjs-dist')) {
-              return 'pdfjs';
-            }
-            if (id.includes('primevue') || id.includes('primeicons')) {
-              return 'primevue';
-            }
-            if (id.includes('@fortawesome')) {
-              return 'fontawesome';
-            }
-            return 'vendor';
-          }
-        }
-      }
-    }
+
+
   },
 })
