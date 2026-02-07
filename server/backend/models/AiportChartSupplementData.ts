a730436021b7f3e0f3ccf35a1a438e43c71a0858
JC Jollant <jc@jollant.net>
2026-02-06 18:38:13 -0800

    New model for Chart Supplement Data

---
+++ server/backend/models/AiportChartSupplementData.ts
@@ -0,0 +1,8 @@
+export interface AirportChartSupplementData {
+    airportName: string;
+    city: string;
+    locId: string;
+    cycle: string;
+    supplementChartName: string | undefined;
+    noticeChartName: string | undefined;
+}
