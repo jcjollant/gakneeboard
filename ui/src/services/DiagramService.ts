ffb36713322ee930382de5f6087961361683db19
JC Jollant <jc@jollant.net>
2026-02-07 06:59:32 -0800

    Moved DiagramData to DiagramService

--- ui/src/services/DiagramService.ts
+++ ui/src/services/DiagramService.ts
@@ -1,17 +1,25 @@
 import axios from "axios"
-import { UrlService } from "../services/UrlService"
-import { LocalStoreService } from "../services/LocalStoreService"
+import { UrlService } from "./UrlService"
+import { LocalStoreService } from "./LocalStoreService"
 
-export class DiagramData {
-    static async getPdf(pdfFile: string): Promise<string> {
+export enum ChartType {
+    Diagram = 'diagram',
+    Supplement = 'supplement'
+}
+
+export class DiagramService {
+    static async getPdf(pdfFile: string, type: ChartType = ChartType.Diagram): Promise<string> {
         // do we already have that image in localstore?
         const image = LocalStoreService.getApproachPlate(pdfFile)
         if (image) {
             return (image);
         }
 
+        const endpoint = type === ChartType.Supplement ? 'supplement/' : 'diagram/';
+        const url = UrlService.root + endpoint + encodeURIComponent(pdfFile)
+        console.debug('[DiagramService.getPdf] url', url)
         return new Promise<string>(res => {
-            axios.get(UrlService.root + 'diagram/' + pdfFile).then(response => {
+            axios.get(url).then(response => {
                 // console.log('[Approach.getImage] response length', response.data.length)
                 // const apchImage = 'data:image/jpeg;base64,' + response.data;
                 const apchImage = response.data;
