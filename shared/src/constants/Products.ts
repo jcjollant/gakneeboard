f8a9f2614d8e636cd114bc513d0f48b95194c65c
JC Jollant <jc@jollant.net>
2026-02-08 07:05:12 -0800

    Products definition

---
+++ shared/src/constants/Products.ts
@@ -0,0 +1,12 @@
+import { Product } from "../models/Product";
+
+export const PRODUCTS: Product[] = [
+    {
+        id: 'ref-card-lam',
+        displayName: 'Laminated Reference Card',
+        description: 'Durable, high-quality laminated reference card for cockpit use.',
+        price: 9.99,
+        priceIdEnvVar: 'STRIPE_PRODUCT_REFCARD_PRICE',
+        images: ['/products/ref-card-front.png']
+    }
+];
