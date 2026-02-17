ea203fc322d65fe51b9cbbc4aa013ff757660bf9
JC Jollant <jc@jollant.net>
2026-02-16 16:25:53 -0800

    schema changes for null user_id

---
+++ server/sql/migrations/migration_20260216_system_kneeboards.sql
@@ -0,0 +1,3 @@
+-- Active: 1768842526847@@ep-proud-field-a6tfe60l-pooler.us-west-2.aws.neon.tech@5432@neondb
+-- Active: 1768058934965@@ep-shrill-silence-a6ypne6y-pooler.us-west-2.aws.neon.tech@5432@verceldber.us-west-2.aws.neon.tech@5432@postgres
+ALTER TABLE sheets ALTER COLUMN user_id DROP NOT NULL;
