e28572cc974f69b9c3a946a6dff4c8e3fb6319c0
JC Jollant <jc@jollant.net>
2026-02-18 08:02:16 -0800

    New Usage Type

---
+++ server/sql/migrations/migration_20260218_add_create_doc_usage_type.sql
@@ -0,0 +1,1 @@
+ALTER TYPE usagetype ADD VALUE 'create-doc';
--- shared/src/models/UsageType.ts
+++ shared/src/models/UsageType.ts
@@ -1,4 +1,5 @@
 export enum UsageType {
+    CreateDoc = 'create-doc',
     Eula = 'eula',
     Export = 'export',
     Print = 'print',
