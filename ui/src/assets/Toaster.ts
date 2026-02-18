1d84f3566152d136eea6c54ff55043322656ff9a
JC Jollant <jc@jollant.net>
2026-02-18 06:58:16 -0800

    New Generic Upgrade Method

--- ui/src/assets/Toaster.ts
+++ ui/src/assets/Toaster.ts
@@ -1,29 +1,33 @@
 import { ToastServiceMethods } from "primevue/toastservice";
 
-export const useToaster = (toast:ToastServiceMethods) => {
+export const useToaster = (toast: ToastServiceMethods) => {
     return new Toaster(toast);
 }
 
 export class Toaster {
-    private toast:ToastServiceMethods;
+    private toast: ToastServiceMethods;
 
-    constructor(toast:ToastServiceMethods) {
+    constructor(toast: ToastServiceMethods) {
         this.toast = toast
     }
 
-    error(title:string, message:string, life:number=3000) {
+    error(title: string, message: string, life: number = 3000) {
         this.toast.add({ severity: 'error', summary: title, detail: message, life: life });
     }
 
-    info(title:string, message:string, life:number=3000) {
+    info(title: string, message: string, life: number = 3000) {
         this.toast.add({ severity: 'info', summary: title, detail: message, life: life });
     }
 
-    success(title:string, message:string, life:number=3000) {
+    success(title: string, message: string, life: number = 3000) {
         this.toast.add({ severity: 'success', summary: title, detail: message, life: life });
     }
 
-    warning(title:string, message:string, life:number=3000) {
+    upgrade() {
+        this.warning('Upgrade Required', 'Please upgrade your subscription to use this feature')
+    }
+
+    warning(title: string, message: string, life: number = 3000) {
         this.toast.add({ severity: 'warn', summary: title, detail: message, life: life });
     }
 
