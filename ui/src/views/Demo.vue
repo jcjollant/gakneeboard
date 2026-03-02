2ce57c777c59f792c5c05efcba2406e610d014c6
JC Jollant <jc@jollant.net>
2026-03-01 19:26:11 -0800

    Using route

--- ui/src/views/Demo.vue
+++ ui/src/views/Demo.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="demo">
-    <FlightInfoDialog v-model:visible="showFlightDialog" @cancel="onCancel" @confirm="onFlightConfirm" />
+    <RouteDialog v-model:visible="showFlightDialog" :modelValue="flightRoute" :video="UserUrl.routeVideo"
+      message="Let's make this kneeboard useful by selecting airports you care about"
+      @cancel="onRouteCancel" @confirm="onRouteConfirm" />
     <div class="spinner"></div>
     <h1>Loading Demo...</h1>
     <p>{{ demoName }}</p>
@@ -19,18 +21,33 @@ import { Airport } from '../models/Airport'
 import { DisplayModeIfr, DisplayModeSunlight } from '../models/DisplayMode'
 import { Frequency, FrequencyType } from '../models/Frequency'
 import { Formatter } from '../lib/Formatter'
+import { getAirport } from '../services/AirportDataService'
+import { Route as FlightRoute } from '@gak/shared'
 
-import FlightInfoDialog from '../components/airport/DemoAirportSelection.vue'
+import RouteDialog from '../components/templates/RouteDialog.vue'
+import { UserUrl } from '../lib/UserUrl'
+import { currentUser } from '../assets/data'
 
-const route = useRoute()
+const locationRoute = useRoute()
 const router = useRouter()
-const demoName = route.params.name as string
+const demoName = locationRoute.params.name as string
 const showFlightDialog = ref(false)
 const toast = useToast()
 const toaster = useToaster(toast)
 
+const flightRoute = ref<FlightRoute>({ dep: 'KBFI', dst: 'KPAE', alt: 'KRNT' })
+
+let isConfirming = false
 onMounted(() => {
+
   if (demoName === SheetName.vfrFlight || demoName == SheetName.ifrFlight) {
+    // try to build a route from last route
+    const lastRoute = LocalStoreService.getRoute()
+    if (lastRoute) {
+      flightRoute.value = lastRoute
+    } else if (currentUser.homeAirport) {
+      flightRoute.value.dep = currentUser.homeAirport
+    }
     showFlightDialog.value = true
   } else {
     loadDemo()
@@ -47,9 +64,9 @@ function loadDemo() {
   router.replace('/template/local')
 }
 
-function setAirportTile(templateData: any, airport: Airport, pageNumber: number, tileNumber: number) {
+function setAirportTile(templateData: any, code:string, airport: Airport, pageNumber: number, tileNumber: number) {
   if (templateData.data[pageNumber]?.data?.[tileNumber]) {
-    templateData.data[pageNumber].data[tileNumber].data = {code: airport.code, rwy: airport.rwys[0].name}
+    templateData.data[pageNumber].data[tileNumber].data = {code: code, rwy: airport.rwys[0].name}
   }
 }
 
@@ -71,20 +88,39 @@ function setPageData(templateData: any, pageNumber: number, pageData: any) {
   }
 }
 
-function onCancel() {
+function onRouteCancel() {
+  if (isConfirming) return
   router.push('/')
 }
 
-function onFlightConfirm(airports: {from: Airport | null, to: Airport | null, alternate: Airport | null}) {
+async function onRouteConfirm(newRoute: FlightRoute) {
+  isConfirming = true
+  LocalStoreService.saveRoute(newRoute)
+  // close dialog
   showFlightDialog.value = false
   const templateData = DemoData.fromName(demoName)
   if (!templateData) {
     toaster.error('Load Demo', 'Unknown Demo Template')
     return
   }
+
+  const airports: {departure: Airport | null, destination: Airport | null, alternate: Airport | null} = {
+    departure: null,
+    destination: null,
+    alternate: null
+  }
+
+  try {
+    if (newRoute.dep) airports.departure = await getAirport(newRoute.dep)
+    if (newRoute.dst) airports.destination = await getAirport(newRoute.dst)
+    if (newRoute.alt) airports.alternate = await getAirport(newRoute.alt)
+  } catch (e) {
+    console.error('Failed to resolve airports:', e)
+  }
   
+  // Build a list of airports to display frequencies
   const frequencies: Frequency[] = []
-  const airportList = [airports.from, airports.to, airports.alternate].filter(a => a !== null) as Airport[]
+  const airportList = [airports.departure, airports.destination, airports.alternate].filter(a => a !== null && a.isValid()) as Airport[]
   
   airportList.forEach(airport => {
     // console.debug('[Demo.onFlightConfirm]', airport)
@@ -100,36 +136,43 @@ function onFlightConfirm(airports: {from: Airport | null, to: Airport | null, al
   
   try {
     if( demoName == SheetName.vfrFlight) {
-      if (airports.from) {
-        // keep home airport in local storage for future use
-        localStorage.setItem(LocalStoreService.tempHomeAirport, airports.from.code)
-        setAirportTile(templateData, airports.from, 0, 0)
-        setTileData(templateData, 1, 3, {from: airports.from.code, to: airports.from.code, mode: DisplayModeSunlight.Flight})
+      if (airports.departure) {
+        // Force departure airport in top left tile (Page 0 tile 0)
+        setAirportTile(templateData, '#dep', airports.departure, 0, 0)
+        // Force sunlight to departure airport (Page 1 tile 3)
+        setTileData(templateData, 1, 3, {from: airports.departure.code, to: airports.departure.code, mode: DisplayModeSunlight.Flight})
       }
-      if (airports.to) {
-        setAirportTile(templateData, airports.to, 0, 4)
+      if (airports.destination) {
+        // Force destination airport (Page 0 tile 4)
+        setAirportTile(templateData, '#dst', airports.destination, 0, 4)
       }
       if (airports.alternate) {
-        setAirportTile(templateData, airports.alternate, 1, 1)
+        // Force alternate airport (Page 1 tile 1)
+        setAirportTile(templateData, '#alt', airports.alternate, 1, 1)
       }
+      
+      // Set frequencies (Page 0 tile 2)
       setRadioTile(templateData, frequencies, 0, 2)
+
     } else if( demoName == SheetName.ifrFlight) {
       // replace destination airport
-      if(airports.from) {
-        setTileData( templateData, 0, 0, {  mode: DisplayModeIfr.Departure,  airport: airports.from.code} )
+      if(airports.departure) {
+        setTileData( templateData, 0, 0, {  mode: DisplayModeIfr.Departure,  airport: airports.departure.code} )
       }
 
-      if( airports.to) {
-        setPageData( templateData, 2, { airport: airports.to, "pdf": 0 })
+      if( airports.destination) {
+        setPageData( templateData, 2, { airport: airports.destination.code, "pdf": 0 })
       }
 
       // replace alternate airport
       if(airports.alternate) {
-        setAirportTile(templateData, airports.alternate, 1, 2)
+        setAirportTile(templateData, '#alt', airports.alternate, 1, 2)
       }
       // refresh frequencies
       setRadioTile(templateData, frequencies, 0, 1)
     }
+    // Assign route to template
+    templateData.route = newRoute
   } catch (e) {
     toaster.error('Load Demo', 'Error loading demo data')
     console.error( '[Demo.onFlightConfirm]' + e)
